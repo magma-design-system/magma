5bf8a416c998ff0be7859b55d66727906538ab3a
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-02-28 10:41:11 +0100

    Adds radio component

---
+++ src/react/Policy/Form/Radio/Radio.stories.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react'
+import faker from 'faker'
+faker.locale = 'it'
+
+import Radio from './Radio'
+
+export default {
+  title: 'Policy App/Form/Radio',
+  component: Radio,
+}
+
+export const basicUsage = () =>
+  <Fragment>
+    <Radio name="radioGroupName" value="1">
+      {faker.lorem.sentences()}
+    </Radio>
+    <Radio name="radioGroupName" value="2">
+      {faker.lorem.sentences()}
+    </Radio>
+  </Fragment>
+
+export const checkedByDefault = () =>
+  <Fragment>
+    <Radio name="radioGroupName" value="1">
+      {faker.lorem.sentences()}
+    </Radio>
+    <Radio name="radioGroupName" value="2" isChecked={true}>
+      {faker.lorem.sentences()}
+    </Radio>
+  </Fragment>
+
+export const customIcon = () =>
+  <Fragment>
+    <Radio name="radioGroupName" value="1" icon="warning">
+      {faker.lorem.sentences()}
+    </Radio>
+    <Radio name="radioGroupName" value="2" icon="error">
+      {faker.lorem.sentences()}
+    </Radio>
+  </Fragment>
+
+export const customColor = () =>
+  <Fragment>
+    <Radio name="radioGroupName" value="1" icon="warning">
+      {faker.lorem.sentences()}
+    </Radio>
+    <Radio name="radioGroupName" value="2" icon="error" iconClassName="color-red-2">
+      {faker.lorem.sentences()}
+    </Radio>
+  </Fragment>
