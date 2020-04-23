a9e6d0cacc13a68844f139660feed5a9a8d3cb01
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-04-23 15:30:43 +0200

    Updates color structure

--- src/style-dictionary/formats/scss-map/scss-map.js
+++ src/style-dictionary/formats/scss-map/scss-map.js
@@ -7,6 +7,14 @@ const templatePath = path.resolve(__dirname, './scss-map.hbs')
 
 const template = Handlebars.compile(fs.readFileSync(templatePath).toString())
 
+Handlebars.registerHelper('eachSorted', (context, options) => {
+  let ret = ''
+  Object.keys(context).sort().forEach((key, index) => {
+    ret = ret + options.fn({ key, value: context[key] })
+  })
+  return ret
+})
+
 StyleDictionary.registerFormat({
   name: 'scss/map',
   formatter: function(dictionary, platform) {
