c46bc5e978081b3d92db46d6582e4d165237e9cb
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-06-11 12:24:35 +0200

    Removes unstyled paragraphs

--- src/react/Layout/Grid/Grid.stories.js
+++ src/react/Layout/Grid/Grid.stories.js
@@ -2,6 +2,8 @@ import React from 'react'
 import faker from 'faker'
 
 import Grid from './Grid'
+import Paragraph from '@Typography/Paragraph/Paragraph'
+
 faker.locale = 'it'
 
 export default {
@@ -14,50 +16,50 @@ const paragraph2 = faker.lorem.paragraph()
 
 export const gutterLarge = () =>
   <Grid gutter="large">
-    <p className="text-sans text-sans--paragraph">{paragraph1}</p>
-    <p className="text-sans text-sans--paragraph">{paragraph2}</p>
+    <Paragraph>{paragraph1}</Paragraph>
+    <Paragraph>{paragraph2}</Paragraph>
   </Grid>
 
 export const gutterNormal = () =>
   <Grid>
-    <p className="text-sans text-sans--paragraph">{paragraph1}</p>
-    <p className="text-sans text-sans--paragraph">{paragraph2}</p>
+    <Paragraph>{paragraph1}</Paragraph>
+    <Paragraph>{paragraph2}</Paragraph>
   </Grid>
 
 export const gutterSmall = () =>
   <Grid gutter="small">
-    <p className="text-sans text-sans--paragraph">{paragraph1}</p>
-    <p className="text-sans text-sans--paragraph">{paragraph2}</p>
+    <Paragraph>{paragraph1}</Paragraph>
+    <Paragraph>{paragraph2}</Paragraph>
   </Grid>
 
 export const gutterXSmall = () =>
   <Grid gutter="xsmall">
-    <p className="text-sans text-sans--paragraph">{paragraph1}</p>
-    <p className="text-sans text-sans--paragraph">{paragraph2}</p>
+    <Paragraph>{paragraph1}</Paragraph>
+    <Paragraph>{paragraph2}</Paragraph>
   </Grid>
 
 export const with2Columns = () =>
   <Grid columns="2">
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
   </Grid>
 
 export const with3Columns = () =>
   <Grid columns="3">
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
   </Grid>
 
 export const with4Columns = () =>
   <Grid columns="4">
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
-    <p className="text-sans text-sans--paragraph">{faker.lorem.paragraph()}</p>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
+    <Paragraph>{faker.lorem.paragraph()}</Paragraph>
   </Grid>
