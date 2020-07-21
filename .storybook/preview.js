e5af7f84b8fd485e600c7ed671553d3dcf0323f8
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-07-21 20:56:25 +0200

    Adds override example

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,7 +4,13 @@ import Page from '@System/Page/Page'
 // import faker from 'faker'
 // faker.locale = 'it'
 
-import '../src/scss/default-assets.scss'
+// vendors
+import '../node_modules/normalize.css/normalize.css'
+import '../node_modules/material-design-icons/iconfont/material-icons.css'
+import '../node_modules/typeface-karla/index.css'
+import '../node_modules/typeface-roboto/index.css'
+
+// design system base styles
 import '../src/scss/import-global.scss'
 
 addDecorator(storyFn => <Page>{storyFn()}</Page>);
