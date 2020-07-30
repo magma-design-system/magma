6f8f0942d98cd187bd5d04b8ef53e4258d82dc9c
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-07-30 11:44:58 +0200

    Some small update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ['../src/react/**/*.stories.(js|mdx)'],
+  stories: [
+    '../src/react/**/*.stories.js',
+    // '../src/react/**/*.stories.mdx',
+  ],
   addons: [
     //'@storybook/addon-actions',
     //'@storybook/addon-links',
