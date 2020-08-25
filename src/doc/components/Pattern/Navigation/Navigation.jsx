999ef92a2b0bb358a8a1a0b208d632092321c13f
Gioele Masini <gioele.masini@maggioli.it>
2020-08-25 12:03:44 +0200

    Refactory menu creation

--- src/doc/components/Pattern/Navigation/Navigation.jsx
+++ src/doc/components/Pattern/Navigation/Navigation.jsx
@@ -6,12 +6,7 @@ import Grid from '@Layout/Grid/Grid'
 import H1 from '@Typography/H1/H1'
 import Hr from '@Gatsby/Pattern/Hr/Hr'
 import Menu, { MenuItem, MenuSubItem } from '@Gatsby/Pattern/Menu/Menu'
-
-const basePathTitles = {
-  doc: 'Doc',
-  use: 'Use',
-  dev: 'Dev',
-}
+import { createMenuList } from './menu'
 
 const query = graphql`
   query NavigationQuery {
@@ -38,47 +33,8 @@ const query = graphql`
   }
 `
 
-function updateMenu(menu, path) {
-  let menuItem = menu.find(voce => voce.id === path[0])
-  if (menuItem == null) {
-    menuItem = { id: path[0] }
-    menu.push(menuItem)
-  }
-  if (!menuItem.children) menuItem.children = []
-  if (path.length > 1) menuItem.children = updateMenu(menuItem.children, path.slice(1))
-  return menu
-}
-
-function findParent(menu, path) {
-  const menuItem = menu.find(voce => voce.id === path[0])
-  if (path.length > 1) return findParent(menuItem.children, path.slice(1))
-  return menuItem
-}
-
 const Navigation = props => {
-  let menu = []
-  props.data.forEach(edge => {
-    const link = edge.node.slug.endsWith('/') ? edge.node.slug.slice(0, -1) : edge.node.slug
-    const path = link.split('/')
-    if (path.length === 1 && path[0] === '') return
-
-    menu = updateMenu(menu, path.slice(0, -1))
-    const parent = findParent(menu, path.slice(0, -1))
-
-    const menuItem = parent.children.find(voce => voce.id === path[path.length - 1])
-    if (menuItem == null) {
-      parent.children.push({
-        id: path[path.length - 1],
-        title: edge.node.frontmatter.title,
-        url: `/${edge.node.slug}`,
-      })
-    } else {
-      menuItem.title = edge.node.frontmatter.title
-      menuItem.url = `/${edge.node.slug}`
-    }
-  })
-  menu.forEach(voceMenu => (voceMenu.title = basePathTitles[voceMenu.id]))
-
+  const menuList = createMenuList(props.data)
   const currentUrl = typeof window !== 'undefined' ? window.location.pathname : ''
 
   return (
@@ -86,16 +42,16 @@ const Navigation = props => {
       <Grid>
         <H1>{ props.title }</H1>
         {
-          menu.map((menuItem, key) =>
+          menuList.map((menu, key) =>
             <Fragment key={key}>
               <Hr/>
-              <Menu title={menuItem.title}>
+              <Menu title={menu.title}>
                 {
-                  (menuItem.children || []).map((menuSubItem, key) =>
-                    <MenuItem key={key} title={menuSubItem.title} url={menuSubItem.url} isSelected={currentUrl === menuSubItem.url} isOpened={currentUrl.startsWith(menuSubItem.url)}>
+                  (menu.children || []).map((menuItem, key) =>
+                    <MenuItem key={key} title={menuItem.title} url={menuItem.url} isSelected={currentUrl === menuItem.url} isOpened={currentUrl.startsWith(menuItem.url)}>
                       {
-                        (menuSubItem.children || []).map((menuLastItem, key) =>
-                          <MenuSubItem key={key} title={menuLastItem.title} url={menuLastItem.url} isSelected={currentUrl === menuLastItem.url}/>,
+                        (menuItem.children || []).map((menuSubItem, key) =>
+                          <MenuSubItem key={key} title={menuSubItem.title} url={menuSubItem.url} isSelected={currentUrl === menuSubItem.url}/>,
                         )
                       }
                     </MenuItem>,
---
+++ src/doc/components/Pattern/Navigation/menu.js
@@ -0,0 +1,59 @@
+const basePathTitles = {
+  doc: 'Doc',
+  use: 'Use',
+  dev: 'Dev',
+}
+
+export function createMenuList(edges) {
+  let menuList = []
+  edges.forEach(({ node: { slug, frontmatter } }) => {
+    const path = (slug.endsWith('/') ? slug.slice(0, -1) : slug).split('/')
+    if (isBlacklisted(path)) return // Some pages shouldn't be in the menu
+
+    menuList = createStructure(menuList, path.slice(0, -1))
+    const parent = findVoceMenu(menuList, path.slice(0, -1))
+
+    const menuItemData = {
+      id: path[path.length - 1],
+      title: frontmatter.title,
+      url: `/${slug}`,
+    }
+
+    const menuItem = parent.children.find(voce => voce.id === menuItemData.id)
+    if (menuItem == null) {
+      parent.children.push(menuItemData)
+    } else {
+      menuItem.title = menuItemData.title
+      menuItem.url = menuItemData.url
+    }
+  })
+  menuList.forEach(voceMenu => (voceMenu.title = basePathTitles[voceMenu.id]))
+  return menuList
+}
+
+function createStructure(menu, [voceId, ...restPath]) {
+  let menuItem = menu.find(voce => voce.id === voceId)
+  if (menuItem == null) {
+    menuItem = { id: voceId }
+    menu.push(menuItem)
+  }
+  if (restPath.length > 0) {
+    if (!menuItem.children) menuItem.children = []
+    menuItem.children = createStructure(menuItem.children, restPath)
+  }
+  return menu
+}
+
+function findVoceMenu(menuList, path) {
+  const menuItem = menuList.find(voce => voce.id === path[0])
+  if (path.length > 1) return findVoceMenu(menuItem.children, path.slice(1))
+  return menuItem
+}
+
+function isBlacklisted(path) {
+  return isHomePage(path)
+}
+
+function isHomePage(path) {
+  return path.length === 1 && path[0] === ''
+}
