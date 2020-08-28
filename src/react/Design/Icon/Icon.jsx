befbb4244e3cb7b44175fe1ac779adcea77609ee
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-08-28 17:51:16 +0200

    Adds some test to icons

--- src/react/Design/Icon/Icon.jsx
+++ src/react/Design/Icon/Icon.jsx
@@ -4,19 +4,24 @@ import './Icon.scss'
 
 import dictionary from './dictionary.json'
 
+// todo: https://www.meziantou.net/creating-a-fontawesome-bundle-with-only-the-icons-you-use.htm
+// todo: https://vuetifyjs.com/en/customization/icons/
+// todo: https://stackoverflow.com/questions/57552261/vuetifyjs-adding-only-used-icons-to-build#answer-57552882
+
 const Icon = props =>
-  <i title={dictionary[props.name] ? '' : 'Warning: property "name" not set'} className={`material-icons icon ${(props.size ? 'icon--' + props.size : '')} ${props.className}`}>{(dictionary[props.name] ? dictionary[props.name] : dictionary.missingIcon)}</i>
+  <i title={dictionary[props.name][props.family] ? '' : 'Warning: property "name" not set'} className={`material-icons icon ${(props.size ? 'icon--' + props.size : '')} ${props.className}`}>{(dictionary[props.name][props.family] ? dictionary[props.name][props.family] : dictionary.missingIcon[props.family])}</i>
 
 Icon.propTypes = {
   className: PropTypes.string,
   name: PropTypes.string,
+  family: PropTypes.string,
   size: PropTypes.string,
 }
 
 Icon.defaultProps = {
   className: '',
   name: 'missingIcon',
-  family: 'maggioli|material',
+  family: 'material',
   size: '',
 }
 
