65498ab4357e4c5b8cd0799906aa9643aef6af05
Gioele Masini <gioele.masini@maggioli.it>
2020-09-16 16:33:37 +0200

    Fix bin files and build folder position

--- projects/font-icons/lib/utils.js
+++ projects/font-icons/lib/utils.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
 const ROOT_PATH_DIR = path.join(__dirname, '..')
+const BUILD_PATH_DIR = path.join(process.cwd(), 'build')
 
 exports.ROOT_PATH_DIR = ROOT_PATH_DIR
+exports.BUILD_PATH_DIR = BUILD_PATH_DIR
