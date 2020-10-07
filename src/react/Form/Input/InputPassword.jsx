cb122d733505f68db50b96ddd13de672658bb679
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-10-07 09:39:07 +0200

    Adds password field

---
+++ src/react/Form/Input/InputPassword.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Input.scss'
+import Input from './Input'
+
+const InputPassword = props =>
+  <Input {...props}>
+    <input
+      autoComplete={props.autoComplete}
+      className="input__field text-sans text-sans--input"
+      defaultValue={props.value}
+      name={props.name}
+      onChange={props.onChange}
+      placeholder={props.placeholder}
+      type="password"
+    />
+  </Input>
+
+InputPassword.propTypes = {
+  autoComplete: PropTypes.string,
+  icon: PropTypes.string,
+  iconClassName: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+}
+
+InputPassword.defaultProps = {
+  autoComplete: 'off',
+  icon: 'password',
+  iconClassName: '',
+  name: 'unassigned',
+  onChange: value => { return value },
+  placeholder: '',
+  value: '',
+}
+
+export default InputPassword
