ce89c795f9d9f255a4faa77f5a5116b8ff2f7cfb
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-10-13 12:56:35 +0200

    Adds KPI component

--- src/react/Content/Kpi/Kpi.stories.js
+++ src/react/Content/Kpi/Kpi.stories.js
@@ -1,14 +1,48 @@
 import React from 'react'
 import faker from 'faker'
 
-import Paragraph from '@Typography/Paragraph/Paragraph'
+import Kpi, { KpiItem } from '@Content/Kpi/Kpi'
+import randomIcon from '@Design/Icon/faker'
 
 faker.locale = 'it'
 
 export default {
   title: 'Content/KPI',
-  component: Paragraph,
+  component: Kpi,
 }
 
 export const basicUsage = () =>
-  <Paragraph>This component is unfinished</Paragraph>
+  <Kpi>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+  </Kpi>
+
+export const fewItems = () =>
+  <Kpi>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+  </Kpi>
+
+export const manyItems = () =>
+  <Kpi>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+  </Kpi>
+
+export const customColor = () =>
+  <Kpi>
+    <KpiItem className="background-color-adjust-tone-05 color-adjust-tone-12" amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem className="background-color-adjust-tone-05 color-adjust-tone-12" amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem className="background-color-adjust-tone-05 color-adjust-tone-12" amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem className="background-color-adjust-tone-05 color-adjust-tone-12" amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+    <KpiItem className="background-color-adjust-tone-05 color-adjust-tone-12" amount={faker.random.number(10000)} icon={randomIcon()}>{faker.hacker.verb()}</KpiItem>
+  </Kpi>
