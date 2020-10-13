b5bd774e8331af763080e97507124b5b1805c283
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-10-13 17:29:53 +0200

    Updates docs

--- src/react/Element/Note/Note.stories.js
+++ src/react/Element/Note/Note.stories.js
@@ -3,7 +3,10 @@ import React from 'react'
 import Note from '@Element/Note/Note'
 import Grid from '@Layout/Grid/Grid'
 import Caption from '@Typography/Caption/Caption'
+import Detail from '@Typography/Detail/Detail'
 import H5 from '@Typography/H5/H5'
+
+import faker from 'faker'
 
 export default {
   title: 'Element/Note',
@@ -14,7 +17,7 @@ export const basicUsage = () =>
   <Grid columns="3">
     <Note>
       <H5>Note</H5>
-      <Caption></Caption>
+      <Caption>{faker.lorem.paragraph()}</Caption>
     </Note>
   </Grid>
 
@@ -22,7 +25,7 @@ export const Dismissable = () =>
   <Grid columns="3">
     <Note dismiss={true}>
       <H5>Action complete</H5>
-      <Caption></Caption>
+      <Caption>{faker.lorem.paragraph()}</Caption>
     </Note>
   </Grid>
 
@@ -30,18 +33,18 @@ export const Colors = () =>
   <Grid columns="3" fit={true}>
     <Note status="success">
       <H5>Success</H5>
-      <Caption></Caption>
+      <Detail>{faker.lorem.paragraph()}</Detail>
     </Note>
     <Note status="info">
       <H5>Info</H5>
-      <Caption></Caption>
+      <Detail>{faker.lorem.paragraph()}</Detail>
     </Note>
     <Note status="warning">
       <H5>Warning</H5>
-      <Caption></Caption>
+      <Detail>{faker.lorem.paragraph()}</Detail>
     </Note>
     <Note status="error">
       <H5>Error</H5>
-      <Caption></Caption>
+      <Detail>{faker.lorem.paragraph()}</Detail>
     </Note>
   </Grid>
