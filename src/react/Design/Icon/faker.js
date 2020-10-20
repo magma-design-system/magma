e57a76a47909e303783da7f8c47dce8bfb560a08
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-10-20 12:18:44 +0200

    Optimize random icons

--- src/react/Design/Icon/faker.js
+++ src/react/Design/Icon/faker.js
@@ -2,8 +2,8 @@ import faker from 'faker'
 import mggIconsDictionary from '£Project/mgg-icons/src/mgg-icons.json'
 
 const randomIcon = () => {
-  const icons = Object.entries(mggIconsDictionary)
-  return icons[faker.random.number(icons.length - 1)][0]
+  const iconNames = Object.keys(mggIconsDictionary)
+  return faker.random.arrayElement(iconNames)
 }
 
 export default randomIcon
