64ecb10941265c2e0ffb1a38ed0bb49eade3d3dd
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-10-26 15:05:40 +0100

    Adds datalist on backoffice

--- src/react/Backoffice/Form/Input/InputText.stories.js
+++ src/react/Backoffice/Form/Input/InputText.stories.js
@@ -2,6 +2,9 @@ import React from 'react'
 import faker from 'faker'
 
 import InputText from '@Backoffice/Form/Input/InputText'
+import InputList, { InputListItem } from '@Backoffice/Form/Input/InputList'
+import InputPassword from '@Backoffice/Form/Input/InputPassword'
+import InputEmail from '@Backoffice/Form/Input/InputEmail'
 
 faker.locale = 'it'
 
@@ -21,3 +24,22 @@ export const noPlaceholder = () =>
 
 export const fieldFilled = () =>
   <InputText value="Filled content" label={faker.lorem.sentence()} placeholder={faker.phone.phoneNumber()} icon="operator"/>
+
+export const Email = () =>
+  <InputEmail label="Email" placeholder="Insert e-mail address..."/>
+
+export const Password = () =>
+  <InputPassword label="Password" placeholder="Insert password..."/>
+
+const cities = new Array(50)
+  .fill(null)
+  .map(() =>
+    faker.address.city(),
+  ).sort()
+
+export const List = () =>
+  <InputList label="Città" placeholder="Inerisci il nome di una città...">
+    {cities.map((city, index) =>
+      <InputListItem value={city}/>,
+    )}
+  </InputList>
