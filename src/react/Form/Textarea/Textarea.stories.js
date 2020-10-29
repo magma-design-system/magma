08f097115ec340c2fae4d9bb30aec12d69676792
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-10-29 14:13:09 +0100

    Aligns textarea to other form inputs

--- src/react/Form/Textarea/Textarea.stories.js
+++ src/react/Form/Textarea/Textarea.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import faker from 'faker'
-
+import randomIcon from '@Design/Icon/faker'
 import Textarea from '@Form/Textarea/Textarea'
 faker.locale = 'it'
 
@@ -16,4 +16,4 @@ export const noLabel = () =>
   <Textarea placeholder={faker.company.catchPhrase()}></Textarea>
 
 export const customIcon = () =>
-  <Textarea label={faker.lorem.sentence()} placeholder={faker.company.catchPhrase()} icon="operator"></Textarea>
+  <Textarea label={faker.lorem.sentence()} placeholder={faker.company.catchPhrase()} icon={randomIcon()}></Textarea>
