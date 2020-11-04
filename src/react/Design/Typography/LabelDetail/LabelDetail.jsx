cf31ab190b67ed1c56f9501151986f551533409c
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-11-04 14:49:05 +0100

    Adds missing Label detail typography

---
+++ src/react/Design/Typography/LabelDetail/LabelDetail.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@Typography/Typography'
+
+const LabelParagraph = props =>
+  <Typography
+    className={`text-secondary text-secondary--label-detail ${props.className}`}
+    htmlTag={props.htmlTag}
+  >
+    {props.children}
+  </Typography>
+
+LabelParagraph.propTypes = {
+  className: PropTypes.string,
+  htmlTag: PropTypes.string,
+}
+
+LabelParagraph.defaultProps = {
+  className: '',
+  htmlTag: 'p',
+}
+
+export default LabelParagraph
