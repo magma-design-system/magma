ca3522b308c5ba3078a506c910bb13d5fbafd63b
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-11-06 12:58:11 +0100

    Adds Author component

---
+++ src/react/Content/Author/Author.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Avatar from '@Content/Avatar/Avatar'
+import Row from '@Layout/Row/Row'
+
+const Author = props =>
+  <Row className={`author ${props.className}`} {...props}>
+    <Avatar className="author__avatar" size={props.size} url={require('@Content/Avatar/avatar-example.jpeg')}/>
+    <div className="author__infos">
+      {props.children}
+    </div>
+  </Row>
+
+Author.propTypes = {
+  avatar: PropTypes.string,
+  className: PropTypes.string,
+  size: PropTypes.string,
+}
+
+Author.defaultProps = {
+  className: '',
+  size: 'normal',
+}
+
+export default Author
