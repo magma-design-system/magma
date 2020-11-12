74457efc67ac911d89aa3ad27e44ca53ebee12d5
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-11-12 11:44:18 +0100

    Adds condition to prevent null object and adds onClick event

--- src/react/Layout/Row/Row.jsx
+++ src/react/Layout/Row/Row.jsx
@@ -3,22 +3,25 @@ import PropTypes from 'prop-types'
 import './Row.scss'
 
 const Row = props =>
-  <div className={`row ${props.className} ${props.gutter !== '' ? 'row--gutter-' + props.gutter : ''} ${props.align !== '' ? 'row--align-' + props.align : ''} ${props.lastToRight ? 'row--last-to-right' : ''} `}>
+  <div onClick={props.onClick} className={`row ${props.className} ${props.gutter !== '' ? 'row--gutter-' + props.gutter : ''} ${props.align !== '' ? 'row--align-' + props.align : ''} ${props.lastToRight ? 'row--last-to-right' : ''} `}>
     {
       Children.map(props.children, (child, index) => {
-        return cloneElement(child, {
-          key: index,
-          className: child.props.className ? `${child.props.className} row__item` : 'row__item',
-        })
+        if (child !== null) {
+          return cloneElement(child, {
+            key: index,
+            className: child.props.className ? `${child.props.className} row__item` : 'row__item',
+          })
+        }
       })
     }
   </div>
 
 Row.propTypes = {
   align: PropTypes.string,
-  lastToRight: PropTypes.bool,
   className: PropTypes.string,
   gutter: PropTypes.string,
+  lastToRight: PropTypes.bool,
+  onClick: PropTypes.func,
 }
 
 Row.defaultProps = {
