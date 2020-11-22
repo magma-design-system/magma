7b896b18ddc179a72b006d8ecd0bdd8ee244f2e6
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-11-22 18:54:15 +0100

    Updates grid naming convetion for templates

--- src/react/Design/Icon/Icon.jsx
+++ src/react/Design/Icon/Icon.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { appendSelectors, modifiers } from '@Library/styles'
 import './Icon.scss'
 
 import mggIconsDictionary from '+Project/mgg-icons/src/mgg-icons.json'
 const dictionary = Object.keys(mggIconsDictionary)
 
-const Icon = props => {
+const Icon = ({ name, onClick, size, ...restProps }) => {
+  const mainSelector = 'icon'
+  const localClassNames = appendSelectors([
+    mainSelector,
+    restProps.className,
+  ])
+
+  const modifierClassNames = modifiers(mainSelector, {
+    size,
+  })
+
   let icon = 'mgg-icons-status-warning'
   let notFound = true
-  if (dictionary.includes(props.name)) {
-    icon = `mgg-icons-${props.name}`
+  if (dictionary.includes(name)) {
+    icon = `mgg-icons-${name}`
     notFound = false
   }
-  return <i onClick={ () => props.onClick() } title={notFound ? `Warning, icon "${icon}" not found` : ''} className={`mgg-icons ${icon} icon ${(props.size ? 'icon--size-' + props.size : '')} ${props.className}`}></i>
+  return <i onClick={onClick} className={`mgg-icons ${icon} ${localClassNames} ${modifierClassNames}`} title={notFound ? `Warning, icon "${name}" not found` : ''}></i>
 }
 
 Icon.propTypes = {
   className: PropTypes.string,
-  family: PropTypes.string,
   name: PropTypes.string,
   onClick: PropTypes.func,
   size: PropTypes.string,
 }
 
 Icon.defaultProps = {
-  className: '',
-  family: 'material',
   name: 'missingIcon',
-  onClick: () => {},
-  size: '',
 }
 
 export default Icon
