9c1e035c49fb1cd2b0a03de22864bd8522078b01
Pascazi Stefano <stefano.pascazi@maggioli.it>
2020-11-24 12:50:38 +0100

    Test fix Row cloneElement

--- src/react/Layout/Row/Row.jsx
+++ src/react/Layout/Row/Row.jsx
@@ -17,15 +17,14 @@ const Row = ({ align, gutter, htmlTag, lastChild, onClick, ...restProps }) => {
     lastChild,
   })
 
-  const children = Children.map(restProps.children, (child, index) =>
-    cloneElement(child, {
+  const updateProps = Children.map(restProps.children, (child, index) => {
+    return cloneElement(child, {
       key: index,
       className: child.props.className ? `${child.props.className} row__item` : 'row__item',
-    }))
+    }, null)
+  })
 
-  return <HtmlTag {...restProps} onClick={onClick} className={`${localClassNames} ${modifierClassNames}`}>
-    {children}
-  </HtmlTag>
+  return <HtmlTag {...restProps} onClick={onClick} className={`${localClassNames} ${modifierClassNames}`}>{updateProps}</HtmlTag>
 }
 
 Row.propTypes = {
