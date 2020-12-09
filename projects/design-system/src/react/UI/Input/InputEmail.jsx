9032bdf94dcf52e5da2808c29c23877f0b54cf6f
Vittorio Vittori <vittorio.vittori@maggioli.it>
2020-12-09 17:42:37 +0100

    refactor(design-system): component props are now better splitted

--- projects/design-system/src/react/UI/Input/InputEmail.jsx
+++ projects/design-system/src/react/UI/Input/InputEmail.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Input from '@UI/Input/Input'
-import { capitalize } from '@UI/Select/functions'
 
-const InputEmail = props =>
-  <Input {...props}>
+const InputEmail = ({ autoComplete, font, name, placeholder, value, ...restProps }) => {
+  return <Input {...restProps}>
     <input
-      autoComplete={props.autoComplete}
-      className={`input__field ${props.font}`}
-      defaultValue={props.value}
-      name={props.name}
-      onBlur={e => { console.log(e) }}
-      placeholder={capitalize(props.placeholder)}
+      autoComplete={autoComplete}
+      className={`input__field ${font}`}
+      defaultValue={value}
+      name={name}
+      placeholder={placeholder}
       type="email"
     />
   </Input>
+}
 
 InputEmail.propTypes = {
+  ...Input.propTypes,
   autoComplete: PropTypes.string,
   font: PropTypes.string,
-  icon: PropTypes.string,
-  iconClassName: PropTypes.string,
   name: PropTypes.string,
   placeholder: PropTypes.string,
   isValid: PropTypes.bool,
@@ -34,7 +32,7 @@ InputEmail.defaultProps = {
   icon: 'document-email',
   iconClassName: '',
   name: 'email',
-  placeholder: 'your@email.com',
+  placeholder: 'tua@email.com',
   isValid: false,
   validate: true,
   value: '',
