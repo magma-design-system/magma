7a6c54039ac947f6e287445452a78827501b548c
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-01-11 16:57:33 +0100

    feat(design-system): add Tab component

---
+++ projects/design-system/src/react/UI/Tab/Tab.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { styles } from '@Library/styles'
+import Row from '@Layout/Row/Row'
+import Button from '@UI/Button/Button'
+import './Tab.scss'
+
+const TabItem = ({ active, borderRadius, className, ...restProps }) => {
+  const classes = styles('tab__item', {
+    selectors: [
+      className,
+      active ? 'background-color-brand-maggioli-05 color-adjust-tone-20' : 'background-color-adjust-tone-19 background-color-adjust-tone-20:hover box-shadow-box:hover color-adjust-tone-04',
+    ],
+    scaffolded: {
+      borderRadius,
+    },
+  })
+
+  return <Button className={classes} {...restProps}>
+    {restProps.children}
+  </Button>
+}
+
+TabItem.propTypes = {
+  ...Button.propTypes,
+  ...Row.propTypes,
+  active: PropTypes.bool,
+}
+
+TabItem.defaultProps = {
+  active: false,
+  borderRadius: 'large',
+}
+
+const Tab = ({ borderRadius, className, padding, ...restProps }) => {
+  const classes = styles('tab', {
+    selectors: [
+      className,
+    ],
+    scaffolded: {
+      borderRadius,
+      padding,
+    },
+  })
+
+  return <Row className={classes} {...restProps}>
+    {restProps.children}
+  </Row>
+}
+
+Tab.propTypes = {
+  ...Row.propTypes,
+  borderRadius: PropTypes.string,
+  current: PropTypes.number,
+  padding: PropTypes.string,
+}
+
+Tab.defaultProps = {
+  className: 'background-color-adjust-tone-19',
+  gutter: 'xsmall',
+  padding: 'xsmall',
+  borderRadius: 'xlarge',
+}
+
+export default Tab
+export {
+  TabItem,
+}
---
+++ projects/design-system/src/react/UI/Tab/Tab.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import faker from 'faker'
+
+import Tab, { TabItem } from '@UI/Tab/Tab'
+
+faker.locale = 'it'
+
+export default {
+  title: 'UI/Tab',
+  component: Tab,
+}
+
+export const basicUsage = () =>
+  <Tab current={2}>
+    <TabItem active>{faker.lorem.word()}</TabItem>
+    <TabItem>{faker.lorem.word()}</TabItem>
+    <TabItem>{faker.lorem.word()}</TabItem>
+  </Tab>
