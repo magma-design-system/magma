a93626441641d80a577d802af0ebc0fd1f2c06c5
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-01-12 17:06:48 +0100

    fix(design-system): change new utility selectors into react

--- projects/design-system/src/lib/styles.js
+++ projects/design-system/src/lib/styles.js
@@ -1,14 +1,28 @@
+import config from '+Tokens/css-tokens/config.json'
+
 const toDashCase = value =>
   value[0].toLowerCase() + value.slice(1, value.length).replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`)
 
 export const componentSelectors = items =>
   items.filter(value => !!value).join(' ')
 
+const selectorName = key => {
+  let selector = ''
+  config.scaffold.properties.forEach(element => {
+    if (element.indexOf(toDashCase(key)) > -1) {
+      if (element.split(':')[1] === toDashCase(key)) {
+        selector = element.split(':')[0]
+      }
+    }
+  })
+  return selector
+}
+
 export const scaffoldedSelectors = items => {
   const selectors = []
   for (const [key, value] of Object.entries(items)) {
     if (value) {
-      selectors.push(`${toDashCase(key)}-${value}`)
+      selectors.push(`${selectorName(key)}-${value}`)
     }
   }
   return componentSelectors(selectors)
