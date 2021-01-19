386376a524e24c439515d3894a78a10ac1dd09d9
Gioele Masini <gioele.masini@maggioli.it>
2021-01-19 11:36:57 +0100

    feat(mgg-icons)!: update font-icons-cli and change package structure
    Now is available the build of the typescript file mgg-icons.ts.
    
    BREAKING CHANGES: the package has no more the dist folder

---
+++ projects/mgg-icons/tools/setup-package-font.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// __dirname with "type: module" or .mjs file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// This file is used by build system to build a clean npm package with the compiled js files in the root of the package.
+// It will not be included in the npm package.
+// source: https://stackoverflow.com/a/52177090/3687018
+
+const ROOT_DIR = __dirname + "/..";
+const DIST_DIR = ROOT_DIR + "/dist";
+
+function main() {
+    const source = fs.readFileSync(join(ROOT_DIR, "/package.json")).toString('utf-8');
+    const sourceObj = JSON.parse(source);
+
+    // Package.json editing
+    sourceObj.scripts = {};
+    sourceObj.devDependencies = {};
+    if (sourceObj.main.startsWith('dist/')) {
+        sourceObj.main = sourceObj.main.slice(5);
+    }
+    sourceObj.files = sourceObj.files.map(file => file.startsWith('/dist') ? file.slice(5) : file);
+
+    // Output
+    if (!fs.existsSync(DIST_DIR)) {
+      fs.mkdirSync(DIST_DIR);
+    }
+
+    fs.writeFileSync(join(DIST_DIR, '/package.json'), Buffer.from(JSON.stringify(sourceObj, null, 2)) );
+    // fs.writeFileSync(join(DIST_DIR, '/version.txt'), Buffer.from(sourceObj.version));
+
+    // fs.copyFileSync(join(ROOT_DIR, '/.npmignore'), join(DIST_DIR, '/.npmignore'));
+
+    const resourcesDir = join(DIST_DIR, 'resources')
+    if (!fs.existsSync(resourcesDir)) {
+      fs.mkdirSync(resourcesDir);
+    }
+    fs.copyFileSync(join(ROOT_DIR, 'resources', 'mgg-icons.json'), join(resourcesDir, 'mgg-icons.json'));
+}
+
+main();
