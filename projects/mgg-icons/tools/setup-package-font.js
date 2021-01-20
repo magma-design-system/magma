37534c95e10810154f4ed3f9a4ed291f1e87d110
Gioele Masini <gioele.masini@maggioli.it>
2021-01-20 16:54:35 +0100

    feat(mgg-icons)!: Move all no-font files out of fonts directory

--- projects/mgg-icons/tools/setup-package-font.js
+++ projects/mgg-icons/tools/setup-package-font.js
@@ -15,33 +15,42 @@ const ROOT_DIR = __dirname + "/..";
 const DIST_DIR = ROOT_DIR + "/dist";
 
 function main() {
-    const source = fs.readFileSync(join(ROOT_DIR, "/package.json")).toString('utf-8');
-    const sourceObj = JSON.parse(source);
+  const source = fs.readFileSync(join(ROOT_DIR, "/package.json")).toString('utf-8');
+  const sourceObj = JSON.parse(source);
 
-    // Package.json editing
-    sourceObj.scripts = {};
-    sourceObj.devDependencies = {};
-    if (sourceObj.main.startsWith('dist/')) {
-        sourceObj.main = sourceObj.main.slice(5);
-    }
-    sourceObj.files = sourceObj.files.map(file => file.startsWith('/dist') ? file.slice(5) : file);
+  // Package.json editing
+  sourceObj.scripts = {};
+  sourceObj.devDependencies = {};
+  if (sourceObj.main.startsWith('dist/')) {
+    sourceObj.main = sourceObj.main.slice(5);
+  }
+  sourceObj.files = sourceObj.files.map(file => file.startsWith('/dist') ? file.slice(5) : file);
 
-    // Output
-    if (!fs.existsSync(DIST_DIR)) {
-      fs.mkdirSync(DIST_DIR);
-    }
+  // Output folder
+  if (!fs.existsSync(DIST_DIR)) {
+    fs.mkdirSync(DIST_DIR);
+  }
 
-    fs.writeFileSync(join(DIST_DIR, '/package.json'), Buffer.from(JSON.stringify(sourceObj, null, 2)) );
-    // fs.writeFileSync(join(DIST_DIR, '/version.txt'), Buffer.from(sourceObj.version));
-    fs.copyFileSync(join(ROOT_DIR, '.npmrc'), join(DIST_DIR, '.npmrc') );
+  fs.writeFileSync(join(DIST_DIR, '/package.json'), Buffer.from(JSON.stringify(sourceObj, null, 2)) );
+  // fs.writeFileSync(join(DIST_DIR, '/version.txt'), Buffer.from(sourceObj.version));
+  fs.copyFileSync(join(ROOT_DIR, '.npmrc'), join(DIST_DIR, '.npmrc') );
 
-    // fs.copyFileSync(join(ROOT_DIR, '/.npmignore'), join(DIST_DIR, '/.npmignore'));
+  // fs.copyFileSync(join(ROOT_DIR, '/.npmignore'), join(DIST_DIR, '/.npmignore'));
 
-    const resourcesDir = join(DIST_DIR, 'resources')
-    if (!fs.existsSync(resourcesDir)) {
-      fs.mkdirSync(resourcesDir);
-    }
-    fs.copyFileSync(join(ROOT_DIR, 'resources', 'mgg-icons.json'), join(resourcesDir, 'mgg-icons.json'));
+  const resourcesDir = join(DIST_DIR, 'resources');
+  if (!fs.existsSync(resourcesDir)) {
+    fs.mkdirSync(resourcesDir);
+  }
+  fs.copyFileSync(join(ROOT_DIR, 'resources', 'mgg-icons.json'), join(resourcesDir, 'mgg-icons.json'));
+
+  // Moving out of "fonts" folder files witch aren't fonts
+  const fontsDir = join(DIST_DIR, 'fonts');
+  const isFontFile = /\.(ttf|woff|woff2|eot|svg)$/i;
+  const files = fs.readdirSync(fontsDir).filter(filename => !isFontFile.test(filename));
+  for (const file of files) {
+    console.log(file);
+    fs.renameSync(join(fontsDir, file), join(DIST_DIR, file));
+  }
 }
 
 main();
