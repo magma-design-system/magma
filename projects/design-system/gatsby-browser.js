ef898fe06714d7e4e12a481da5801a107efdf720
Gioele Masini <gioele.masini@maggioli.it>
2021-01-22 18:01:48 +0100

    feat(design-system): update mgg-icons 1.0.0-RC.3

--- projects/design-system/gatsby-browser.js
+++ projects/design-system/gatsby-browser.js
@@ -1,5 +1,5 @@
 import './node_modules/normalize.css/normalize.css'
-import './node_modules/@maggioli-common/mgg-icons/dist/mgg-icons.css'
+import './node_modules/@maggioli-common/mgg-icons/mgg-icons.css'
 import './node_modules/typeface-karla/index.css'
 import './node_modules/typeface-roboto/index.css'
 import './src/scss/import-global.scss'
