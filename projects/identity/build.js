50f7d90a465206a8c2135609d0f915c5edfca88e
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-02-15 15:31:02 +0100

    feat(identity): add possibility to generate PNG logo versions dynamically

---
+++ projects/identity/build.js
@@ -0,0 +1,79 @@
+const fs = require('fs')
+const path = require('path')
+const sharp = require('sharp')
+const shell = require('shelljs')
+
+const directoryPath = path.join(__dirname, 'src')
+
+var walk = function(dir, done) {
+  var results = []
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err)
+    var pending = list.length
+    if (!pending) return done(null, results)
+    list.forEach(function(file) {
+      file = path.resolve(dir, file)
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res)
+            if (!--pending) done(null, results)
+          })
+        } else {
+          results.push(file)
+          if (!--pending) done(null, results)
+        }
+      })
+    })
+  })
+}
+
+function removeDuplicates(arr) {
+  let s = new Set(arr)
+  let it = s.values()
+  return Array.from(it)
+}
+
+function exportImage(item, size) {
+  sharp(path.join(__dirname, `src/${item}.svg`))
+    .resize(size)
+    .png()
+    .toFile(path.join(__dirname, `dist/${item}-${size}w.png`))
+    .then(function(info) {
+      console.log(info)
+    })
+    .catch(function(err) {
+      console.error(err)
+    })
+}
+
+walk(directoryPath, function (err, results) {
+  if (err) {
+    console.error(err)
+  }
+
+  const cleanResults = []
+
+  results.forEach((item, index) => {
+    if (item.indexOf('.svg') != -1) {
+      cleanResults.push(item.split('src/')[1])
+    }
+  })
+
+  let paths = []
+  cleanResults.forEach((item, index) => {
+    paths.push(item.split('/')[0])
+  })
+
+  paths = removeDuplicates(paths)
+  paths.forEach((path, index) => {
+    shell.mkdir('-p', `dist/${path}`);
+  })
+
+  cleanResults.forEach((item, index) => {
+    item = item.replace('.svg', '')
+    shell.cp('-R', 'src/*', 'dist')
+    exportImage(item, 512)
+    exportImage(item, 1024)
+  })
+})
