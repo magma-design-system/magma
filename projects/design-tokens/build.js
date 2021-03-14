485a93ae8724999447eea61199f8a0b030d57758
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-03-14 16:26:10 +0100

    feat(design-tokens): add colors palette for tailwind

--- projects/design-tokens/build.js
+++ projects/design-tokens/build.js
@@ -1,3 +1,3 @@
-const StyleDictionary = require('./formats/scss-map/scss-map')
-  .extend('./config.json')
+let StyleDictionary = require('./formats/scss-map/scss-map').extend('./config.json')
+StyleDictionary = require('./formats/js-config/js-config').extend('./config.json')
 StyleDictionary.buildAllPlatforms()
