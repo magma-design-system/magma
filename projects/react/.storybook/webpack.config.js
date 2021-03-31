0dc1d6ecacfea13f8c7078a3717f57fd3a896574
Vittorio Vittori <vittori.vittorio@maggioli.it>
2021-03-31 17:30:17 +0200

    build(design-system): updates dependencies

--- projects/react/.storybook/webpack.config.js
+++ projects/react/.storybook/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = async({ config, mode }) => {
               'autoprefixer',
             ],
           ],
-        },c
+        },
       },
     }, {
       loader: 'sass-loader',
