f6a7b8bc96625bf4354b7efcccc3a0d4e1957172
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-04-08 17:39:15 +0200

    debug

--- projects/react/.storybook/webpack.config.js
+++ projects/react/.storybook/webpack.config.js
@@ -6,12 +6,11 @@ const webpack = require('webpack')
 
 module.exports = async({ config }) => {
   // config.mode = 'production'
-
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-  )
+  // config.plugins.push(
+  //   new webpack.DefinePlugin({
+  //     'process.env.NODE_ENV': JSON.stringify('production'),
+  //   }),
+  // )
 
   config.module.rules.push({
     test: /\.s?css$/,
