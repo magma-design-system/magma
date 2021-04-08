06c7d3ebd839309ff41b8c1be2eebb3ebb027f50
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-04-08 17:25:17 +0200

    debug

--- projects/react/src/Design/Icon/faker.js
+++ projects/react/src/Design/Icon/faker.js
@@ -1,5 +1,5 @@
 import faker from 'faker'
-import mggIconsDictionary from '@maggioli-design-system/icons/mgg-icons.json'
+import mggIconsDictionary from '@maggioli-design-system/icons/resources/mgg-icons.json'
 
 const randomIcon = () => {
   const iconNames = Object.keys(mggIconsDictionary)
