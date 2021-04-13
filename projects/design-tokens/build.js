5a5ac501f3fd38bb488704ebad1f8e54014d6b4e
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-04-13 16:13:16 +0200

    change(design-tokens): add js config for media query rules exported

--- projects/design-tokens/build.js
+++ projects/design-tokens/build.js
@@ -1,5 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+const beautify = require('js-beautify').js
+
 let StyleDictionary = require('./formats/scss-map/scss-map').extend('./config.json')
 StyleDictionary = require('./formats/js-module/js-module').extend('./config.json')
 StyleDictionary = require('./formats/css-vars/css-vars').extend('./config.json')
 StyleDictionary = require('./formats/css-vars-tailwind/css-vars-tailwind').extend('./config.json')
 StyleDictionary.buildAllPlatforms()
+
+const beautifyConfig = {
+  indent_size: 2,
+  space_in_empty_paren: true
+}
+
+fs.readFile(path.resolve(__dirname, 'css-tokens/media.json'), (err, data) => {
+  if (err) throw err;
+  let media = JSON.parse(data);
+
+  const jsData = `
+  const media = ${JSON.stringify(media, null, 4)}
+  module.exports = {
+    media,
+  }
+  `
+
+  fs.writeFile(path.resolve(__dirname, 'dist/js/media.js'), beautify(jsData, beautifyConfig), function (err) {
+    if (err) return console.log(err);
+    console.log('CSS: media.js exported successfully.');
+  })
+});
