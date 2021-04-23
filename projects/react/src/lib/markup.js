6f7b672630ab2525256a9003a9cbfa579c09c850
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-04-23 08:40:28 +0200

    fix(react): fix id for non string values

--- projects/react/src/lib/markup.js
+++ projects/react/src/lib/markup.js
@@ -1,12 +1,14 @@
 export const id = (id, value, length) => {
-  if (id) {
-    if (typeof id === 'boolean') {
-      const maxLength = length ? length : 1000
-      const fullId = value.replace(/(\W{1,})/gm, '-').replace(/(-)$/gm, '').toLowerCase()
-      const shortId = fullId.split('-').slice(0, fullId.length >= maxLength ? maxLength : fullId.length)
-      return shortId.join('-')
+  if (value) {
+    if (id) {
+      if (typeof id === 'boolean') {
+        const maxLength = length || 1000
+        const fullId = value.toString().replace(/(\W{1,})/gm, '-').replace(/(-)$/gm, '').toLowerCase()
+        const shortId = fullId.split('-').slice(0, fullId.length >= maxLength ? maxLength : fullId.length)
+        return shortId.join('-')
+      }
+      return id.toString().replace(/(\W{1,})/gm, '-').replace(/(-)$/gm, '').toLowerCase()
     }
-    return id.replace(/(\W{1,})/gm, '-').replace(/(-)$/gm, '').toLowerCase()
   }
   return undefined
 }
