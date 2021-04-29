250051e9c07408e3637105b3aaf4b2b712fb40fa
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-04-29 09:43:16 +0200

    build(docs): remove duplicated config

--- projects/docs/tailwind.config.js
+++ projects/docs/tailwind.config.js
@@ -1,16 +1,9 @@
 const config = require('@maggioli-design-system/styles/tailwind.config')
-config.mode = 'jit'
+
 config.purge.content = [
   './src/**/*.{js,jsx}',
   '../../node_modules/@maggioli-design-system/react/**/*.{js,jsx}',
 ]
-// config.theme.extend.padding = {
-//   '1/1': '100%',
-//   '3/2': '66.67%',
-//   '4/3': '75%',
-//   '5/4': '80%',
-//   '16/9': '56.25%',
-// }
 
 config.variants.extend = {
   backgroundColor: ['group-hover'],
