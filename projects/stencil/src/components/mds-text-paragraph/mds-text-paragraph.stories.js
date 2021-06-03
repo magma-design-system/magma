5a4f7acb547c4b0b619ac31acf71728d23b1246a
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-06-03 17:32:58 +0200

    fix(stencil): fix wrong scss files path

---
+++ projects/stencil/src/components/mds-text-paragraph/mds-text-paragraph.stories.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import faker from 'faker'
+import MdsTextParagraph from '../../../dist/collection/components/mds-text-paragraph/mds-text-paragraph';
+
+export default {
+  title: 'MdsTextParagraph',
+  component: MdsTextParagraph,
+  argTypes: {
+    label: { type: 'text', description: 'The text which is shown as label' },
+    name: {
+      type: 'text',
+      description:
+        'Is needed to reference the form data after the form is submitted',
+    },
+    disabled: {
+      type: 'boolean',
+      description: 'If true, the button is displayed as disabled',
+      defaultValue: { summary: false },
+    },
+  },
+};
+
+const defaultArgs = {
+  disabled: false,
+};
+
+const Template = args => {
+  return <mds-text-paragraph class="p-2 bg-adjust-tone-18" {...args}>
+    {faker.lorem.paragraph()}
+  </mds-text-paragraph>
+}
+
+export const Default = Template.bind({});
+Default.args = { ...defaultArgs };
