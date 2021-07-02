95fcc26675010b9376421d8ff87480f63c588e27
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-07-02 20:50:50 +0200

    feat(stencil): add input component draft

--- projects/stencil/src/components/mds-input/mds-input.stories.js
+++ projects/stencil/src/components/mds-input/mds-input.stories.js
@@ -6,21 +6,19 @@ export default {
   title: 'Form / Input',
   component: MdsInput,
   argTypes: {
-    label: { type: 'text', description: 'The text which is shown as label' },
     name: {
       type: 'text',
       description:
         'Is needed to reference the form data after the form is submitted',
+      defaultValue: 'text',
     },
     disabled: {
       type: 'boolean',
       description: 'If true, the button is displayed as disabled',
-      defaultValue: { summary: false },
+      defaultValue: false,
     },
   },
 }
 
 export const Default = args =>
-  <mds-input {...args}>
-    {faker.lorem.paragraph()}
-  </mds-input>
+  <mds-input {...args} value={faker.lorem.paragraph()}></mds-input>
--- projects/stencil/src/components/mds-input/mds-input.tsx
+++ projects/stencil/src/components/mds-input/mds-input.tsx
@@ -1,22 +1,154 @@
-import { Component, Host, h, Prop } from '@stencil/core';
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  Host,
+  Method,
+  Prop,
+  State,
+  Watch,
+  h,
+} from '@stencil/core';
+
+import { AutocompleteTypes, TextFieldTypes } from './interface';
+export interface InputChangeEventDetail {
+  value: string | number | undefined | null;
+}
 
 @Component({
   tag: 'mds-input',
   styleUrl: 'mds-input.css',
   shadow: true,
 })
+
 export class MdsInput {
 
-  @Prop() label?: string;
-  @Prop() type?: string = 'text';
+  private nativeInput?: HTMLInputElement;
+  private tabindex?: string | number;
+
+  @Element() el!: HTMLElement;
+
+  @State() hasFocus = false;
+
+  // https://www.w3schools.com/tags/tag_input.asp
+  @Prop() autocomplete: AutocompleteTypes = 'off';
+  @Prop() autofocus?: boolean = false;
+  @Prop() disabled?: boolean = false;
+  @Prop() max?: string;
+  @Prop() maxlength?: number;
+  @Prop() min?: string;
+  @Prop() minlength?: number;
+  @Prop() name?: string;
+  @Prop() pattern?: string;
+  @Prop() placeholder?: string;
+  @Prop() readonly?: boolean = false;
+  @Prop() required?: boolean = false;
+  @Prop() spellcheck?: boolean = false;
+  @Prop() step?: string;
+  @Prop() type: TextFieldTypes = 'text';
+  @Prop({ mutable: true }) value?: string | number | null = '';
+
+  @Event() changeEvent!: EventEmitter<InputChangeEventDetail>;
+  @Event() keyDownEvent!: EventEmitter<KeyboardEvent>;
+  @Event() blurEvent!: EventEmitter<void>;
+  @Event() focusEvent!: EventEmitter<void>;
+
+  componentWillLoad() {
+    // If the mds-input has a tabindex attribute we get the value
+    // and pass it down to the native input, then remove it from the
+    // mds-input to avoid causing tabbing twice on the same element
+    if (this.el.hasAttribute('tabindex')) {
+      const tabindex = this.el.getAttribute('tabindex');
+      this.tabindex = tabindex !== null ? tabindex : undefined;
+      this.el.removeAttribute('tabindex');
+    }
+  }
+
+  @Watch('value')
+  protected valueChanged() {
+    this.changeEvent.emit({ value: this.value == null ? this.value : this.value.toString() });
+  }
+
+  /**
+   * Sets focus on the specified `my-input`.
+   * Use this method instead
+   * of the global `input.focus()`.
+   */
+  @Method()
+  async setFocus() {
+    if (this.nativeInput) {
+      this.nativeInput.focus();
+    }
+  }
+
+  /**
+   * Returns the native `<input>` element used under the hood.
+   */
+  @Method()
+  getInputElement(): Promise<HTMLInputElement> {
+    return Promise.resolve(this.nativeInput!);
+  }
+
+  private getValue(): string {
+    return typeof this.value === 'number' ? this.value.toString() : (this.value || '').toString();
+  }
+
+  private onInput = (ev: Event) => {
+    const input = ev.target as HTMLInputElement | null;
+    if (input) {
+      this.value = input.value || '';
+    }
+    this.keyDownEvent.emit(ev as KeyboardEvent);
+  };
+
+  private hasValue(): boolean {
+    return this.getValue().length > 0;
+  }
+
+  private onBlur = () => {
+    this.hasFocus = false;
+    this.blurEvent.emit();
+  };
+
+  private onFocus = () => {
+    this.hasFocus = true;
+    this.focusEvent.emit();
+  }
 
   render() {
+    const value = this.getValue();
+
     return (
-      <Host>
-        { this.label && <mds-h1>{this.label}</mds-h1> }
-        <slot></slot>
+      <Host aria-disabled={this.disabled ? 'true' : null}
+        class={{
+          'has-value': this.hasValue(),
+          'has-focus': this.hasFocus,
+        }}>
+        <input
+          ref={(input) => (this.nativeInput = input)}
+          disabled={this.disabled}
+          autoComplete={this.autocomplete}
+          autoFocus={this.autofocus}
+          min={this.min}
+          max={this.max}
+          minLength={this.minlength}
+          maxLength={this.maxlength}
+          name={this.name}
+          pattern={this.pattern}
+          placeholder={this.placeholder || ''}
+          readOnly={this.readonly}
+          required={this.required}
+          spellcheck={this.spellcheck ? 'true' : undefined}
+          step={this.step}
+          tabindex={this.tabindex}
+          type={this.type}
+          value={value}
+          onInput={this.onInput}
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+        />
       </Host>
     );
   }
-
 }
