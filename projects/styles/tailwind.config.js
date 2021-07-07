9b321b8aff80b0d802b2db3d4dd8775d8dfc2863
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-07-07 17:47:53 +0200

    style(styles): add new box shadow style

--- projects/styles/tailwind.config.js
+++ projects/styles/tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
       ...ease,
     },
     extend: {
+      boxShadow: {
+        'lg-sharp': '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      },
       fontSize: {
         xxs: '0.625rem',
       },
