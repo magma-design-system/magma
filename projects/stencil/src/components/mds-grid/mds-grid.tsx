e4679ab5514e7987ee7ab39e0a3ca2b8b5f87155
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-07-14 15:45:15 +0200

    refact(stencil): moves storybook stories into compontnt's test folder

---
+++ projects/stencil/src/components/mds-grid/mds-grid.tsx
@@ -0,0 +1,17 @@
+import { Component, Host, h } from '@stencil/core'
+
+@Component({
+  tag: 'mds-grid',
+  styleUrl: 'mds-grid.css',
+  shadow: true,
+})
+export class MdsGrid {
+
+  render() {
+    return (
+      <Host>
+        <slot></slot>
+      </Host>
+    )
+  }
+}
---
+++ projects/stencil/src/components/mds-grid/test/mds-grid.spec.tsx
@@ -0,0 +1,18 @@
+import { newSpecPage } from '@stencil/core/testing'
+import { MdsGrid } from '../mds-grid'
+
+describe('mds-grid', () => {
+  it('renders', async() => {
+    const page = await newSpecPage({
+      components: [MdsGrid],
+      html: '<mds-grid></mds-grid>',
+    })
+    expect(page.root).toEqualHtml(`
+      <mds-grid>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </mds-grid>
+    `)
+  })
+})
