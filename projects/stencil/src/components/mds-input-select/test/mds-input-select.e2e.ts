5025d06d143fecd1098de07e12f9a016463ba049
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-07-30 17:28:59 +0200

    feat(stencil): add textarea to input

---
+++ projects/stencil/src/components/mds-input-select/test/mds-input-select.e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('mds-input-select', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<mds-input-select></mds-input-select>');
+
+    const element = await page.find('mds-input-select');
+    expect(element).toHaveClass('hydrated');
+  });
+});
---
+++ projects/stencil/src/components/mds-input-select/test/mds-input-select.spec.tsx
@@ -0,0 +1,18 @@
+import { newSpecPage } from '@stencil/core/testing'
+import { MdsInputSelect } from '../mds-input-select'
+
+describe('mds-input-select', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [MdsInputSelect],
+      html: '<mds-input-select></mds-input-select>',
+    })
+    expect(page.root).toEqualHtml(`
+      <mds-input-select>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </mds-input-select>
+    `)
+  })
+})
