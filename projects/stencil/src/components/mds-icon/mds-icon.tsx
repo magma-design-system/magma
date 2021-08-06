28b9187c1fcf8674e547dcc85a99290829a33e82
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-08-06 12:29:25 +0200

    fix(stencil): fix some style

--- projects/stencil/src/components/mds-icon/mds-icon.tsx
+++ projects/stencil/src/components/mds-icon/mds-icon.tsx
@@ -13,6 +13,12 @@ export class MdsIcon {
    */
   @Prop() readonly name?: string
 
+  /**
+   * The name of the icon set.
+   * The icon set is strictly realted to @maggioli-design-system/icons
+   */
+  @Prop() readonly size?: string
+
   render () {
     return (
       <Host>
