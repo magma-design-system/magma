65d10db079015d5a5bbfe8e104a1e60aee9b8827
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-08-16 17:59:41 +0200

    refact(stencil): lint code

--- projects/stencil/src/components/mds-input-field/test/mds-input-field.e2e.ts
+++ projects/stencil/src/components/mds-input-field/test/mds-input-field.e2e.ts
@@ -1,11 +1,11 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage } from '@stencil/core/testing'
 
 describe('mds-input-field', () => {
   it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<mds-input-field></mds-input-field>');
+    const page = await newE2EPage()
+    await page.setContent('<mds-input-field></mds-input-field>')
 
-    const element = await page.find('mds-input-field');
-    expect(element).toHaveClass('hydrated');
-  });
-});
+    const element = await page.find('mds-input-field')
+    expect(element).toHaveClass('hydrated')
+  })
+})
