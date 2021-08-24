bde59a619304910bc5f61cd889e356c3626af7ea
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-08-24 11:05:24 +0200

    style(styles): add sharp small shadow

--- projects/styles/tailwind.config.js
+++ projects/styles/tailwind.config.js
@@ -36,6 +36,7 @@ module.exports = {
         'sharp-lg': '0 0 1px 1px rgba(0, 0, 0, 0.06), 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
         'sharp-md': '0 0 1px 1px rgba(0, 0, 0, 0.06), 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
         sharp: '0 0 1px 1px rgba(0, 0, 0, 0.06), 0 1px 3px 0 rgba(0, 0, 0, 0.1)',
+        'sharp-sm': '0 0 1px 1px rgba(0, 0, 0, 0.06), 0 1px 2px 0 rgba(0, 0, 0, 0.05)',
       },
       fontSize: {
         xxs: '0.625rem',
