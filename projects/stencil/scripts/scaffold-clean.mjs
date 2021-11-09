06f0bef5b1db0285e1856f273293a38ddf881b13
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-11-09 12:51:26 +0100

    fix(stencil): removes unused code

--- projects/stencil/scripts/scaffold-clean.mjs
+++ projects/stencil/scripts/scaffold-clean.mjs
@@ -1,12 +1,19 @@
 import { ask } from 'stdio';
-import Handlebars from 'handlebars'
-import { readFile, copyFile, rm } from 'fs/promises'
-import fs from 'fs'
+import { rm } from 'fs/promises'
 const PROJECT_PATH = '..'
 const COMPONENTS_PATH = `${PROJECT_PATH}/src/components`
 
 async function main () {
   console.log('This script will DELETE the stencil and package.json global configuration from a specific component.')
+  const continueTask = await ask('Continue?', { options: ['y', 'n', ''] });
+
+  if (continueTask === 'n') {
+    return
+  }
+  cleanComponent()
+}
+
+async function cleanComponent () {
   const componentName = await ask('Component name')
   const keepPackage = await ask('Keep package.json file? (leave empty to confirm)', {
     options: ['', 'y', 'n'],
@@ -37,5 +44,5 @@ async function main () {
 }
 
 main()
-  .then(() => console.log('Component cleaned.'))
+  .then(() => console.log('Process finished.'))
   .catch(error => console.warn(error))
