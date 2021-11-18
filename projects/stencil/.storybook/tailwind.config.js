c64c183c0009ddefe0145c33c38237791db61dda
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-11-18 18:43:37 +0100

    fix(stencil): lint storybook config

--- projects/stencil/.storybook/tailwind.config.js
+++ projects/stencil/.storybook/tailwind.config.js
@@ -1,8 +1,6 @@
 const config = require('@maggioli-design-system/styles/tailwind.config')
 const chalk = require('chalk')
 
-console.log(chalk.green('Storybook config / Tailwind'))
-
 config.purge.content = [
   './src/**/*.{stories.js,tsx}',
 ]
