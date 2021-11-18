2f4eb0b334dd4968cc49f16fe49655e07ec80fe3
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-11-18 16:54:27 +0100

    style(mds-card): change custom property names to improve naming convention

--- projects/stencil/src/components/mds-card/test/mds-card.stories.js
+++ projects/stencil/src/components/mds-card/test/mds-card.stories.js
@@ -17,7 +17,7 @@ const Template = args =>
 export const Default = Template.bind({})
 
 export const tailwindStyle = () => <mds-card class="gap-4 p-6 shadow-xl">
-  <mds-text class="bg-status-info-18 text-status-info-05 -mx-6 -mt-6 p-6">{faker.lorem.paragraph()}</mds-text>
+  <mds-text class="bg-status-info-09 text-status-info-03 -mx-6 -mt-6 p-6">{faker.lorem.paragraph()}</mds-text>
   <mds-text>{faker.lorem.paragraph()}</mds-text>
   <mds-text>{faker.lorem.paragraph()}</mds-text>
 </mds-card>
