8213aac83037ba2016002c4a3460bf13f9f3bca1
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-11-25 23:38:43 +0100

    build(stencil): remove useless console logs

--- projects/stencil/.storybook/postcss.config.js
+++ projects/stencil/.storybook/postcss.config.js
@@ -1,7 +1,3 @@
-const chalk = require('chalk')
-
-console.log(chalk.green('Storybook config / PostCSS'))
-
 module.exports = {
   plugins: [
     require('postcss-import'),
--- projects/stencil/.storybook/tailwind.config.js
+++ projects/stencil/.storybook/tailwind.config.js
@@ -1,5 +1,4 @@
 const config = require('@maggioli-design-system/styles/tailwind.config')
-const chalk = require('chalk')
 
 config.purge.content = [
   './src/**/*.{stories.js,tsx}',
