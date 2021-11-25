b628063ec09eacb5db3a56d7fe7438bf5af702d4
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-11-25 15:30:56 +0100

    refact(design-tokens): update adobe leonardo library

--- projects/design-tokens/leonardo.mjs
+++ projects/design-tokens/leonardo.mjs
@@ -1,48 +1,55 @@
-// based on https://leonardocolor.io/
+import { Theme, Color, BackgroundColor } from '@adobe/leonardo-contrast-colors'
+import { readFileSync } from 'fs'
+import chalk from 'chalk'
+import { writeFile, mkdir } from 'fs/promises'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-import { generateAdaptiveTheme } from '@adobe/leonardo-contrast-colors'
-import { readFileSync, writeFileSync, mkdirSync } from 'fs'
+const PROJECT_PATH = resolve(dirname(fileURLToPath(import.meta.url)), './')
+const COLOR_PATH = `${PROJECT_PATH}/properties/color`
 
 const colorsRawData = readFileSync('colors.json')
 const { colors, defaultColorspace, ratios } = JSON.parse(colorsRawData)
 
-const baseScale = 'adjust.tone'
+const output = 'HEX'
+const smooth = true
 
-const reverseRatios = ratios.reverse()
-const filteredRatios = (ratios, scaffold) => {
-  const newRatios = []
-  reverseRatios.forEach((ratioElement, ratioIdex) => {
-    scaffold.forEach(scaffoldElement => {
-      if (ratioIdex === scaffoldElement) {
-        newRatios.push(ratioElement)
-      }
-    })
+const getBackgroundColor = (colors, name) => {
+  let filteredColor = null
+  colors.forEach(color => {
+    if (color.name === name) {
+      filteredColor = new BackgroundColor({
+        colorKeys: [color.color],
+        colorSpace: color.colorSpace || defaultColorspace,
+        name: color.name,
+        output,
+        ratios,
+        smooth,
+      })
+    }
   })
 
-  return newRatios
+  return filteredColor
 }
 
-const addAdaptivePalette = (color, name, itemColorspace, itemScaffold) => {
-  const colorKeys = typeof color === 'string' ? [color] : color
-  const colorspace = itemColorspace !== undefined ? itemColorspace : defaultColorspace
-  const scaffold = itemScaffold !== undefined ? itemScaffold : false
-  const itemRatios = itemScaffold !== undefined ? filteredRatios(ratios, itemScaffold) : ratios
-  return {
-    name,
-    colorKeys,
-    colorspace,
-    ratios: itemRatios,
-    scaffold,
-  }
+const color = colorItem => {
+  return new Color({
+    colorKeys: [colorItem.color],
+    colorSpace: colorItem.colorSpace || defaultColorspace,
+    name: colorItem.name,
+    output,
+    ratios,
+    smooth,
+  })
 }
 
-const getPalette = (theme, colorName, colorValue, scaffold, colorDark) => {
+const formatColor = (theme, colorName, colorValue, scaffold, colorDark) => {
   const palette = {}
   theme.forEach(element => {
     if (Object.prototype.hasOwnProperty.call(element, 'name')) {
       if (element.name === colorName) {
         const paletteSource = element.values
-        paletteSource.forEach((element, index) => {
+        paletteSource.reverse().forEach((element, index) => {
           let codeIndex = 0
 
           if (scaffold) {
@@ -59,10 +66,10 @@ const getPalette = (theme, colorName, colorValue, scaffold, colorDark) => {
           palette[colorCode] = { value: element.value }
 
           if (paletteSource.length === index + 1) {
+            palette.color = { value: colorValue }
+
             if (colorDark !== undefined) {
               palette.color = { value: colorDark }
-            } else {
-              palette.color = { value: colorValue }
             }
           }
         })
@@ -72,22 +79,8 @@ const getPalette = (theme, colorName, colorValue, scaffold, colorDark) => {
   return palette
 }
 
-const colorScales = colors.map(item => addAdaptivePalette(item.color, item.name, item.colorspace, item.scaffold))
-
-if (colorScales.length > 0) {
-  let brightness = 100
-  const themeLight = generateAdaptiveTheme({
-    colorScales,
-    baseScale,
-    brightness,
-  })
-
-  brightness = 0
-  const themeDark = generateAdaptiveTheme({
-    colorScales,
-    baseScale,
-    brightness,
-  })
+const formatPalette = async opts => {
+  console.log('Formatting color palette to JSON format')
 
   const palette = {
     color: {},
@@ -100,25 +93,61 @@ if (colorScales.length > 0) {
     const name = element.name.split('.')[nameIndex]
 
     if (!Object.prototype.hasOwnProperty.call(palette.color, group)) {
+
+      console.log(`Creating color ${chalk.magenta('group')} ${group}`)
       palette.color[group] = {}
     }
 
     if (!Object.prototype.hasOwnProperty.call(palette.color[group], name)) {
+      console.log(`Creating color ${chalk.blue('name')} ${name}`)
       palette.color[group][name] = {
-        light: getPalette(themeLight, `${group}.${name}`, element.color, element.scaffold),
-        dark: getPalette(themeDark, `${group}.${name}`, element.color, element.scaffold, element.colorDark),
+        light: formatColor(opts.themeLight, `${group}.${name}`, element.color, element.scaffold),
+        dark: formatColor(opts.themeDark, `${group}.${name}`, element.color, element.scaffold, element.colorDark),
       }
     }
   })
 
   const jsonPalette = JSON.stringify(palette, null, 2)
 
-  mkdirSync('./properties/color', { recursive: true })
-  writeFileSync('./properties/color/base.json', jsonPalette, 'utf8', err => {
+  console.log('Exporting color palette')
+
+  await mkdir(new URL(`${COLOR_PATH}`, import.meta.url), { recursive: true })
+  await writeFile(new URL(`${COLOR_PATH}/base.json`, import.meta.url), jsonPalette, 'utf8', err => {
     if (err) {
-      console.log('An error occured while writing JSON Object to File.')
-      return console.log(err)
+      console.log(chalk.red('An error occured while writing JSON Object to File.'))
+      console.log(chalk.red(err))
     }
-    console.log('Colors token generated successfully.')
+  })
+  console.log(chalk.green('Design tokens color palette generated successfully.'))
+}
+
+const build = () => {
+
+  console.log('Generating color palette')
+
+  const palette = []
+  colors.forEach(element => {
+    palette.push(color(element))
+  })
+
+  const backgroundColor = getBackgroundColor(colors, 'adjust.tone')
+
+  const themeLight = new Theme({
+    colors: palette,
+    backgroundColor,
+    lightness: 100,
+  })
+
+  const themeDark = new Theme({
+    colors: palette,
+    backgroundColor,
+    lightness: 0,
+  })
+
+  formatPalette({
+    themeLight: themeLight.contrastColors,
+    themeDark: themeDark.contrastColors,
   })
 }
+
+build()
