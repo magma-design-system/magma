61d6755d51ebeac68c4f6eea2fde30c1282a4795
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-12-10 12:27:10 +0100

    fix(mds-banner): remove storybook defaults and faker for regression tests

--- projects/stencil/src/components/mds-banner/test/mds-banner.stories.js
+++ projects/stencil/src/components/mds-banner/test/mds-banner.stories.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import faker from 'faker'
 import clsx from 'clsx'
 import MdsBanner from '@component/mds-banner/mds-banner'
 import { iconsDictionary } from '@dictionary/icon'
@@ -10,11 +9,11 @@ export default {
   component: MdsBanner,
   argTypes: {
     deletable: {
-      type: { name: 'boolean', required: false },
+      type: { name: 'boolean' },
       description: 'Shows the cross icon to perform cancel/delete action on element',
     },
     headline: {
-      type: { name: 'string', required: false },
+      type: { name: 'string' },
       description: 'The title on the top of the banner',
     },
     icon: {
@@ -24,13 +23,13 @@ export default {
       control: { type: 'select' },
     },
     tone: {
-      type: { name: 'string', required: false },
+      type: { name: 'string' },
       description: 'Sets the tone of the color variant',
       options: toneSimpleVariantDictionary,
       control: { type: 'select' },
     },
     variant: {
-      type: { name: 'string', required: false },
+      type: { name: 'string' },
       description: 'Sets the theme variant colors',
       options: themeVariantDictionary,
       control: { type: 'select' },
@@ -41,7 +40,7 @@ export default {
 const Template = args =>
   <mds-banner {...args}>
     <mds-text typography="detail">
-      { faker.lorem.paragraph() }
+      Questa è una finestra banner, serve a informare gli utenti su uno stato iniziale o sul fatto che è cambiato.
     </mds-text>
   </mds-banner>
 
@@ -49,48 +48,44 @@ const TemplateActions = args =>
   <div class={clsx(args.tone === 'quiet' && args.variant === 'light' ? 'bg-adjust-tone-01' : 'bg-adjust-tone-10', 'p-4 transition-colors delay-300')}>
     <mds-banner {...args}>
       <mds-text typography="detail">
-        { faker.lorem.paragraph() }
+        Pare che la licenza di questo servizio sia scaduta, il servizio non smetterà di funzionare ma non potrai più utilizzare le <a href="#">funzioni premium</a>.
       </mds-text>
-      <mds-button slot="actions" variant={args.variant} tone="ghost">{ faker.hacker.verb() }</mds-button>
-      <mds-button slot="actions" variant={args.variant}>{ faker.hacker.verb() }</mds-button>
+      <mds-button slot="actions" variant={args.variant} tone="ghost">Annulla</mds-button>
+      <mds-button slot="actions" variant={args.variant}>Rinnova abbonamento</mds-button>
     </mds-banner>
   </div>
 
 const TemplateVariantDefault = args =>
   <mds-banner {...args}>
     <mds-text typography="detail">
-      { faker.lorem.paragraph() }
+      Il tuo account è stato aggiornato alla versione PRO, ora puoi utilizzare le notifiche su aggiornamenti di norme di legge e la consultazione dei volumi correlati.
     </mds-text>
     <mds-button slot="actions" variant="primary" tone="quiet">Cancel</mds-button>
     <mds-button slot="actions" variant="primary">Confirm</mds-button>
   </mds-banner>
 
 const TemplateVariantDark = args =>
-  <div class={clsx(args.tone === 'quiet' && args.variant === 'light' ? 'bg-adjust-tone-01' : 'bg-adjust-tone-10', 'p-4 transition-colors delay-300')}>
-    <mds-banner {...args}>
-      <mds-text typography="detail">
-        { faker.lorem.paragraph() }
-      </mds-text>
-      <mds-button slot="actions" variant="light" tone="quiet">Cancel</mds-button>
-      <mds-button slot="actions" variant="light">Confirm</mds-button>
-    </mds-banner>
-  </div>
+  <mds-banner {...args}>
+    <mds-text typography="detail">
+      Il tuo account è stato aggiornato alla versione PRO, ora puoi utilizzare le notifiche su aggiornamenti di norme di legge e la consultazione dei volumi correlati.
+    </mds-text>
+    <mds-button slot="actions" variant="light" tone="quiet">Cancel</mds-button>
+    <mds-button slot="actions" variant="light">Confirm</mds-button>
+  </mds-banner>
 
 const TemplateVariantLight = args =>
-  <div class={clsx(args.tone === 'quiet' && args.variant === 'light' ? 'bg-adjust-tone-01' : 'bg-adjust-tone-10', 'p-4 transition-colors delay-300')}>
-    <mds-banner {...args}>
-      <mds-text typography="detail">
-        { faker.lorem.paragraph() }
-      </mds-text>
-      <mds-button slot="actions" variant="dark" tone="quiet">Cancel</mds-button>
-      <mds-button slot="actions" variant="dark">Confirm</mds-button>
-    </mds-banner>
-  </div>
+  <mds-banner {...args}>
+    <mds-text typography="detail">
+      Il tuo account è stato aggiornato alla versione PRO, ora puoi utilizzare le notifiche su aggiornamenti di norme di legge e la consultazione dei volumi correlati.
+    </mds-text>
+    <mds-button slot="actions" variant="dark" tone="quiet">Cancel</mds-button>
+    <mds-button slot="actions" variant="dark">Confirm</mds-button>
+  </mds-banner>
 
 const TemplateVariantStatus = args =>
   <mds-banner {...args}>
     <mds-text typography="detail">
-      { args._text }
+    Se procederai, perderai i dati relativi modelli del database che hai registrato in precedenza. Conferma solo se sei consapevole di ciò che stai facendo.
     </mds-text>
     <mds-button slot="actions" variant={ args.variant } tone="quiet">Annulla</mds-button>
     <mds-button slot="actions" variant={ args.variant } tone="weak">Conferma</mds-button>
@@ -100,7 +95,7 @@ export const Default = Template.bind({})
 
 export const Headline = Template.bind({})
 Headline.args = {
-  headline: faker.animal.dog(),
+  headline: 'Avviso',
 }
 
 export const Icon = Template.bind({})
@@ -117,14 +112,14 @@ export const Actions = TemplateActions.bind({})
 
 export const Variant = TemplateVariantDefault.bind({})
 Variant.args = {
-  headline: faker.animal.dog(),
+  headline: 'Impostazioni account',
   icon: 'info',
   deletable: true,
 }
 
 export const VariantDark = TemplateVariantDark.bind({})
 VariantDark.args = {
-  headline: faker.animal.dog(),
+  headline: 'Impostazioni account',
   icon: 'info',
   deletable: true,
   variant: 'dark',
@@ -132,7 +127,7 @@ VariantDark.args = {
 
 export const VariantLight = TemplateVariantLight.bind({})
 VariantLight.args = {
-  headline: faker.animal.dog(),
+  headline: 'Impostazioni account',
   icon: 'info',
   deletable: true,
   variant: 'light',
@@ -140,9 +135,8 @@ VariantLight.args = {
 
 export const VariantStatusWarning = TemplateVariantStatus.bind({})
 VariantStatusWarning.args = {
-  _text: 'Se procederai, perderai i dati relativi modelli del database che hai registrato in precedenza. Conferma solo se sei consapevole di ciò che stai facendo.',
   deletable: true,
-  headline: 'Questa azione non può essere annullata',
+  headline: 'Attenzione',
   icon: 'warning',
   variant: 'warning',
 }
