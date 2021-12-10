679a7327a83d49583090a2ac46e8d47efe5d942c
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-12-10 12:51:57 +0100

    fix(mds-download): remove storybook defaults and faker for regression tests

--- projects/stencil/src/components/mds-download/test/mds-download.stories.js
+++ projects/stencil/src/components/mds-download/test/mds-download.stories.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import faker from 'faker'
 import MdsDownload from '@component/mds-download/mds-download'
 import MdsIcon from '@component/mds-icon/mds-icon'
 import { fileExtensionsDictionary } from '../meta/dictionary'
+import { filesList } from './fixtures'
 
-// import imageFile from './avatar-gruppo-maggioli-512w.png'
-
-const filesList = [
-  'this_is_an_extensionless_file',
-  'https://i2.wp.com/clipart.info/images/ccovers/1495750818Apple-PNG-Clip-Art.png',
-]
 const extensionsList = Object.keys(fileExtensionsDictionary).sort()
-extensionsList.forEach((extension) => {
-  filesList.push(`${faker.system.commonFileName().split('.')[0]}.${extension}`)
-})
-
-const getFile = () =>
-  filesList[faker.datatype.number(filesList.length - 1)]
 
 export default {
   title: 'UI / Download',
@@ -24,21 +12,21 @@ export default {
   subcomponents: { MdsIcon },
   argTypes: {
     description: {
-      type: { name: 'string', required: false },
+      type: { name: 'string' },
       description: 'Overrides the default filetype description',
     },
     filename: {
-      type: { name: 'string', required: false },
+      type: { name: 'string' },
       description: 'The filename shown as component title, is used to auto assign one of the filetype known in the filetype dictionary',
       options: filesList,
       control: { type: 'select' },
     },
     preview: {
-      type: { name: 'string', required: false },
+      type: { name: 'string' },
       description: 'The image preview src if available of a file, useful if you have a logo to display, or a smaller version of a bigger image',
     },
     suffix: {
-      type: { name: 'string', required: false },
+      type: { name: 'string' },
       description: 'Overrides the automatic filetype recongition by forcing the suffix to one of the available formats choosen',
       options: extensionsList,
       control: { type: 'select' },
@@ -51,26 +39,23 @@ const Template = args =>
 
 export const Default = Template.bind({})
 Default.args = {
-  filename: getFile(),
+  filename: filesList[1],
 }
 
 export const Description = Template.bind({})
 Description.args = {
   description: 'This is a custom description',
-  filename: getFile(),
+  filename: filesList[2],
 }
-
-export const tailwindInline = () =>
-  <mds-download filename={getFile()} class="w-auto"/>
 
 export const filesWithoutExtension = Template.bind({})
 filesWithoutExtension.args = {
-  filename: getFile(),
+  filename: filesList[0],
   suffix: 'pdf',
 }
 
 export const preview = Template.bind({})
 preview.args = {
-  filename: getFile(),
+  filename: filesList[5],
   preview: 'https://via.placeholder.com/64x64',
 }
