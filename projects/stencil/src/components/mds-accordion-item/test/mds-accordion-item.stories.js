6bd54d53aa4257a1be662b3348ba01b379408562
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-12-10 11:30:49 +0100

    fix(mds-accordion-item): remove faker fixtures for regression tests

--- projects/stencil/src/components/mds-accordion-item/test/mds-accordion-item.stories.js
+++ projects/stencil/src/components/mds-accordion-item/test/mds-accordion-item.stories.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import MdsAccordionItem from '@component/mds-accordion-item/mds-accordion-item'
 import { typographyPrimaryDictionary } from '@dictionary/typography'
-import faker from 'faker'
 
 export default {
   title: 'UI / Accordion / Accordion Item',
@@ -25,19 +24,17 @@ export default {
 }
 
 const Template = args =>
-  <div>
-    <mds-accordion-item {...args}>
-      <mds-text typography="paragraph" class="text-adjust-tone-08">{ faker.lorem.paragraphs(2) }</mds-text>
-    </mds-accordion-item>
-  </div>
+  <mds-accordion-item {...args}>
+    <mds-text>Blipbug presenta delle fattezze riconducibili agli insetti nello stadio pre-crisalide. Il suo corpo si sviluppa in lunghezza, ed è formato principalmente da tre parti: la sua grande testa, il suo collo (molto simile ad un collare), e il corpo vero e proprio. La prima di queste è suddivisa in una parte color crema e una parte color denim; dello stesso colore sono le appenidici a forma di "V" che si trovano sopra e ai lati della testa. I suoi occhi sono enormi e grigi, ed hanno delle sottilissime sopracciglia sopra di essi. Il suo "collare", anch'esso color denim, presenta delle "setole" giallo sabbia, con le quali percepisce i segnali esterni: stesso colore si presenta nel segmento centrale della sua parte inferiore, dove sono presenti un primo paio di zampe crema. Il segmento superiore del corpo è bianco e ospita delle zampe anteriori color crema, ed infine, la parte finale, o la "coda", è color denim e finisce con un'appendice a "V" un po' piú grossa.</mds-text>
+  </mds-accordion-item>
 
 export const Default = Template.bind({})
 Default.args = {
-  description: 'Come funziona il monitoraggio del sonno?',
+  description: 'Blipbug',
 }
 
 export const opened = Template.bind({})
 opened.args = {
   opened: true,
-  description: 'Come funziona il monitoraggio del sonno?',
+  description: 'Blipbug',
 }
