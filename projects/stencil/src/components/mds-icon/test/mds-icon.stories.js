f37071671d93f45c973d18543e6a3a3fb540afa6
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-12-10 15:29:52 +0100

    fix(mds-icon): remove storybook defaults and faker fixtures for regression tests

---
+++ projects/stencil/src/components/mds-icon/test/mds-icon.stories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import MdsIcon from '@component/mds-icon/mds-icon'
+import { iconsDictionary } from '@dictionary/icon'
+
+export default {
+  title: 'Design / Icon',
+  component: MdsIcon,
+  argTypes: {
+    name: {
+      type: { name: 'string' },
+      description: 'The name of the icon set. The icon set is strictly realted to @maggioli-design-system/icons',
+      options: iconsDictionary,
+      control: { type: 'select' },
+    },
+  },
+}
+
+const Template = args =>
+  <mds-icon {...args}/>
+
+export const Default = Template.bind({})
+Default.args = {
+  name: iconsDictionary[1]
+}
