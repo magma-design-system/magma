879de14b5ead38a09b89b74997d8573b6d5e464d
Vittorio Vittori <vittorio.vittori@maggioli.it>
2021-12-27 14:57:03 +0100

    feat(mds-header-bar): add isolated header bar to ease custom styles

---
+++ projects/stencil/src/components/mds-header-bar/mds-header-bar.tsx
@@ -0,0 +1,55 @@
+import { Component, Element, Event, EventEmitter, Host, Listen, State, h } from '@stencil/core'
+import clsx from 'clsx'
+
+@Component({
+  tag: 'mds-header-bar',
+  styleUrl: 'mds-header-bar.css',
+  shadow: true,
+})
+export class MdsHeaderBar {
+
+  private hasNav: boolean
+  @Element() hostElement: HTMLMdsHeaderElement
+
+  @State() isOpened:boolean
+
+  componentWillLoad (): void {
+    this.hasNav = this.hostElement.querySelector('[slot="nav"]') !== null
+  }
+
+  /**
+   * Emits when the component is opened
+   */
+  @Event({ bubbles: true, composed: true }) headerOpened: EventEmitter<void>
+
+  private open = () => {
+    this.isOpened = true
+    this.headerOpened.emit()
+  }
+
+  @Listen('headerClosed', { target: 'document' })
+  closedHandler (): void {
+    this.isOpened = false
+  }
+
+  render () {
+    return (
+      <Host class={clsx(this.isOpened && 'opened')}>
+        <div class="contents">
+          <div class="logo">
+            <slot/>
+          </div>
+          { this.hasNav &&
+            <nav class="nav">
+              <slot name="nav"/>
+            </nav>
+          }
+          { this.hasNav &&
+            <mds-icon class="icon" name="menu" onClick={ this.open }/>
+          }
+        </div>
+      </Host>
+    )
+  }
+
+}
