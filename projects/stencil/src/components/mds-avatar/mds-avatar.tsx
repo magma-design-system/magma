741ce3f92c4ac315a54dcbda38adabee1e8a95e4
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-01-14 15:41:57 +0100

    style(mds-usage): change colors to ease contrast

--- projects/stencil/src/components/mds-avatar/mds-avatar.tsx
+++ projects/stencil/src/components/mds-avatar/mds-avatar.tsx
@@ -1,5 +1,6 @@
-import { Component, Host, h, State, Prop } from '@stencil/core'
+import { Component, Host, h, State, Prop, Watch } from '@stencil/core'
 import clsx from 'clsx'
+import { avatarVariant } from './meta/variants'
 
 @Component({
   tag: 'mds-avatar',
@@ -10,6 +11,7 @@ export class MdsAvatar {
 
   @State() placeholder = false
   @State() loaded = false
+  @State() hasInitials = false
 
   /**
    * Specifies the path to the image
@@ -21,29 +23,60 @@ export class MdsAvatar {
    */
   @Prop() readonly initials?: string
 
+  private checkInitials = (value: string): void => {
+    if (value !== undefined && value !== '') {
+      this.hasInitials = true
+      return
+    }
+
+    this.hasInitials = false
+  }
+
+  componentWillLoad (): void {
+    this.checkInitials(this.initials)
+  }
+
+  @Watch('initials')
+  initialsHandler (newValue: string): void {
+    this.checkInitials(newValue)
+  }
+
   render () {
+    let backgroundColor = ''
+    if (this.hasInitials) {
+      let cleanedInitials = this.initials.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '').substring(0, 2)
+      if (cleanedInitials.length === 1) {
+        cleanedInitials = cleanedInitials + cleanedInitials
+      }
+      backgroundColor = avatarVariant[(cleanedInitials.substring(0, 1).charCodeAt(0) + cleanedInitials.substring(1, 2).charCodeAt(0)) % avatarVariant.length]
+    }
+
     if (this.src === undefined) {
       this.loaded = true
       this.placeholder = true
     }
     return (
-      <Host class={clsx(
-        this.initials !== undefined && 'initials',
-        this.loaded && 'loaded',
-        this.placeholder && 'fallback',
-      )}>
-        { this.placeholder
-          ? <div class={clsx(this.initials !== undefined ? 'initials-text' : 'fallback-image')}>
-            { this.initials !== undefined && <mds-text typography="h5">{ this.initials.substring(0, 2) }</mds-text> }
-          </div>
-          : <mds-img
-            class="image"
-            loading="lazy"
-            onLoadError={ () => { this.loaded = true; this.placeholder = true } }
-            onLoadSuccess={ () => { this.loaded = true } }
-            src={ this.src }
-          />
-        }
+      <Host>
+        <div class={clsx(
+          'avatar',
+          this.hasInitials && 'initials',
+          this.loaded ? 'loaded' : 'pending',
+          this.placeholder && !this.hasInitials && 'fallback',
+          backgroundColor,
+        )}>
+          { this.placeholder
+            ? <div class={clsx(this.hasInitials ? 'initials-text' : 'fallback-image')}>
+              { this.hasInitials && <mds-text typography="h5">{ this.initials.substring(0, 2) }</mds-text> }
+            </div>
+            : <mds-img
+              class="image"
+              loading="lazy"
+              onLoadError={ () => { this.loaded = true; this.placeholder = true } }
+              onLoadSuccess={ () => { this.loaded = true } }
+              src={ this.src }
+            />
+          }
+        </div>
       </Host>
     )
   }
--- projects/stencil/src/components/mds-usage/meta/variants.ts
+++ projects/stencil/src/components/mds-usage/meta/variants.ts
@@ -1,20 +1,20 @@
 const usageVariant = {
   do: {
     alias: 'Do',
-    border: 'border-t-status-success-03',
-    color: 'text-status-success-03',
+    border: 'border-t-status-success-04',
+    color: 'text-status-success-04',
     icon: 'check-circle-outline',
   },
   dont: {
     alias: 'Don\'t',
-    border: 'border-t-status-error-03',
-    color: 'text-status-error-03',
+    border: 'border-t-status-error-04',
+    color: 'text-status-error-04',
     icon: 'error-outline',
   },
   warn: {
     alias: 'Warning',
-    border: 'border-t-status-warning-03',
-    color: 'text-status-warning-03',
+    border: 'border-t-status-warning-04',
+    color: 'text-status-warning-04',
     icon: 'warning-amber',
   },
 }
