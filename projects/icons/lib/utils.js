6217c5bb9e49dcbb4f9df510bd0af043ffcc197a
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-01-26 14:47:47 +0100

    fix(icons): move build to package root

--- projects/icons/lib/utils.js
+++ projects/icons/lib/utils.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const ROOT_PATH_DIR = path.join(__dirname, '..')
-const BUILD_PATH_DIR = path.join(process.cwd(), 'dist')
+const BUILD_PATH_DIR = path.join(process.cwd(), './')
 const BUILD_ORIGINAL_PATH_DIR = path.join(BUILD_PATH_DIR, 'original')
 
 exports.ROOT_PATH_DIR = ROOT_PATH_DIR
