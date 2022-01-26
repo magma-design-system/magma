b35485fc93fba5135c762c1548b3dd5512b75b98
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-01-26 17:31:53 +0100

    fix(docs): lint comment

--- projects/docs/next/stencil.d.ts
+++ projects/docs/next/stencil.d.ts
@@ -1,21 +1,21 @@
-import type { JSX as LocalJSX } from "@stencil/core";
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import type { JSX as LocalJSX } from '@stencil/core'
+import { DetailedHTMLProps, HTMLAttributes } from 'react'
 
 type StencilProps<T> = {
-    [P in keyof T]?: Omit<T[P], 'ref'> | HTMLAttributes<T>;
-};
+    [P in keyof T]?: Omit<T[P], 'ref'> | HTMLAttributes<T>
+}
 
 type ReactProps<T> = {
-    [P in keyof T]?: DetailedHTMLProps<HTMLAttributes<T[P]>, T[P]>;
-};
+    [P in keyof T]?: DetailedHTMLProps<HTMLAttributes<T[P]>, T[P]>
+}
 
 type StencilToReact<
     T = LocalJSX.IntrinsicElements,
     U = HTMLElementTagNameMap
-> = StencilProps<T> & ReactProps<U>;
+> = StencilProps<T> & ReactProps<U>
 
 declare module 'react' {
     namespace JSX {
-        type IntrinsicElements = StencilToReact;
+        type IntrinsicElements = StencilToReact
     }
 }
