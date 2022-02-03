789fb5b857fd58f9b44f749e2ea4222fe54be400
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-03 17:28:29 +0100

    refact(mds-file): rename component due to npm naming policies

---
+++ projects/stencil/src/components/mds-file/mds-file.tsx
@@ -0,0 +1,101 @@
+import { Component, Host, h, Prop } from '@stencil/core'
+import { ExtensionSuffixType } from './meta/types'
+import { fileFormatsVariant } from './meta/variants'
+import { fileExtensionsDictionary } from './meta/dictionary'
+import { ThemeFullVariantType } from '../../types/variant'
+
+@Component({
+  tag: 'mds-file',
+  styleUrl: 'mds-file.css',
+  shadow: true,
+})
+export class MdsFile {
+
+  /**
+   * Overrides the automatic filetype recongition by forcing the suffix to one of the available formats choosen
+   */
+  @Prop() readonly suffix?: ExtensionSuffixType
+
+  /**
+   * Overrides the default filetype description
+   */
+  @Prop() readonly description?: string
+
+  /**
+   * The filename shown as component title, is used to auto assign one of the filetype known in the filetype dictionary
+   */
+  @Prop() readonly filename: string
+
+  /**
+   * The image preview src if available of a file, useful if you have a logo to display, or a smaller version of a bigger image
+   */
+  @Prop() readonly preview?: string
+
+  private sanitizeFilename = () => {
+    if (this.filename === undefined ) {
+      throw console.error('Attribute "filename" is undefined.')
+    }
+    if (this.filename.includes('/')) {
+      return this.filename.split('/').pop()
+    }
+    return this.filename
+  }
+
+  private sanitizeSuffix = () => {
+    const filename = this.sanitizeFilename()
+    if (filename.includes('.')) {
+      return filename.split('.').pop()
+    }
+    return filename
+  }
+
+  private getName = () => {
+    const filename = this.sanitizeFilename()
+    if (filename.includes('.')) {
+      return filename.split('.')[0]
+    }
+    return filename
+  }
+
+  private getSuffix = () => {
+    const suffix = this.sanitizeSuffix()
+    const filename = this.sanitizeFilename()
+    if (this.suffix !== null && this.suffix !== undefined) {
+      return this.suffix.toLowerCase()
+    }
+    if (suffix !== filename) {
+      return suffix
+    }
+    return null
+  }
+
+  private getExtensionInfos = () => {
+    const suffix = this.getSuffix() !== null ? this.getSuffix().toLowerCase() : 'default'
+    return fileExtensionsDictionary[suffix] !== undefined ? fileExtensionsDictionary[suffix] : fileExtensionsDictionary.default
+  }
+
+  render () {
+    const { format, description } = this.getExtensionInfos()
+    const { variant, color, icon, iconBackground } = fileFormatsVariant[format]
+    return (
+      <Host>
+        <div class={`preview ${color} ${iconBackground}`}>
+          { this.preview !== undefined
+            ? <div class="image-preview" style={{ backgroundImage: `url(${this.preview})` }}></div>
+            : <mds-icon name={icon}/>
+          }
+        </div>
+        <div class="info">
+          <div class="filename" title={ this.filename }>
+            <mds-text typography="h6" class="name">{ this.getName() }</mds-text>
+            { this.suffix === undefined && this.getSuffix() && <mds-text typography="h6" class="extension">.{ this.getSuffix() }</mds-text> }
+          </div>
+          <div class="detail">
+            { this.getSuffix() && <mds-badge variant={variant as ThemeFullVariantType} tone="quiet" class="suffix">{ this.getSuffix() }</mds-badge> }
+            <mds-text typography="caption" class="description" title={ this.description || description }>{ this.description || description }</mds-text>
+          </div>
+        </div>
+      </Host>
+    )
+  }
+}
---
+++ projects/stencil/src/components/mds-file/test/mds-file.stories.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import MdsFile from '@component/mds-file/mds-file'
+import MdsIcon from '@component/mds-icon/mds-icon'
+import { fileExtensionsDictionary } from '../meta/dictionary'
+import { filesList } from './fixtures'
+
+const extensionsList = Object.keys(fileExtensionsDictionary).sort()
+
+export default {
+  title: 'UI / File',
+  component: MdsFile,
+  subcomponents: { MdsIcon },
+  argTypes: {
+    description: {
+      type: { name: 'string' },
+      description: 'Overrides the default filetype description',
+    },
+    filename: {
+      type: { name: 'string' },
+      description: 'The filename shown as component title, is used to auto assign one of the filetype known in the filetype dictionary',
+      options: filesList,
+      control: { type: 'select' },
+    },
+    preview: {
+      type: { name: 'string' },
+      description: 'The image preview src if available of a file, useful if you have a logo to display, or a smaller version of a bigger image',
+    },
+    suffix: {
+      type: { name: 'string' },
+      description: 'Overrides the automatic filetype recongition by forcing the suffix to one of the available formats choosen',
+      options: extensionsList,
+      control: { type: 'select' },
+    },
+  },
+}
+
+const Template = args =>
+  <mds-file {...args}/>
+
+export const Default = Template.bind({})
+Default.args = {
+  filename: filesList[1],
+}
+
+export const Description = Template.bind({})
+Description.args = {
+  description: 'This is a custom description',
+  filename: filesList[2],
+}
+
+export const filesWithoutExtension = Template.bind({})
+filesWithoutExtension.args = {
+  filename: filesList[0],
+  suffix: 'pdf',
+}
+
+export const preview = Template.bind({})
+preview.args = {
+  filename: filesList[5],
+  preview: 'https://via.placeholder.com/64x64',
+}
