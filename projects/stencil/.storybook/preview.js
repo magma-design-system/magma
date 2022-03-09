d4ac0fdbf41f12790f6a5c618fec95bd3fe116a3
Andrea Pruccoli <andrea.pruccoli@maggioli.it>
2022-03-09 17:08:55 +0100

    refactor(stencil): move code from mds-icon-svg to mds-icon v2.0.0-rc.1

--- projects/stencil/.storybook/preview.js
+++ projects/stencil/.storybook/preview.js
@@ -12,40 +12,40 @@ import './iconsauce.css'
 
 import devices from './devices.json'
 import media from '@maggioli-design-system/design-tokens/dist/css-tokens/media.json'
-import { mds_icon_svg } from '../dist/esm/mds-icon-svg.entry'
-import { MdsIconSvg } from '../dist/components/mds-icon-svg'
+import { mds_icon } from '../dist/esm/mds-icon.entry'
+import { MdsIcon } from '../dist/components/mds-icon'
 
 defineCustomElements();
 
-// Method 1 - call static function of MdsIconSvg
-const mdsIconSvgGet = async () => {
-  await customElements.whenDefined('mds-icon-svg')
+// Method 1 - call static function of MdsIcon
+const mdsIconGet = async () => {
+  await customElements.whenDefined('mds-icon')
 
-  mds_icon_svg.setSvgPathStatic('/svg/')
-  // MdsIconSvg.setSvgPathStatic('/svg/') // non va, why?
+  mds_icon.setSvgPathStatic('/svg/')
+  // MdsIcon.setSvgPathStatic('/svg/') // non va, why?
 }
 
-mdsIconSvgGet()
+mdsIconGet()
 
 // Method 2 - use of sessionStorage
-// window.sessionStorage.setItem('mdsIconSvgPath', '/svg/')
+// window.sessionStorage.setItem('mdsIconPath', '/svg/')
 
-// Method 3 - instantiate a temp MdsIconSvg DOM node element to call a stencil class Method
-// const mdsIconSvgGet = async () => {
-//   await customElements.whenDefined('mds-icon-svg')
+// Method 3 - instantiate a temp MdsIcon DOM node element to call a stencil class Method
+// const mdsIconGet = async () => {
+//   await customElements.whenDefined('mds-icon')
 
-//   const mdsIconSvg = document.createElement('mds-icon-svg')
+//   const mdsIcon = document.createElement('mds-icon')
 
-//   document.body.appendChild(mdsIconSvg)
+//   document.body.appendChild(mdsIcon)
 
-//   if ('setSvgPath' in mdsIconSvg) {
-//     mdsIconSvg.setSvgPath('/svg/')
+//   if ('setSvgPath' in mdsIcon) {
+//     mdsIcon.setSvgPath('/svg/')
 //   }
 
-//   document.body.removeChild(mdsIconSvg)
+//   document.body.removeChild(mdsIcon)
 // }
 
-// mdsIconSvgGet()
+// mdsIconGet()
 
 const toUpperCase = string => {
   return string.charAt(0).toUpperCase() + string.slice(1)
--- projects/stencil/src/components/mds-icon/mds-icon.tsx
+++ projects/stencil/src/components/mds-icon/mds-icon.tsx
@@ -1,5 +1,7 @@
-import { Component, Host, h, Prop } from '@stencil/core'
-import clsx from 'clsx'
+import { Component, h, Host, Method, Prop, State, Watch } from '@stencil/core'
+import { IconsSetService } from './services/icons-set.service'
+
+import 'external-svg-loader'
 
 @Component({
   tag: 'mds-icon',
@@ -9,14 +11,39 @@ import clsx from 'clsx'
 export class MdsIcon {
 
   /**
-   * The name of the icon set.
-   * The icon set is strictly realted to @maggioli-design-system/icons
+   * The name of the icon.
    */
-  @Prop() readonly name!: string
+  @Prop({ reflect: true }) readonly name!: string
+
+  @State() _iconHref: string
+
+  static readonly _svgPathKey = 'mdsIconSvgPath'
+
+  static setSvgPathStatic (path: string): void {
+    IconsSetService.setSvgPath(path)
+  }
+
+  @Method()
+  async setSvgPath (svgPath: string): Promise<void> {
+    IconsSetService.setSvgPath(svgPath)
+    this.updateIconHref()
+  }
+
+  @Watch('name')
+  updateIconHref (): void {
+    const svgPath = IconsSetService.getSvgPath() || window.sessionStorage.getItem(MdsIcon._svgPathKey)
+    this._iconHref = svgPath && !this.name.startsWith('http') ? window.location.origin.concat(svgPath.concat(this.name).concat('.svg')) : this.name
+  }
+
+  componentWillLoad (): void {
+    this.updateIconHref()
+  }
 
   render () {
     return (
-      <Host class={clsx(this.name)}/>
+      <Host>
+        <svg data-src={this._iconHref} height="24" width="24" data-cache="disabled"></svg>
+      </Host>
     )
   }
 }
