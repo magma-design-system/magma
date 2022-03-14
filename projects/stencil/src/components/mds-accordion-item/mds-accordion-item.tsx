552279588953e29c440147454d893cb0ed362cbf
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-03-14 17:02:51 +0100

    feat(mds-accordion-item): add support for inline svg icons

--- projects/stencil/src/components/mds-accordion-item/mds-accordion-item.tsx
+++ projects/stencil/src/components/mds-accordion-item/mds-accordion-item.tsx
@@ -1,6 +1,7 @@
-import { Component, Host, h, Prop, State, Element, Event, EventEmitter, Watch } from '@stencil/core'
-import clsx from 'clsx'
+import { Component, Host, h, Prop, Element, Event, EventEmitter } from '@stencil/core'
 import { TypographyPrimaryType } from '../../types/typography'
+
+import miBaselineKeyboardArrowUp from '@icon/mi/baseline/keyboard-arrow-up.svg'
 
 @Component({
   tag: 'mds-accordion-item',
@@ -10,7 +11,6 @@ import { TypographyPrimaryType } from '../../types/typography'
 export class MdsAccordionItem {
 
   @Element() private element: HTMLMdsAccordionItemElement
-  @State() isOpened:boolean
 
   /**
    * Specifies the typography of the element
@@ -20,22 +20,16 @@ export class MdsAccordionItem {
   /**
    * Specifies if the accordion item is opened or not
    */
-  @Prop() readonly opened?: boolean
+  @Prop({ mutable: true, reflect: true }) opened?: boolean
 
   /**
    * Specifies the title shown when the accordion is closed or opened
    */
   @Prop() readonly description!: string
 
-  componentWillLoad (): void {
-    this.isOpened = this.opened
-  }
-
   private toggle = () => {
-    this.isOpened = !this.isOpened
-    if (this.isOpened) {
-      this.openedEvent.emit(this.element.id)
-    }
+    this.opened = !this.opened
+    this.openedEvent.emit(this.element.id)
   }
 
   /**
@@ -43,38 +37,16 @@ export class MdsAccordionItem {
    */
   @Event() openedEvent: EventEmitter<string>
 
-  @Watch('opened')
-  validateOpened (newValue: boolean): void {
-    this.isOpened = newValue
-  }
-
   render () {
-    let fontSizeInherit: string
-    switch (this.typography) {
-    case 'h1': {
-      fontSizeInherit = 'font-inherit'
-      break
-    }
-    case 'h2': {
-      fontSizeInherit = 'font-inherit'
-      break
-    }
-    case 'h3': {
-      fontSizeInherit = 'font-inherit'
-      break
-    }
-    }
     return (
-      <Host class={ clsx(this.isOpened && 'opened') }>
+      <Host>
         <mds-grid class="header" onClick={ this.toggle }>
           <mds-text typography={ this.typography }>{ this.description }</mds-text>
           <mds-text class="icon-button" typography={ this.typography }>
-            <mds-icon class={ clsx('icon', fontSizeInherit)} name={clsx(
-              this.isOpened ? 'keyboard-arrow-up' : 'keyboard-arrow-down',
-            ) }/>
+            <i class="svg icon" innerHTML={miBaselineKeyboardArrowUp}/>
           </mds-text>
         </mds-grid>
-        <div class={ clsx('contents', this.isOpened && 'contents--opened') }>
+        <div class="contents">
           <slot/>
         </div>
       </Host>
