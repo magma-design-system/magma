299acde4ac698a75079e49d531fbbb24a924a412
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-03-17 17:43:10 +0100

    change(mds-usage): change icon component

--- projects/stencil/src/components/mds-usage/meta/variants.ts
+++ projects/stencil/src/components/mds-usage/meta/variants.ts
@@ -2,20 +2,20 @@ const usageVariant = {
   do: {
     alias: 'Do',
     border: 'border-t-status-success-04',
-    color: 'text-status-success-04',
-    icon: 'check-circle-outline',
+    color: 'fill-status-success-05 text-status-success-04',
+    icon: 'mi/baseline/check-circle-outline',
   },
   dont: {
-    alias: 'Don\'t',
+    alias: 'Don’t',
     border: 'border-t-status-error-04',
-    color: 'text-status-error-04',
-    icon: 'error-outline',
+    color: 'fill-status-error-05 text-status-error-04',
+    icon: 'mi/baseline/error-outline',
   },
   warn: {
     alias: 'Warning',
     border: 'border-t-status-warning-04',
-    color: 'text-status-warning-04',
-    icon: 'warning-amber',
+    color: 'fill-status-warning-05 text-status-warning-04',
+    icon: 'mi/baseline/warning-amber',
   },
 }
 
