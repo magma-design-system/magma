75bd03bdf1e23098f7fba191119ae9d902ca1d32
Andrea Pruccoli <andrea.pruccoli@maggioli.it>
2022-03-31 14:31:25 +0200

    fix(stencil): change element to be equal to in test

--- projects/stencil/src/components/mds-modal/test/mds-modal.e2e.ts
+++ projects/stencil/src/components/mds-modal/test/mds-modal.e2e.ts
@@ -22,7 +22,11 @@ describe('mds-modal', () => {
       <div class="window">
         <slot></slot>
       </div>
-      <mds-icon class="close hydrated"></mds-icon>
+      <i class="close svg">
+        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"></path>
+        </svg>
+      </i>
     `)
   })
 
@@ -38,7 +42,11 @@ describe('mds-modal', () => {
       <div class="window">
         <slot></slot>
       </div>
-      <mds-icon class="close hydrated"></mds-icon>
+      <i class="close svg">
+        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"></path>
+        </svg>
+      </i>
     `)
   })
 
