9329ddf3aea73cf7b6326d010260c86692be7226
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-05-24 17:41:59 +0200

    refact(icons): convert from js to ts

---
+++ projects/icons/scripts/icons-groups.ts
@@ -0,0 +1,230 @@
+
+import chalk from 'chalk'
+import fs from 'fs/promises'
+import path from 'path'
+
+class Maggioli {
+  static ICONS_DIR = `${path.dirname(require.resolve('@maggioli-design-system/svg-icons/package.json'))}/svg`
+  static FILE_NAME_REGEX = /^([\w-]+)\.svg$/
+
+  /**
+   * List of paths of subdirectories (possibly) with icons
+   */
+  static async subDirectories (): Promise<string[]> {
+    return [this.ICONS_DIR]
+  }
+
+  /**
+   * Search the requested icon in Maggioli icons
+   */
+  static async getPath (iconName: string): Promise<string> {
+    const subdirectories = await Maggioli.subDirectories()
+    const filename = `${iconName}.svg`
+
+    return iconGroupGetHelper('maggioli', subdirectories, iconName, filename)
+  }
+
+  /**
+   * Search all the Maggioli icons
+   */
+  static async listPath (): Promise<string[]> {
+    const subdirectories = await Maggioli.subDirectories()
+    return iconGroupListHelper('maggioli', subdirectories, Maggioli.FILE_NAME_REGEX)
+  }
+
+  /**
+   * Given the path of an icon or just the file name, it returns the icon name
+   */
+  static getIconName (path: string): string {
+    return path.split('/').slice(-1)[0].match(Maggioli.FILE_NAME_REGEX)[1]
+  }
+}
+
+class Material {
+  static ICONS_DIR = `${path.dirname(require.resolve('material-design-icons-updated/package.json'))}/icons/filled`
+  static FILE_NAME_REGEX = /^ic_([\w-_]+)_24px\.svg$/
+
+  /**
+   * List of paths of subdirectories (possibly) with icons
+   */
+  static async subDirectories (): Promise<string[]> {
+    return subDirectories(this.ICONS_DIR)
+  }
+
+  /**
+   * Search the requested icon in Material icons
+   */
+  static async getPath (iconName: string): Promise<string> {
+    const subdirectories = await Material.subDirectories()
+    const filename = `ic_${iconName}_24px.svg`
+
+    return iconGroupGetHelper('material', subdirectories, iconName, filename)
+  }
+
+  /**
+   * Search all the Material icons
+   */
+  static async listPath (): Promise<string[]> {
+    const subdirectories = await Material.subDirectories()
+    return iconGroupListHelper('material', subdirectories, Material.FILE_NAME_REGEX)
+  }
+
+  /**
+   * Given the path of an icon or just the file name, it returns the icon name
+   */
+  static getIconName (path: string): string {
+    return path.split('/').slice(-1)[0].match(Material.FILE_NAME_REGEX)[1]
+  }
+}
+
+class MaterialCommunity {
+  static ICONS_DIR = `${path.dirname(require.resolve('@mdi/svg/package.json'))}/svg`
+  static FILE_NAME_REGEX = /^([\w-]+)\.svg$/
+
+  /**
+   * List of paths of subdirectories (possibly) with icons
+   */
+  static async subDirectories (): Promise<string[]> {
+    return [this.ICONS_DIR]
+  }
+
+  /**
+   * Search the requested icon in Material Design Icons Community
+   * @param iconName {string} name of the icon
+   */
+  static async getPath (iconName: string): Promise<string> {
+    const subdirectories = await MaterialCommunity.subDirectories()
+    const filename = `${iconName}.svg`
+
+    return iconGroupGetHelper('mdi', subdirectories, iconName, filename)
+  }
+
+  /**
+   * Search all the Maggioli icons
+   */
+  static async listPath (): Promise<string[]> {
+    const subdirectories = await MaterialCommunity.subDirectories()
+    return iconGroupListHelper('mdi', subdirectories, MaterialCommunity.FILE_NAME_REGEX)
+  }
+
+  /**
+   * Given the path of an icon or just the file name, it returns the icon name
+   */
+  static getIconName (path: string): string {
+    return path.split('/').slice(-1)[0].match(MaterialCommunity.FILE_NAME_REGEX)[1]
+  }
+}
+
+class LocalDirectory {
+  static FILE_NAME_REGEX = /^([\w-]+)\.svg$/
+
+  /**
+   * List of paths of subdirectories (possibly) with icons
+   */
+  static async subDirectories (): Promise<string[]> {
+    return LocalDirectory._subDirectories
+  }
+  static _subDirectories = []
+
+  /**
+   * Search the requested icon in the local directory
+   */
+  static async getPath (iconName: string): Promise<string> {
+    const subdirectories = await LocalDirectory.subDirectories()
+    const filename = `${iconName}.svg`
+
+    return iconGroupGetHelper(null, subdirectories, iconName, filename)
+  }
+
+  /**
+   * Search all the local icons
+   * @return {Promise<string[]>} Paths of all the local icons
+   */
+  static async listPath (): Promise<string[]> {
+    const subdirectories = await LocalDirectory.subDirectories()
+    return iconGroupListHelper(null, subdirectories)
+  }
+
+  /**
+   * Given the path of an icon or just the file name, it returns the icon name
+   */
+  static getIconName (path: string): string | boolean {
+    if (!path.includes('/')) return false
+    return path.match(LocalDirectory.FILE_NAME_REGEX)[1]
+  }
+}
+
+/**
+ * Ritorna un array con i path delle sottocartelle rispetto alla cartella passata in input
+ */
+async function subDirectories (source: string): Promise<string[]> {
+  return (await fs.readdir(source, { withFileTypes: true }))
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => `${source}/${dirent.name}`)
+}
+
+/**
+ * Get icon helper to simplify groups functions
+ */
+async function iconGroupGetHelper (iconGroup: string, directories: string[], iconName: string, filename: string): Promise<string> {
+  const hidePath = path.resolve(__dirname, '../../../')
+  for (const directory of directories) {
+    const path = await searchFileInDirectory(directory, filename)
+    if (path) {
+      console.log(`${chalk.green('Found:')} ${iconGroup}/${iconName}  >  ${path.replace(hidePath, '')}`)
+      return path
+    }
+  }
+  throw new Error(`${chalk.red('Icon not found:')} ${iconGroup ? iconGroup + '/' : ''}${iconName}, searched as ${filename}`)
+}
+
+/**
+ * List icons helper to simplify groups functions
+ */
+async function iconGroupListHelper (iconGroup: string, directories: string[], fileTemplate?: RegExp): Promise<string[]> {
+  return Promise.all(directories.map(async dir => (await listFilesInDirectory(dir, fileTemplate)).map(filename => path.join(dir, filename))))
+    .then(matrix => matrix.flat())
+}
+
+/**
+ * List all files in a directory
+ */
+async function listFilesInDirectory (directory: string, fileTemplate?: RegExp): Promise<string[]> {
+  return fs.readdir(directory)
+    .catch(() => [])
+    .then(files => (fileTemplate ? files.filter(file => file.match(fileTemplate)) : files))
+}
+
+/**
+ * Search a file in a directory and return its path, if found
+ */
+async function searchFileInDirectory (directory: string, filename: string): Promise<string | void> {
+  const files = await listFilesInDirectory(directory)
+
+  if (files.includes(filename)) return path.join(directory, filename)
+}
+
+const ICON_GROUPS = {
+  localDirectory: { getPath: LocalDirectory.getPath, listPath: LocalDirectory.listPath, getIconName: LocalDirectory.getIconName, subDirectories: LocalDirectory._subDirectories },
+  maggioli: { getPath: Maggioli.getPath, listPath: Maggioli.listPath, getIconName: Maggioli.getIconName },
+  material: { getPath: Material.getPath, listPath: Material.listPath, getIconName: Material.getIconName },
+  mdi: { getPath: MaterialCommunity.getPath, listPath: MaterialCommunity.listPath, getIconName: MaterialCommunity.getIconName },
+}
+
+/**
+ * Given the path of an icon, it returns the icon group name
+ * @param path path of an icon
+ * @return {Promise<string>}
+ */
+async function pathToIconsGroup (path: string): Promise<string> {
+  for (const [key, value] of ICON_GROUPS) {
+    const subdirectories = await value.subDirectories()
+    if (subdirectories.find((dir: string) => path.startsWith(dir))) {
+      return key
+    }
+  }
+  throw new Error(`Could not find an icons group for path ${path}`)
+}
+
+exports.ICON_GROUPS = ICON_GROUPS
+exports.pathToIconsGroup = pathToIconsGroup
--- projects/icons/scripts/meta.ts
+++ projects/icons/scripts/meta.ts
@@ -1,6 +1,10 @@
 import path from 'path'
+const ROOT_DIR = path.join(__dirname, '../')
 const DIST_DIR = path.resolve(__dirname, '../dist')
+const ORIGINAL_DIR = path.join(DIST_DIR, 'original')
 
 export {
+  ROOT_DIR,
+  ORIGINAL_DIR,
   DIST_DIR,
 }
