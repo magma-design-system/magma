1b3a08bf4bad244aab77baedde03f7f78f20c213
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-05-24 16:58:40 +0200

    refact(icons) change rm bash script with node scripts

---
+++ projects/icons/scripts/clean.ts
@@ -0,0 +1,11 @@
+import chalk from 'chalk'
+import path from 'path'
+import { DIST_DIR } from './meta'
+import { rm } from 'fs/promises'
+
+rm(DIST_DIR, { force: true, recursive: true })
+  .then(() => {
+    console.log(`Directory ${chalk.green(path.basename(DIST_DIR))} deleted ${chalk.green('successfully')} ${chalk.gray('(or skipped if missing)')}`)
+  }).catch(error => {
+    throw Error(chalk.red(error))
+  })
