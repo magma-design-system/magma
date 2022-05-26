16e6cc41cfeb3a48068cec968ee38861f6c29e09
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-05-26 16:08:54 +0200

    build(magma): add consistent logging

--- scripts/log.ts
+++ scripts/log.ts
@@ -8,23 +8,45 @@ const logDirectoryCopied = (directory: PathLike, destination: PathLike) => {
   console.log(`Directory ${chalk.yellow(path.basename(directory.toString()))} successfully ${chalk.blue('copied')} in ${chalk.yellow(path.basename(destination.toString()))}`)
 }
 
+const logDirectoryCreated = (directory: PathLike, destination?: PathLike) => {
+  console.log(`Directory ${chalk.yellow(path.basename(directory.toString()))} successfully ${chalk.green('created')} ${destination ? 'in ' + chalk.yellow(path.basename(destination.toString())) : ''}`)
+}
+
 const logDirectoryDeleted = (directory: PathLike) => {
   console.log(`Directory ${chalk.yellow(path.basename(directory.toString()))} successfully ${chalk.red('deleted')} ${chalk.gray('(or skipped if missing)')}`)
 }
 
+const logFileSavedTo = (file: PathLike, destination?: PathLike) => {
+  console.log(`File ${chalk.yellow(path.basename(file.toString().trim()))} successfully ${chalk.green('saved')} ${destination ? 'in ' + chalk.yellow(path.basename(destination.toString().trim())) : ''}`)
+}
+
 interface FileActionDoneOptions {
-  entity: string
-  source: PathLike
-  action: string
+  entity: string // inventory
+  source?: PathLike
+  actionDone: string // created, copied, deleted, saved
   destination?: PathLike
 }
 
 const logFileActionDone = (args: FileActionDoneOptions) => {
-  console.log(`${capitalize(args.entity)} ${chalk.yellow(path.basename(args.source.toString()))} successfully ${chalk.blue(args.action)} ${args.destination ? `as ${chalk.yellow(path.basename(args.destination.toString()))}` : ''}`)
+  console.log(`${capitalize(args.entity.trim())}${args.source ? ' ' + chalk.yellow(path.basename(args.source.toString().trim())) : ''} successfully ${chalk.blue(args.actionDone.trim())} ${args.destination ? `as ${chalk.yellow(path.basename(args.destination.toString().trim()))}` : ''}`)
+}
+
+interface StatusOptions {
+  actionDoing: string // checking
+  subject?: string // A
+  status?: string // matches
+  match?: string // B
+}
+
+const logStatus = (args: StatusOptions) => {
+  console.log(`${capitalize(args.actionDoing)} if ${args.subject ? chalk.yellow(args.subject) : ''} ${args.status ? args.status : ''} ${args.match ? chalk.yellow(args.match) : ''}`)
 }
 
 export {
-  logDirectoryDeleted,
   logDirectoryCopied,
+  logDirectoryCreated,
+  logDirectoryDeleted,
   logFileActionDone,
+  logStatus,
+  logFileSavedTo,
 }
