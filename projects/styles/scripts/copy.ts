bd163abda6b8514fab05b09aacdd7268a6215ec2
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-06-13 18:34:55 +0200

    change(styles): add compatibility with tailwind and postcss both

--- projects/styles/scripts/copy.ts
+++ projects/styles/scripts/copy.ts
@@ -1,18 +1,28 @@
 import chalk from 'chalk'
 import path from 'path'
-import { DIST_DIR } from './meta'
+import { DIST_DIR, PROJECT_DIR } from './meta'
 import { copy } from 'fs-extra'
 import { logDirectoryCopied } from '../../../scripts/log'
 
-const copyDirectory = async (src: string, dest: string) => {
-  copy(path.join(src), dest)
-    .then(() => {
-      logDirectoryCopied(src, dest)
-    })
-    .catch(error => {
-      throw Error(chalk.red(error))
-    })
+const copyDirectory = async (src: string, dest: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    copy(path.join(src), dest)
+      .then(() => {
+        logDirectoryCopied(src, dest)
+        resolve()
+      })
+      .catch(error => {
+        reject()
+        throw Error(chalk.red(error))
+      })
+  })
 }
 
-copyDirectory('tailwind', path.join(DIST_DIR, 'tailwind'))
-copyDirectory('../design-tokens/dist/css', path.join(DIST_DIR, 'css'))
+const main = async () => {
+  await copyDirectory(path.join(PROJECT_DIR, 'tailwind'), path.join(DIST_DIR, 'tailwind'))
+  await copyDirectory(path.join(PROJECT_DIR, 'css'), path.join(DIST_DIR, 'css'))
+  await copyDirectory(path.join(PROJECT_DIR, '../design-tokens/dist/css'), path.join(DIST_DIR, 'css'))
+}
+
+main()
+
