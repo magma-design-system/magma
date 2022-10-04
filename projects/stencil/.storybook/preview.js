efd99bf3c0beaa90251533c8bdc8aa998d882602
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-10-04 09:54:49 +0200

    fix(magma): remove wrong font size

--- projects/stencil/.storybook/preview.js
+++ projects/stencil/.storybook/preview.js
@@ -12,7 +12,7 @@ import '@fontsource/karla/700.css'
 import '@fontsource/merriweather/400.css'
 import '@fontsource/merriweather/700.css'
 import '@fontsource/roboto/700.css'
-import '@fontsource/roboto/800.css'
+import '@fontsource/roboto/900.css'
 import './tailwind.css'
 import './iconsauce.css'
 
