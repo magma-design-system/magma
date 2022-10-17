ffa10e7bf2ded4ca7d1d7d0447105992eb20c60f
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-10-17 15:14:25 +0200

    feat(mds-dropdown): add some aria feature

---
+++ projects/stencil/src/common/aria.ts
@@ -0,0 +1,22 @@
+const hash = (s: string): string => {
+  let i: number, h: number
+  for (i = 0, h = 0; i < s.length; i ++) {
+    h = Math.imul(31, h) + s.charCodeAt(i) | 0
+  }
+  return h.toString()
+}
+
+const setAttributeIfEmpty = (element: HTMLElement, attribute: string, value: string): string => {
+  if (element.hasAttribute(attribute)) {
+    return element.getAttribute(attribute)
+  }
+  element.setAttribute(attribute, value)
+  return value
+}
+
+const hashValue = (value: string): string => `${value}-${hash(value)}`
+
+export {
+  setAttributeIfEmpty,
+  hashValue,
+}
