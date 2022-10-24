98839aee29bb1b93e10454b8f0966714397de629
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-10-24 18:04:32 +0200

    feat(mds-progress): add accessibility support

--- projects/stencil/src/components/mds-progress/mds-progress.tsx
+++ projects/stencil/src/components/mds-progress/mds-progress.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop } from '@stencil/core'
+import { Component, Element, Host, h, Prop, Watch, State } from '@stencil/core'
 import { DirectionType } from './meta/types'
 import { ThemeVariantType } from '../../types/variant'
 
@@ -8,6 +8,9 @@ import { ThemeVariantType } from '../../types/variant'
   shadow: true,
 })
 export class MdsProgress {
+  @Element() private element: HTMLMdsAccordionTimerElement
+  @State() currentStep: string
+  private stepsList = []
 
   /**
    * A value between 0 and 1 that rapresents the status progress
@@ -24,9 +27,36 @@ export class MdsProgress {
    */
   @Prop({ reflect: true }) readonly variant?: ThemeVariantType = 'dark'
 
+  /**
+   * Sets the steps that can be pronounced by accessibility technologies
+   */
+  @Prop() readonly steps?: string = 'Inizio,Un quarto,Metà,Tre quarti,Fine'
+
+  componentWillLoad (): void {
+    this.stepsList = this.steps.split(',')
+    this.setProgress(this.progress)
+  }
+
+  private setProgress (progress: number): void {
+    if (this.steps) {
+      this.currentStep = this.stepsList[Math.round(progress * (this.stepsList.length - 1))]
+      this.element.setAttribute('aria-valuetext', this.currentStep)
+    }
+  }
+
+  @Watch('progress')
+  progressChanged (progress: number): void {
+    this.setProgress(progress)
+  }
+
+  @Watch('steps')
+  stepsChanged (steps: string): void {
+    this.stepsList = steps.split(',')
+  }
+
   render () {
     return (
-      <Host class={this.direction}>
+      <Host aria-valuemax="100" aria-valuemin="0" aria-valuenow={ Math.round(this.progress * 100) } role="progressbar">
         <div class="progress" style={
           this.direction === 'horizontal'
             ? { flexGrow: `${this.progress}` }
