d0f29cb4ca6662d38148ca70e37aa75544423028
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-11-03 17:38:24 +0100

    fix(mds-entity): add aria features

--- projects/stencil/src/components/mds-entity/test/mds-entity.stories.tsx
+++ projects/stencil/src/components/mds-entity/test/mds-entity.stories.tsx
@@ -26,23 +26,23 @@ export default {
 }
 
 const Template = args =>
-  <mds-entity {...args}>
-    <mds-text typography="h6">Mario Rossi</mds-text>
-    <mds-badge slot="detail" variant="orange" tone="quiet">good</mds-badge>
-    <mds-text slot="detail" typography="caption">mario@nintendo.com</mds-text>
+  <mds-entity {...args} aria-label="Personaggio">
+    <mds-text aria-label="Nome" typography="h6">Mario Rossi</mds-text>
+    <mds-badge aria-label="Carattere" slot="detail" variant="orange" tone="quiet">buono</mds-badge>
+    <mds-text aria-label="Email" slot="detail" typography="caption">mario@nintendo.com</mds-text>
   </mds-entity>
 
 const TemplateWario = args =>
-  <mds-entity {...args}>
-    <mds-text typography="h6">Wario</mds-text>
-    <mds-badge slot="detail" variant="violet" tone="quiet">evil</mds-badge>
-    <mds-text slot="detail" typography="caption">wario@nintendo.com</mds-text>
+  <mds-entity {...args} aria-label="Personaggio">
+    <mds-text aria-label="Nome" typography="h6">Wario</mds-text>
+    <mds-badge aria-label="Carattere" slot="detail" variant="violet" tone="quiet">cattivo</mds-badge>
+    <mds-text aria-label="Email" slot="detail" typography="caption">wario@nintendo.com</mds-text>
   </mds-entity>
 
 const TemplateLocation = args =>
-  <mds-entity {...args}>
-    <mds-text typography="h6">Maggioli Headquarters</mds-text>
-    <mds-text slot="detail" typography="caption">Via Pinelli 64, Santarcangelo di Romanga, Italy</mds-text>
+  <mds-entity {...args} aria-label="Luogo">
+    <mds-text aria-label="Nome" typography="h6">Maggioli Headquarters</mds-text>
+    <mds-text aria-label="Strada" slot="detail" typography="caption">Via Pinelli 64, Santarcangelo di Romanga, Italy</mds-text>
   </mds-entity>
 
 export const Default = Template.bind({})
