ea1df487b9fa5612ee742178e7399631f42ebd98
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-11-03 17:41:21 +0100

    fix(mds-chip): fix some accessibility features

--- projects/stencil/src/components/mds-chip/mds-chip.tsx
+++ projects/stencil/src/components/mds-chip/mds-chip.tsx
@@ -105,7 +105,7 @@ export class MdsChip {
         <mds-text class="label" tabindex="0" typography="caption" truncate={true}>
           { this.label }
         </mds-text>
-        { this.deletable && <i aria-label={ this.deleteLabel } class="svg delete" innerHTML={miBaselineCancel} onClick={this.onDeleteHandler.bind(this)} role="button" tabindex="0" title={ `${this.deleteLabel} ${this.label}` }/> }
+        { this.deletable && <i class="svg delete" innerHTML={miBaselineCancel} onClick={this.onDeleteHandler.bind(this)} role="button" tabindex="0" title={ `${this.deleteLabel} ${this.label}` }/> }
       </Host>
     )
   }
