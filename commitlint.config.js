2812ebe2e47cadb9de4783e6f98eac48592c8945
Andrea Pruccoli <andrea.pruccoli@maggioli.it>
2023-01-16 12:58:42 +0100

    feat(lit): add first lit project (still wip)

--- commitlint.config.js
+++ commitlint.config.js
@@ -37,6 +37,7 @@ module.exports = {
         'design-tokens',
         'icons',
         'identity',
+        'lit',
         'magma',
         'react',
         'stencil',
---
+++ projects/lit/mds-input/src/mds-input.ts
@@ -0,0 +1,320 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property, query } from 'lit/decorators.js'
+import clsx from 'clsx'
+
+type InputValueType =
+  | null
+  | number
+  | string
+  | undefined
+
+type InputTextType =
+  | 'date'
+  | 'email'
+  | 'number'
+  | 'password'
+  | 'search'
+  | 'tel'
+  | 'text'
+  | 'textarea'
+  | 'time'
+  | 'url'
+
+type AutocompleteType =
+  | 'additional-name'
+  | 'address'
+  | 'address-level1'
+  | 'address-level2'
+  | 'address-level3'
+  | 'address-level4'
+  | 'address-line1'
+  | 'address-line2'
+  | 'address-line3'
+  | 'bday'
+  | 'bday-day'
+  | 'bday-month'
+  | 'bday-year'
+  | 'cc-additional-name'
+  | 'cc-csc'
+  | 'cc-exp'
+  | 'cc-exp-month'
+  | 'cc-exp-year'
+  | 'cc-family-name'
+  | 'cc-given-name'
+  | 'cc-name'
+  | 'cc-number'
+  | 'cc-type'
+  | 'country'
+  | 'country-name'
+  | 'current-password'
+  | 'email'
+  | 'family-name'
+  | 'given-name'
+  | 'honorific-prefix'
+  | 'honorific-suffix'
+  | 'impp'
+  | 'language'
+  | 'name'
+  | 'new-password'
+  | 'nickname'
+  | 'off'
+  | 'on'
+  | 'one-time-code'
+  | 'organization'
+  | 'organization-title'
+  | 'photo'
+  | 'postal-code'
+  | 'sex'
+  | 'street-address'
+  | 'tel'
+  | 'tel-area-code'
+  | 'tel-country-code'
+  | 'tel-extension'
+  | 'tel-local'
+  | 'tel-national'
+  | 'transaction-amount'
+  | 'transaction-currency'
+  | 'url'
+  | 'username'
+
+type ThemeStatusVariantType =
+  | 'error'
+  | 'info'
+  | 'success'
+  | 'warning'
+
+@customElement('mds-input')
+export class MdsInput extends LitElement {
+
+  static formAssociated = true
+
+  private internals = this.attachInternals()
+
+  private tabindex?: number
+
+  static styles = css``
+
+  /**
+   * Specifies whether the element should have autocomplete enabled
+   */
+  @property() readonly autocomplete?: AutocompleteType = 'off'
+
+  /**
+   * Specifies that the element should automatically get focus when the page loads
+   */
+  @property() readonly autofocus: boolean = false
+
+  /**
+   * A list of search terms to be searched from the input field,
+   * it should be used with type="search" input.
+   */
+  @property() readonly datalist?: string[]
+
+  /**
+   * If true, the element is displayed as disabled
+   */
+  @property() readonly disabled?: boolean = false
+
+  /**
+   * An icon displayed at the right of the input
+   */
+  @property() readonly icon?: string
+
+  /**
+   * Specifies the maximum value
+   * use it with input type="number" or type="date"
+   * Example: max="180", max="2046-12-04"
+   */
+  @property() readonly max?: number
+
+  /**
+   * Specifies the maximum number of characters allowed in an element
+   * use it with input type="number"
+   */
+  @property() readonly maxlength?: number
+
+  /**
+   * Specifies the minimum value
+   * use it with input type="number" or type="date"
+   * Example: min="-3", min="1988-04-15"
+   */
+  @property() readonly min?: string
+
+  /**
+   * Specifies the minimum number of characters allowed in an element
+   * use it with input type="number"
+   */
+  @property() readonly minlength?: number
+
+  /**
+   * Is needed to reference the form data after the form is submitted
+   */
+  @property() readonly name?: string
+
+  /**
+   * Specifies a regular expression that element\'s value is checked against
+   */
+  @property() readonly pattern?: string
+
+  /**
+   * Specifies a short hint that describes the expected value of the element
+   */
+  @property() readonly placeholder?: string
+
+  /**
+   * Specifies that the element is read-only
+   */
+  @property() readonly readonly?: boolean = false
+
+  /**
+   * Specifies that the element must be filled out before submitting the form
+   */
+  @property() readonly required?: boolean = false
+
+  /**
+   * Sets the variant of the input field
+   */
+  @property({ reflect: true }) readonly variant?: ThemeStatusVariantType
+
+  /**
+   * Sets the word(s) of the variant of the input field
+   */
+  @property() readonly variantTip?: string
+
+  /**
+   * Specifies the interval between legal numbers in an input field
+   */
+  @property() readonly step?: string
+
+  /**
+   * Specifies the type of input element
+   */
+  @property() readonly type: InputTextType = 'text'
+
+  /**
+   * Specifies the value of the input element
+   */
+  @property({ reflect: true }) value?: InputValueType
+
+  @query('.input') private nativeInput?: HTMLInputElement | HTMLTextAreaElement
+
+  private getValue (): string {
+    return typeof this.value === 'number' ? this.value.toString() : (this.value || '').toString()
+  }
+
+  private onInput = (ev: InputEvent) => {
+    // const input = ev.target as HTMLInputElement | HTMLTextAreaElement | false
+    // if (input) {
+    //   this.value = input.value || ''
+    // }
+    // this.keyDownEvent.emit(ev as KeyboardEvent)
+    console.log('Fubbe ad esserci un input', (ev.target as HTMLInputElement | HTMLTextAreaElement).value)
+  }
+
+  private onBlur = () => {
+    // this.hasFocus = false
+    // this.blurEvent.emit()
+    console.log('Fubbe ad esserci un blur')
+  }
+
+  private onFocus = (ev: Event) => {
+    // const input = ev.target as HTMLInputElement | HTMLTextAreaElement
+    // this.hasFocus = true
+    // this.focusEvent.emit()
+    // if (this.readonly) {
+    //   // setTimeout to avoid Safari 14.1.2
+    //   // to unselect text when mouse is clicked slowly
+    //   setTimeout(() => {
+    //     input.select()
+    //   }, 10)
+    // }
+    console.log('Fubbe ad esserci un focus', ev, this.nativeInput)
+  }
+
+  private buildInput (value: string) {
+    let input
+    if (this.type === 'textarea') {
+      input = html`
+        <textarea
+          class=${clsx('input', this.icon && 'has-icon')}
+          .value=${value}
+          .maxlength=${this.maxlength}
+          .minlength=${this.minlength}
+          .name=${this.name}
+          .placeholder=${this.placeholder || ''}
+          .tabindex=${this.tabindex}
+          ?autofocus=${this.autofocus}
+          ?disabled=${this.disabled}
+          ?readonly=${this.readonly}
+          ?required=${this.required}
+          @blur=${this.onBlur}
+          @focus=${this.onFocus}
+          @input=${this.onInput}>
+        </textarea>
+      `
+    } else {
+      input = html`
+        <input
+          class=${clsx('input', this.icon && 'has-icon')}
+          .max=${this.max}
+          .maxlength=${this.maxlength}
+          .min=${this.min}
+          .minlength=${this.minlength}
+          .name=${this.name}
+          .type=${this.type}
+          .pattern=${this.pattern}
+          .placeholder=${this.placeholder || ''}
+          .step=${this.step}
+          .tabindex=${this.tabindex}
+          .value=${value}
+          ?autocomplete=${this.autocomplete}
+          ?autofocus=${this.autofocus}
+          ?disabled=${this.disabled}
+          ?readonly=${this.readonly}
+          ?required=${this.required}
+          @blur=${this.onBlur}
+          @focus=${this.onFocus}
+          @input=${this.onInput}
+        />
+      `
+    }
+
+    return input
+  }
+
+  render () {
+    const value = this.getValue()
+    const input = this.buildInput(value)
+    return html`
+      ${input}
+      ${this.required ? html`
+        <mds-text typography="option" class="tip top-1 required">Obbligatorio</mds-text>
+      ` : ''}
+      ${this.disabled ? html`
+        <mds-text typography="option" class="tip top-1 disabled">Disabilitato</mds-text>
+      ` : ''}
+      ${this.readonly ? html`
+        <mds-text typography="option" class="tip top-1 read-only">Sola lettura</mds-text>
+      ` : ''}
+      ${this.variant && this.variantTip ? html`
+        <mds-text typography="option" class="tip-variant bottom-1">${this.variantTip}</mds-text>
+      ` : ''}
+      ${this.datalist ? html`
+        <datalist id="datalist" class="datalist">
+          ${this.datalist.map(element => {
+            html`<option value=${element} />`
+          })}
+        </datalist>
+      ` : ''}
+      ${this.icon ? html`
+        <mds-icon class=${clsx('icon', this.variant)} name=${this.icon}/>
+      ` : ''}
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mds-input': MdsInput
+  }
+}
