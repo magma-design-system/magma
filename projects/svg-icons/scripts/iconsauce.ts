d3a0338894c341ab845000acf5826675f137f6fc
Vittorio Vittori <vittorio.vittori@maggioli.it>
2023-02-13 14:29:03 +0100

    feat(svg-icons): add dictionary generator for iconsauce

---
+++ projects/svg-icons/scripts/iconsauce.ts
@@ -0,0 +1,46 @@
+import chalk from 'chalk'
+import fg from 'fast-glob'
+import { DIST_DIR, ICONS_DIR } from './meta'
+import { PathLike } from 'fs'
+import { basename, join } from 'path'
+import { logFileSavedTo } from '../../../scripts/log'
+import { writeFile, mkdir } from 'fs/promises'
+
+const DICTIONARY_FILENAME = 'iconsauce.json'
+
+const selector = (file: PathLike): string =>
+  `mgg/${basename(file.toString()).replace('.svg', '')}`
+
+const dictionary = (icons: PathLike[]): string[] => {
+  const iconsDictionary: Array<string> = []
+  for (const icon of icons) {
+    iconsDictionary.push(selector(icon).toLowerCase())
+  }
+  return iconsDictionary
+}
+
+
+const dumpIconsauceIcons = async (): Promise<string[]> => {
+  const entries = await fg(`${ICONS_DIR.toString()}/*.svg`, { dot: true })
+  return dictionary(entries)
+}
+
+const buildDictionary = async (): Promise<void> => {
+  const icons = await dumpIconsauceIcons()
+  const jsonDictionary = JSON.stringify(icons, null, 4)
+
+  await mkdir(DIST_DIR)
+    .catch(error => {
+      throw Error(chalk.red(error))
+    })
+
+  await writeFile(join(DIST_DIR, DICTIONARY_FILENAME), jsonDictionary, { encoding: 'utf8' })
+    .then(() => {
+      logFileSavedTo(DICTIONARY_FILENAME, join(DIST_DIR, DICTIONARY_FILENAME))
+    })
+    .catch(error => {
+      throw Error(chalk.red(error))
+    })
+}
+
+buildDictionary()
---
+++ projects/svg-icons/scripts/meta.ts
@@ -0,0 +1,10 @@
+import path from 'path'
+const PROJECT_DIR = path.resolve(__dirname, '../')
+const DIST_DIR = path.resolve(PROJECT_DIR, 'dist')
+const ICONS_DIR = path.resolve(PROJECT_DIR, 'svg')
+
+export {
+  DIST_DIR,
+  ICONS_DIR,
+  PROJECT_DIR,
+}
