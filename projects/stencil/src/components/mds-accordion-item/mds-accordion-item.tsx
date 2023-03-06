3f9c57714e8332e0d2bd1c6061d9e7aacaeda929
Vittorio Vittori <vittorio.vittori@maggioli.it>
2023-03-06 16:16:18 +0100

    change(mds-accordion-item): change event and attribute names to new name conventions

--- projects/stencil/src/components/mds-accordion-item/mds-accordion-item.tsx
+++ projects/stencil/src/components/mds-accordion-item/mds-accordion-item.tsx
@@ -35,7 +35,7 @@ export class MdsAccordionItem {
   /**
    * Emits when the accordion is opened
    */
-  @Event({ eventName: 'opened' }) openedEvent: EventEmitter<AccordionClickedEvent>
+  @Event({ eventName: 'mdsOpen' }) openedEvent: EventEmitter<AccordionClickedEvent>
 
   render () {
     return (
