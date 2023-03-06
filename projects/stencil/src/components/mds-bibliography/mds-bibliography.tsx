1e6d2e3c943a93b11e43d6a7a4dee30ee5a206d8
Vittorio Vittori <vittorio.vittori@maggioli.it>
2023-03-06 16:59:13 +0100

    chore(mds-bibliography): update depdendencies

--- projects/stencil/src/components/mds-bibliography/mds-bibliography.tsx
+++ projects/stencil/src/components/mds-bibliography/mds-bibliography.tsx
@@ -1,6 +1,6 @@
 import { Component, Host, h, Prop } from '@stencil/core'
 import { BibliographyFormatType, BibliographyRelationshipType } from './meta/types'
-import { TypographyInfoType, TypographyReadType, TypographyVariants } from '../../types/typography'
+import { TypographyInfoType, TypographyReadType, TypographyVariants } from '@type/typography'
 import { FormattedAuthor } from './meta/interface'
 @Component( {
   tag: 'mds-bibliography',
