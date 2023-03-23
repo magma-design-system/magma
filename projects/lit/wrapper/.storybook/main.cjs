be4dfc8de2e2f52da5ef6cf5c4a8f7f4f6431963
Andrea Pruccoli <andrea.pruccoli@maggioli.it>
2023-03-23 12:46:06 +0100

    chore(lit): add first level component to place storybook

---
+++ projects/lit/wrapper/.storybook/main.cjs
@@ -0,0 +1,21 @@
+const { mergeConfig } = require('vite')
+
+module.exports = {
+  "stories": [
+    "../../components/mds-*/src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/web-components",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {})
+  }
+}
