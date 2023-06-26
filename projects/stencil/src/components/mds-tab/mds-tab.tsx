536f54b6d6d7367af84edadfa5d65833f52349a8
Vittorio Vittori <vittorio.vittori@maggioli.it>
2023-06-26 17:01:56 +0200

    change(mds-tab): remove automatic content handling

--- projects/stencil/src/components/mds-tab/mds-tab.tsx
+++ projects/stencil/src/components/mds-tab/mds-tab.tsx
@@ -1,8 +1,5 @@
-import { cssDurationToMilliseconds } from '@common/unit'
-import { Component, Element, Event, EventEmitter, Host, Listen, Prop, Watch, h } from '@stencil/core'
-import clsx from 'clsx'
+import { Component, Element, Event, EventEmitter, Host, Listen, h } from '@stencil/core'
 import { MdsTabEventDetail } from './meta/event-detail'
-import { DirectionType, StrategyType } from './meta/type'
 
 @Component({
   tag: 'mds-tab',
@@ -13,25 +10,11 @@ export class MdsTab {
 
   @Element() private element: HTMLMdsTabElement
   private currentItem: number
-  private previousItem = -1
-  private cssAnimationDuration: number
-  private contentsHeightTimeout: NodeJS.Timeout
-  private moveItemsTimeout: NodeJS.Timeout
   private lastKnownContentsScrollX: number
   private contents: HTMLElement
   private tabs: HTMLElement
   private ticking: boolean
   private content: NodeListOf<HTMLElement>
-
-  /**
-   * Sets component's contents to be swappable on mobile devices, this will result in forcing direction attribute to be se to 'natural'
-   */
-  @Prop({ reflect: true }) readonly strategy: StrategyType = 'default'
-
-  /**
-   * Sets the direction where tab contents swipes
-   */
-  @Prop({ mutable: true, reflect: true }) direction: DirectionType = 'natural'
 
   /**
    * Emits when a children is changed
@@ -40,112 +23,6 @@ export class MdsTab {
 
   private queryItems = (): NodeListOf<HTMLMdsTabItemElement> =>
     this.element.querySelectorAll<HTMLMdsTabItemElement>('mds-tab-item')
-
-  private attachContents = (): void => {
-    const items = this.queryItems()
-    this.contents = this.element.shadowRoot?.querySelector('.contents') as HTMLElement
-    const contentsShadow = this.contents.attachShadow({ mode: 'open', slotAssignment: 'manual' })
-    const slot = document.createElement('slot')
-    contentsShadow.append(slot)
-    const w : HTMLDivElement[] = []
-    if (items) {
-      items.forEach((el: Element) => {
-        const wrapper = document.createElement('div')
-        wrapper.classList.add('content')
-        el.childNodes.forEach(node => wrapper.appendChild(node.cloneNode(true)))
-        this.contents.appendChild(wrapper)
-        w.push(wrapper)
-      })
-      slot.assign(...w)
-
-      this.content = this.element.shadowRoot?.querySelectorAll('.content') as NodeListOf<HTMLElement>
-      this.checkStrategyContent()
-    }
-  }
-
-  private checkStrategyContent = (): void => {
-    if (this.strategy === 'default') {
-      this.content.forEach((el: Element) => {
-        el.classList.add('content--absolute')
-      })
-      return
-    }
-
-    this.content.forEach((el: Element) => {
-      el.classList.remove('content--absolute')
-    })
-  }
-
-  private getCurrentItemHeight = (): number => {
-    return (this.content[this.currentItem] as HTMLElement).offsetHeight
-  }
-
-  private setContentsHeight = (height: number): void => {
-
-    this.contents.style.setProperty('--mds-tab-contents-height', `${this.contents.offsetHeight}px`)
-    setTimeout(() => {
-      this.contents.style.setProperty('--mds-tab-contents-height', `${height}px`)
-    }, 1)
-
-    if (this.previousItem >= 0) {
-      this.content[this.previousItem].classList.add('content--absolute')
-    }
-    this.content[this.currentItem].classList.remove('content--absolute')
-
-    this.contentsHeightTimeout = setTimeout(() => {
-      this.resetHeight()
-    }, this.cssAnimationDuration)
-  }
-
-  private moveItems = (nextItemDirection: 'left' | 'right'): void => {
-    const prevItemDirection = nextItemDirection === 'left' ? 'right' : 'left'
-
-    if (this.previousItem >= 0) {
-      this.content[this.previousItem].classList.add(`content--animate-to-${prevItemDirection}`)
-    }
-    this.content[this.currentItem].classList.add(`content--animate-from-${nextItemDirection}`)
-
-    this.moveItemsTimeout = setTimeout(() => {
-      this.resetAnimations()
-    }, this.cssAnimationDuration)
-  }
-
-  private resetHeight = (): void => {
-    this.contents.style.setProperty('--mds-tab-contents-height', 'auto')
-    this.content[this.currentItem].classList.remove('content--absolute')
-    if (this.previousItem >= 0) {
-      this.content[this.previousItem].classList.remove('content--hidden')
-    }
-  }
-
-  private resetAnimations = (): void => {
-    this.content.forEach((el: HTMLElement) => {
-      el.classList.remove('content--animate-to-left')
-      el.classList.remove('content--animate-to-right')
-      el.classList.remove('content--animate-from-left')
-      el.classList.remove('content--animate-from-right')
-    })
-  }
-
-  private swipeContent = (): void => {
-    this.setContentsHeight(this.getCurrentItemHeight())
-    if (this.previousItem === this.currentItem) {
-      return
-    }
-    if (this.previousItem > this.currentItem) {
-      if (this.direction === 'natural') {
-        this.moveItems('left')
-        return
-      }
-      this.moveItems('right')
-      return
-    }
-    if (this.direction === 'natural') {
-      this.moveItems('right')
-      return
-    }
-    this.moveItems('left')
-  }
 
   componentWillLoad (): void {
     const items = this.queryItems()
@@ -161,20 +38,15 @@ export class MdsTab {
 
   componentDidLoad (): void {
     this.tabs = this.element.shadowRoot?.querySelector('.tabs') as HTMLElement
-    this.attachContents()
     if (this.content.length > 0) {
-      this.handleStrategyAttribute()
+      this.contents.addEventListener('scrollend', this.changeHeightOnScrollEnd.bind(this))
+      this.contents.addEventListener('scroll', this.findCurrentItem.bind(this))
     }
   }
 
   disconnectedCallback (): void {
     this.contents.removeEventListener('scrollend', this.changeHeightOnScrollEnd.bind(this))
     this.contents.removeEventListener('scroll', this.findCurrentItem.bind(this))
-  }
-
-  private updateCSSCustomProps (): void {
-    const elementStyles = window.getComputedStyle(this.element)
-    this.cssAnimationDuration = cssDurationToMilliseconds(elementStyles.getPropertyValue('--mds-tab-duration'))
   }
 
   private scrollTabs = (): void => {
@@ -223,38 +95,9 @@ export class MdsTab {
     this.contents.scrollLeft = contentBox.width * this.currentItem
   }
 
-  @Watch('strategy')
-  handleStrategyAttribute (): void {
-    if (this.strategy === 'default') {
-      this.updateCSSCustomProps()
-      this.swipeContent()
-      this.contents.removeEventListener('scrollend', this.changeHeightOnScrollEnd.bind(this))
-      this.contents.removeEventListener('scroll', this.findCurrentItem.bind(this))
-      return
-    }
-
-    this.contents.addEventListener('scrollend', this.changeHeightOnScrollEnd.bind(this))
-    this.contents.addEventListener('scroll', this.findCurrentItem.bind(this))
-    this.direction = 'natural'
-    this.checkStrategyContent()
-  }
-
   @Listen('mdsTabItemSelect')
   changeEventHandler (event: CustomEvent<string>): void {
     const items = this.queryItems()
-
-    if (this.strategy === 'default') {
-      clearTimeout(this.contentsHeightTimeout)
-      clearTimeout(this.moveItemsTimeout)
-      this.resetAnimations()
-      this.resetHeight()
-    }
-
-    items.forEach((item, key) => {
-      if (item.selected) {
-        this.previousItem = key
-      }
-    })
 
     // since the external developer can define a custom id
     // we must find the key from event.detail
@@ -266,11 +109,6 @@ export class MdsTab {
       }
     })
 
-    if (this.strategy === 'default') {
-      this.swipeContent()
-      return
-    }
-
     this.scrollToContent()
   }
 
@@ -280,7 +118,8 @@ export class MdsTab {
         <div class="tabs" part="tabs">
           <slot />
         </div>
-        <div class={clsx('contents', this.strategy === 'scroll' && 'contents--scroll')} part="contents">
+        <div class="contents" part="contents">
+          <slot name="content"/>
         </div>
       </Host>
     )
--- projects/stencil/src/components/mds-tab/test/mds-tab.stories.tsx
+++ projects/stencil/src/components/mds-tab/test/mds-tab.stories.tsx
@@ -22,10 +22,9 @@ export default {
 const TemplateEmpty = args =>
   <div class="grid gap-6">
     <mds-tab {...args}>
-      <mds-notification strategy="absolute" target="button" value={14} visible={true} />
-      <mds-tab-item selected class="mobile:flex-1" label="First Blood"></mds-tab-item>
-      <mds-tab-item icon="mdi/alien" id="button" class="mobile:flex-1" label="Double Impact"></mds-tab-item>
-      <mds-tab-item class="mobile:flex-1" label="The Third Man"></mds-tab-item>
+      <mds-tab-item selected>First Blood</mds-tab-item>
+      <mds-tab-item icon="mdi/alien" id="button">Double Impact</mds-tab-item>
+      <mds-tab-item>The Third Man</mds-tab-item>
     </mds-tab>
     <mds-hr />
     <div class="grid gap-1">
@@ -37,47 +36,49 @@ const TemplateEmpty = args =>
 const Template = args =>
   <div class="grid gap-6">
     <mds-tab {...args}>
-      <mds-notification strategy="absolute" target="button" value={14} visible={true} />
-      <mds-tab-item selected class="mobile:flex-1" label="First Blood">
-        <div class="py-6 px-2 grid tablet:grid-cols-[1fr_2fr] gap-6">
-          <mds-img class="rounded-lg" src='./movie-first-blood.jpg' />
-          <div class="grid gap-1 auto-rows-min">
-            <mds-text typography='h4'>First Blood</mds-text>
-            <mds-text>
-              Vietnam veteran and drifter John J. Rambo (Sylvester Stallone) wanders into a small Washington town in search of an old friend, but is met with intolerance and brutality by the local sheriff, Will Teasle (Brian Dennehy). When Teasle and his deputies restrain and shave Rambo, he flashes back to his time as a prisoner of war and unleashes his fury on the officers. He narrowly escapes the manhunt, but it will take his former commander (Richard Crenna) to save the hunters from the hunted.
-            </mds-text>
-          </div>
-        </div>
+      <mds-tab-item selected>
+        First Blood
       </mds-tab-item>
-      <mds-tab-item icon="mdi/alien" id="button" class="mobile:flex-1" label="Double Impact">
-        <div class="py-6 px-2 grid tablet:grid-cols-[1fr_2fr] gap-6">
-          <div class="auto-rows-min grid gap-2">
-            <mds-img class="rounded-lg" src='./movie-double-impact.jpg' />
-            <mds-img class="rounded-lg" src='./movie-double-impact-shot-01.jpg' />
-            <mds-img class="rounded-lg" src='./movie-double-impact-shot-02.jpg' />
-          </div>
-          <div class="grid gap-1 auto-rows-min">
-            <mds-text typography='h4'>Double Impact</mds-text>
-            <mds-text>
-              Nearly 25 years after seeing his father killed by Hong Kong crime boss Raymond Zhang (Philip Chan Yan Kin), Chad Wagner (Jean-Claude Van Damme) is living in Los Angeles as a flourishing yet peaceful martial arts trainer. But Chad has a twin brother, Alex (also Van Damme), who suddenly reunites with his less-aggressive sibling and wants to avenge their father's death. As the pair plan their payback against Zhang, they also struggle to overcome their personal differences.
-            </mds-text>
-          </div>
-        </div>
+      <mds-tab-item icon="mdi/alien" id="button">
+        Double Impact
       </mds-tab-item>
-      <mds-tab-item class="mobile:flex-1" label="The Third Man">
-        <div class="py-6 px-2 grid tablet:grid-cols-[1fr_2fr] gap-6">
-          <div class="auto-rows-min grid gap-2">
-            <mds-img class="rounded-lg" src='./movie-the-third-man.jpg' />
-            <mds-img class="rounded-lg" src='./movie-the-third-man-shot.jpg' />
-          </div>
-          <div class="grid gap-1 auto-rows-min">
-            <mds-text typography='h4'>The Third Man</mds-text>
-            <mds-text>
-              Set in postwar Vienna, Austria, "The Third Man" stars Joseph Cotten as Holly Martins, a writer of pulp Westerns, who arrives penniless as a guest of his childhood chum Harry Lime (Orson Welles), only to find him dead. Martins develops a conspiracy theory after learning of a "third man" present at the time of Harry's death, running into interference from British officer Maj. Calloway (Trevor Howard) and falling head-over-heels for Harry's grief-stricken lover, Anna (Alida Valli).
-            </mds-text>
-          </div>
-        </div>
+      <mds-tab-item>
+        The Third Man
       </mds-tab-item>
+      <div slot="content" class="py-6 px-2 grid tablet:grid-cols-[1fr_2fr] gap-6">
+        <mds-img class="rounded-lg" src='./movie-first-blood.jpg' />
+        <div class="grid gap-1 auto-rows-min">
+          <mds-text typography='h4'>First Blood</mds-text>
+          <mds-text>
+            Vietnam veteran and drifter John J. Rambo (Sylvester Stallone) wanders into a small Washington town in search of an old friend, but is met with intolerance and brutality by the local sheriff, Will Teasle (Brian Dennehy). When Teasle and his deputies restrain and shave Rambo, he flashes back to his time as a prisoner of war and unleashes his fury on the officers. He narrowly escapes the manhunt, but it will take his former commander (Richard Crenna) to save the hunters from the hunted.
+          </mds-text>
+        </div>
+      </div>
+      <div slot="content" class="py-6 px-2 grid tablet:grid-cols-[1fr_2fr] gap-6">
+        <div class="auto-rows-min grid gap-2">
+          <mds-img class="rounded-lg" src='./movie-double-impact.jpg' />
+          <mds-img class="rounded-lg" src='./movie-double-impact-shot-01.jpg' />
+          <mds-img class="rounded-lg" src='./movie-double-impact-shot-02.jpg' />
+        </div>
+        <div class="grid gap-1 auto-rows-min">
+          <mds-text typography='h4'>Double Impact</mds-text>
+          <mds-text>
+            Nearly 25 years after seeing his father killed by Hong Kong crime boss Raymond Zhang (Philip Chan Yan Kin), Chad Wagner (Jean-Claude Van Damme) is living in Los Angeles as a flourishing yet peaceful martial arts trainer. But Chad has a twin brother, Alex (also Van Damme), who suddenly reunites with his less-aggressive sibling and wants to avenge their father's death. As the pair plan their payback against Zhang, they also struggle to overcome their personal differences.
+          </mds-text>
+        </div>
+      </div>
+      <div slot="content" class="py-6 px-2 grid tablet:grid-cols-[1fr_2fr] gap-6">
+        <div class="auto-rows-min grid gap-2">
+          <mds-img class="rounded-lg" src='./movie-the-third-man.jpg' />
+          <mds-img class="rounded-lg" src='./movie-the-third-man-shot.jpg' />
+        </div>
+        <div class="grid gap-1 auto-rows-min">
+          <mds-text typography='h4'>The Third Man</mds-text>
+          <mds-text>
+            Set in postwar Vienna, Austria, "The Third Man" stars Joseph Cotten as Holly Martins, a writer of pulp Westerns, who arrives penniless as a guest of his childhood chum Harry Lime (Orson Welles), only to find him dead. Martins develops a conspiracy theory after learning of a "third man" present at the time of Harry's death, running into interference from British officer Maj. Calloway (Trevor Howard) and falling head-over-heels for Harry's grief-stricken lover, Anna (Alida Valli).
+          </mds-text>
+        </div>
+      </div>
     </mds-tab>
     <mds-hr />
     <div class="grid gap-1">
