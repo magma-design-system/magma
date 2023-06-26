af9ec087d4b65ae45d26fac0edc6e6a925303438
Vittorio Vittori <vittorio.vittori@maggioli.it>
2023-06-26 17:02:26 +0200

    change(mds-tab-item): remove automatic content handling

--- projects/stencil/src/components/mds-tab-item/mds-tab-item.tsx
+++ projects/stencil/src/components/mds-tab-item/mds-tab-item.tsx
@@ -13,11 +13,6 @@ export class MdsTabItem {
   @State() isSelected: boolean
 
   /**
-   * Specifies the tab button item label
-   */
-  @Prop() readonly label?: string
-
-  /**
    * Specifies if the tab item is selected or not
    */
   @Prop({ reflect: true }) readonly selected?: boolean
@@ -69,9 +64,8 @@ export class MdsTabItem {
           size={this.size}
           type={this.type}
         >
-          {this.label}
+          <slot/>
         </mds-button>
-        <div hidden><slot /></div>
       </Host>
     )
   }
