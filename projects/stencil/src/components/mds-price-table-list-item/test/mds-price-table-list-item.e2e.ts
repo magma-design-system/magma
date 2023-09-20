b4053db04f9cbc704863de36254e892c5447b90f
Vittorio Vittori <vittorio.vittori@maggioli.it>
2023-09-20 18:26:24 +0200

    feat(mds-price-table-list-item): add new component

---
+++ projects/stencil/src/components/mds-price-table-list-item/test/mds-price-table-list-item.e2e.ts
@@ -0,0 +1,11 @@
+import { newE2EPage } from '@stencil/core/testing'
+
+describe('mds-price-table-list-item', () => {
+  it('renders', async () => {
+    const page = await newE2EPage()
+    await page.setContent('<mds-price-table-list-item></mds-price-table-list-item>')
+
+    const element = await page.find('mds-price-table-list-item')
+    expect(element).toHaveClass('hydrated')
+  })
+})
