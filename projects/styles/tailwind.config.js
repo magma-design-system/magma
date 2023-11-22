bf61547ce7472ca1c64df1b7ae01b21a236d22ef
Vittorio Vittori <vittorio.vittori@maggioli.it>
2023-11-22 15:44:21 +0100

    change(styles): add tailwind based on magma tokens

--- projects/styles/tailwind.config.js
+++ projects/styles/tailwind.config.js
@@ -1,49 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { palette } = require('@maggioli-design-system/design-tokens')
-const mediaQueries = require('@maggioli-design-system/design-tokens/dist/js/media').media
 const { fontFamily } = require('@maggioli-design-system/design-tokens/dist/js/tailwind-font-family')
-const { fontSize } = require('@maggioli-design-system/design-tokens/dist/js/tailwind-font-size')
-const { ease } = require('@maggioli-design-system/design-tokens/dist/js/ease')
-const { gap } = require('@maggioli-design-system/design-tokens/dist/js/gap')
+const {
+  aspectRatio,
+  border,
+  borderRadius,
+  boxShadow,
+  fontSize,
+  gap,
+  gridTemplateColumns,
+  leading,
+  margin,
+  padding,
+  size,
+  timingFunction,
+  transitionProperty,
+} = require('@maggioli-design-system/design-tokens/dist/js/tailwind-props')
 
-const minSize = {
-  4: '1rem',
-  5: '1.25rem',
-  6: '1.5rem',
-  7: '1.75rem',
-  8: '2rem',
-  9: '2.25rem',
-  10: '2.5rem',
-  11: '2.75rem',
-  12: '3rem',
-  14: '3.5rem',
-  16: '4rem',
-  18: '4.5rem',
-  20: '5rem',
-  24: '6rem',
-  28: '7rem',
-  32: '8rem',
-  36: '9rem',
-  40: '10rem',
-  44: '11rem',
-  48: '12rem',
-  52: '13rem',
-  56: '14rem',
-  60: '15rem',
-  64: '16rem',
-  72: '18rem',
-  80: '20rem',
-  96: '24rem',
-  400: '400px',
-  500: '500px',
-  600: '600px',
-  700: '700px',
-  800: '800px',
-}
-
-const paddingAndMargin = {
-  13: '3.25rem',
-}
+const mediaQueries = require('@maggioli-design-system/design-tokens/dist/js/media').media
 
 module.exports = {
   important: true,
@@ -68,75 +42,50 @@ module.exports = {
       ...mediaQueries,
     },
     extend: {
-      boxShadow: {
-        'sharp-lg': '0 0 1px 1px rgb(0 0 0 / 0.06), 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -2px rgb(0 0 0 / 0.05)',
-        'sharp-md': '0 0 1px 1px rgb(0 0 0 / 0.06), 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -1px rgb(0 0 0 / 0.06)',
-        sharp: '0 0 1px 1px rgb(0 0 0 / 0.06), 0 1px 3px 0 rgb(0 0 0 / 0.1)',
-        'sharp-sm': '0 0 1px 1px rgb(0 0 0 / 0.06), 0 1px 2px 0 rgb(0 0 0 / 0.05)',
+      aspectRatio,
+      borderRadius,
+      borderWidth: {
+        ...border,
       },
-      fontSize: {
-        ...fontSize,
-        xxs: '0.625rem',
-      },
-      gap: {
-        ...gap,
-      },
+      boxShadow,
+      fontSize,
+      gap,
       gridAutoRows: {
         1: '1fr',
       },
-      gridTemplateColumns: {
-        'fit-lg': 'repeat(auto-fit, minmax(17.5rem, 1fr))',
-        'fit-md': 'repeat(auto-fit, minmax(15rem, 1fr))',
-        'fit-sm': 'repeat(auto-fit, minmax(10rem, 1fr))',
-        'fit-xl': 'repeat(auto-fit, minmax(20rem, 1fr))',
-        fit: 'repeat(auto-fit, minmax(12.5rem, 1fr))',
-        full: 'repeat(auto-fit, minmax(1px, 100%))',
+      gridTemplateColumns,
+      height: {
+        ...size,
+      },
+      lineHeight: {
+        leading,
+      },
+      maxHeight: {
+        ...size,
       },
       maxWidth: {
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%',
-        phrase: '50ch',
-        text: '75ch',
-        title: '30ch',
-      },
-      height: {
-        13: '3.25rem',
+        ...size,
       },
       minHeight: {
-        ...minSize,
+        ...size,
       },
       minWidth: {
-        ...minSize,
+        ...size,
       },
-      padding: {
-        '1/1': '100%',
-        '16/9': '56.25%',
-        '3/2': '66.67%',
-        '4/3': '75%',
-        '5/4': '80%',
-        ...paddingAndMargin,
-      },
-      margin: {
-        ...paddingAndMargin,
-      },
+      margin,
+      padding,
       overflow: {
         inherit: 'inherit',
       },
       textOverflow: {
         inherit: 'inherit',
       },
-      transitionProperty : {
-        cosmetic: 'background-color, border-color, box-shadow, color, fill, height, margin, opacity, padding, transform, width',
-        size: 'height, margin, padding, width',
-      },
+      transitionProperty,
       transitionTimingFunction: {
-        ...ease,
+        ...timingFunction,
       },
       width: {
-        phrase: '50ch',
-        text: '75ch',
-        title: '30ch',
+        ...size,
       },
     },
   },
