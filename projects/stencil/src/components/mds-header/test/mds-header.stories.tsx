72c2ccacdcb3899926a4217f3f195aa0977e300a
Nicola Tamburini <nicola.tamburini@maggioli.it>
2023-12-06 17:13:18 +0100

    perf(mds-header): avoid rerendering

--- projects/stencil/src/components/mds-header/test/mds-header.stories.tsx
+++ projects/stencil/src/components/mds-header/test/mds-header.stories.tsx
@@ -20,9 +20,9 @@ export default {
 const Template = args =>
   <mds-header {...args}>
     <mds-header-bar>
-      <div class="flex gap-2 items-center">
-        <mds-img class="w-10" src="./logo-gruppo-maggioli.svg" />
-        <div class="mb-1">
+      <div class="flex gap-200 items-center">
+        <mds-img class="w-1000" src="./logo-gruppo-maggioli.svg" />
+        <div class="mb-100">
           <mds-text typography="h6">Mobile menu</mds-text>
           <mds-text typography="option" class="text-tone-neutral-04">Shows up under 1024px</mds-text>
         </div>
@@ -31,14 +31,14 @@ const Template = args =>
       <mds-button slot="nav" icon="mi/round/person">Registrati</mds-button>
     </mds-header-bar>
     <div slot="menu">
-      <div class="flex gap-2 items-center p-6 border-b border-tone-neutral-09">
-        <mds-img class="w-10" src="./logo-gruppo-maggioli.svg" />
-        <div class="mb-1">
+      <div class="flex gap-200 items-center p-600 border-b border-tone-neutral-09">
+        <mds-img class="w-1000" src="./logo-gruppo-maggioli.svg" />
+        <div class="mb-100">
           <mds-text typography="h6">Gruppo Maggioli</mds-text>
           <mds-text typography="option" class="text-tone-neutral-04">Header by RD Team</mds-text>
         </div>
       </div>
-      <div class="grid gap-2 p-6">
+      <div class="grid gap-200 p-600">
         <mds-button variant="dark" tone="ghost">Accedi</mds-button>
         <mds-button icon="mi/round/person">Registrati</mds-button>
       </div>
