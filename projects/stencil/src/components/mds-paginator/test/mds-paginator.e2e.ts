79acc9f5bcc426917c6c348845d9c42cd622c5b5
Nicola Tamburini <nicola.tamburini@maggioli.it>
2023-12-11 16:36:57 +0100

    test(stencil): mock request for icon to not console error

--- projects/stencil/src/components/mds-paginator/test/mds-paginator.e2e.ts
+++ projects/stencil/src/components/mds-paginator/test/mds-paginator.e2e.ts
@@ -1,8 +1,10 @@
 import { newE2EPage } from '@stencil/core/testing'
+import { mockIconResponse } from '@test/mock'
 
 describe('mds-paginator', () => {
   it('renders', async () => {
     const page = await newE2EPage()
+    mockIconResponse(page)
     await page.setContent('<mds-paginator></mds-paginator>')
 
     const element = await page.find('mds-paginator')
