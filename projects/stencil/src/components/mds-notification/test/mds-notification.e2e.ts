9e586fb8e8953185951d2f010538e89971d25bda
Nicola Tamburini <nicola.tamburini@maggioli.it>
2023-12-11 16:38:32 +0100

    test(mds-notification): fix test

--- projects/stencil/src/components/mds-notification/test/mds-notification.e2e.ts
+++ projects/stencil/src/components/mds-notification/test/mds-notification.e2e.ts
@@ -3,9 +3,9 @@ import { newE2EPage } from '@stencil/core/testing'
 describe('mds-notification', () => {
   it('renders', async () => {
     const page = await newE2EPage()
-    await page.setContent('<mds-notification visible></mds-notification>')
+    await page.setContent(`<mds-notification target="my-button" visible></mds-notification>
+    <mds-button id="my-button">Incoming messages</mds-button>`)
     const element = await page.find('mds-notification')
     expect(element).toHaveAttribute('hydrated')
-    expect(true).toBe(true)
   })
 })
