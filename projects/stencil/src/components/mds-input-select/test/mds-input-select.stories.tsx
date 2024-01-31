b982ecf77e7f62d8395f4c424d3ee923a4b76a36
Vittorio Vittori <vittorio.vittori@maggioli.it>
2024-01-31 15:31:52 +0100

    refact(mds-input-select): remove options as object string

--- projects/stencil/src/components/mds-input-select/test/mds-input-select.stories.tsx
+++ projects/stencil/src/components/mds-input-select/test/mds-input-select.stories.tsx
@@ -12,9 +12,14 @@ export default {
 }
 
 const Template = args =>
-  <mds-input-select {...args} options={JSON.stringify(cities)}/>
+  <mds-input-select {...args}>
+    <option value="1">First contact</option>
+    <option value="2">Second impact</option>
+    <option value="3">The Third Man</option>
+    <option value="4">The Fourth Emendament</option>
+  </mds-input-select>
 
 export const Default = Template.bind({})
 Default.args = {
-  placeholder: 'Select an item...',
+  placeholder: 'Seleziona un film...',
 }
