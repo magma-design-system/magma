3fcfb4e0d518423f9f87ee2fd4843b14dd265a26
Nicola Tamburini <nicola.tamburini@maggioli.it>
2024-02-01 17:30:50 +0100

    test(stencil): fix test

--- projects/stencil/src/components/mds-input-upload/test/mds-input-upload.e2e.ts
+++ projects/stencil/src/components/mds-input-upload/test/mds-input-upload.e2e.ts
@@ -3,9 +3,9 @@ import { newE2EPage } from '@stencil/core/testing'
 describe('mds-input-upload', () => {
   it('renders', async () => {
     const page = await newE2EPage()
-    await page.setContent('<mds-input-upload></mds-input-upload>')
+    await page.setContent('<mds-input-upload accept=""></mds-input-upload>')
 
     const element = await page.find('mds-input-upload')
-    expect(element).toHaveClass('hydrated')
+    expect(element).toHaveAttribute('hydrated')
   })
 })
