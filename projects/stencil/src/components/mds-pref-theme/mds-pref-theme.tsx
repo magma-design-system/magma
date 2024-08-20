d4da5e136215ba0713f7a4a477e079d25cf720b3
Vittorio Vittori <vittorio.vittori@maggioli.it>
2024-08-20 14:13:27 +0200

    refact(mds-pref-theme): rename locale languages

--- projects/stencil/src/components/mds-pref-theme/mds-pref-theme.tsx
+++ projects/stencil/src/components/mds-pref-theme/mds-pref-theme.tsx
@@ -1,15 +1,13 @@
-import { Component, Host, Element, h } from '@stencil/core'
+import { Component, Host, Element, h, State } from '@stencil/core'
 import miBaselineLightMode from '@icon/mi/baseline/light-mode.svg'
 import miBaselineDarkMode from '@icon/mi/baseline/dark-mode.svg'
 import miBaselineSettings from '@icon/mi/baseline/settings.svg'
 import { Locale } from '@common/locale'
-import jsonEnData from './meta/i18n.en.json'
-import jsonItData from './meta/i18n.it.json'
+import localeEn from './meta/locale.en.json'
+import localeIt from './meta/locale.it.json'
+import { ThemeModeType } from './meta/types'
 
-const configData = {
-  en: jsonEnData,
-  it: jsonItData,
-}
+
 
 @Component({
   tag: 'mds-pref-theme',
@@ -18,20 +16,61 @@ const configData = {
 })
 export class MdsPrefTheme {
   @Element() private element: HTMLMdsPrefThemeElement
-  private t:Locale = new Locale(configData)
+  @State() themeMode: ThemeModeType
+  private defaultMode: ThemeModeType = 'system'
+  private t:Locale = new Locale({
+    en: localeEn,
+    it: localeIt,
+  })
+
+  // related selectors for @magma-design-system/styles
+  // projects/design-tokens/formats/css-vars-rgb/template.hbs
+  // projects/design-tokens/formats/css-vars-hex/template.hbs
+
+  private theme = {
+    dark: {
+      selector: 'pref-theme-dark',
+      label: 'darkMode',
+    },
+    system: {
+      selector: 'pref-theme-system',
+      label: 'systemSettings',
+    },
+    light: {
+      selector: 'pref-theme-light',
+      label: 'lightMode',
+    },
+  }
 
   componentWillRender (): void {
     this.t.lang(this.element)
   }
 
+  componentDidLoad (): void {
+    this.setTheme(localStorage.getItem('mds-pref-theme') as ThemeModeType ?? this.defaultMode)
+  }
+
+  private setTheme = (mode: ThemeModeType): void => {
+    localStorage.setItem('mds-pref-theme', mode)
+    this.themeMode = mode
+    if (document) {
+      for (const key in this.theme) {
+        if ({}.hasOwnProperty.call(this.theme, key)) {
+          document.querySelector('html')?.classList.remove(this.theme[key].selector)
+        }
+      }
+      document.querySelector('html')?.classList.add(this.theme[mode].selector)
+    }
+  }
+
   render () {
     return (
-      <Host>
-        <mds-text class="info" typography="detail"><b>{this.t.get('label')}</b> {this.t.get('systemSettings')}</mds-text>
+      <Host >
+        <mds-text class="info" typography="caption"><b>{this.t.get('label')}</b> {this.t.get(this.theme[this.themeMode ?? this.defaultMode].label)}</mds-text>
         <mds-tab>
-          <mds-tab-item class="item item--light-mode" icon={miBaselineLightMode}></mds-tab-item>
-          <mds-tab-item class="item item--system-settings" icon={miBaselineSettings}></mds-tab-item>
-          <mds-tab-item class="item item--dark-mode" icon={miBaselineDarkMode}></mds-tab-item>
+          <mds-tab-item onClick={() => { this.setTheme('light') }} class="item item--light-mode" icon={miBaselineLightMode}></mds-tab-item>
+          <mds-tab-item onClick={() => { this.setTheme('system') }} class="item item--system-settings" icon={miBaselineSettings}></mds-tab-item>
+          <mds-tab-item onClick={() => { this.setTheme('dark') }} class="item item--dark-mode" icon={miBaselineDarkMode}></mds-tab-item>
         </mds-tab>
       </Host>
     )
---
+++ projects/stencil/src/components/mds-pref-theme/meta/types.ts
@@ -0,0 +1,4 @@
+export type ThemeModeType =
+  | 'light'
+  | 'dark'
+  | 'system'
