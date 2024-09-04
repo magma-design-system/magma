66fc136782ec1e39273245115c2506da517ede48
Vittorio Vittori <vittorio.vittori@maggioli.it>
2024-09-04 09:43:13 +0200

    build(magma): fix clean cache

--- projects/stencil/scripts/clean-storybook.ts
+++ projects/stencil/scripts/clean-storybook.ts
@@ -5,5 +5,5 @@ console.info('Cleaning Storybook and Stencil caches')
 cleanDir(DIST_STORYBOOK_CACHE_DIR)
 cleanDir(DIST_PUBLISH_STENCIL_CACHE_DIR)
 cleanDir(DIST_STORYBOOK_MANAGER_CACHE_DIR)
-cleanDir(DIST_STENCIL_CACHE_DIR)
+// cleanDir(DIST_STENCIL_CACHE_DIR)
 
