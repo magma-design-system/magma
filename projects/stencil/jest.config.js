fe94371bfbeb3cccc8af852ca6498c61de263fb0
Nicola Tamburini <nicola.tamburini@maggioli.it>
2024-10-16 17:02:04 +0200

    test(stencil): remove deprecated scripts

--- projects/stencil/jest.config.js
+++ projects/stencil/jest.config.js
@@ -5,10 +5,6 @@ module.exports = {
   testEnvironment: "node",
   rootDir: ".",
   preset: 'ts-jest',
-  // modulePathIgnorePatterns: ["<rootDir>/src/components/*.json"],
-  // moduleNameMapper: {
-  //  "@maggioli-design-system/mds-table-header": "<rootDir>/scripts/test/mocks/mds-table-header/package.json"
-  // },
   testMatch: ["<rootDir>/scripts/**/?(*.)+(spec|test).[jt]s"],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {tsconfig: 'scripts.tsconfig.json'}],
