60715e0f16638db427d9879e134d71afd03ef30e
Vittorio Vittori <vittorio.vittori@maggioli.it>
2024-10-31 19:08:27 +0100

    perf(mds-file-preview): reduce included css filesize

--- projects/stencil/src/type/variant-file-format.ts
+++ projects/stencil/src/type/variant-file-format.ts
@@ -13,12 +13,28 @@ import baselineBorderAll from '@icon/mi/baseline/border-all.svg'
 import baselineDescription from '@icon/mi/baseline/description.svg'
 import mdiVectorCurve from '@icon/mdi/vector-curve.svg'
 import baselineVideocam from '@icon/mi/baseline/videocam.svg'
+import { ThemeFullVariantType } from '@type/variant'
+
+type FileFormat =
+  | 'archive'
+  | 'attachment'
+  | 'audio'
+  | 'code'
+  | 'data'
+  | 'document'
+  | 'email'
+  | 'executable'
+  | 'image'
+  | 'markup'
+  | 'slide'
+  | 'spreadsheet'
+  | 'text'
+  | 'vector'
+  | 'video'
 
 interface FileFormatVariant {
-  color: string
   icon: string
-  iconBackground: string
-  variant: string
+  variant: ThemeFullVariantType
 }
 
 interface FileFormatVariants {
@@ -27,111 +43,78 @@ interface FileFormatVariants {
 
 const fileFormatsVariant: FileFormatVariants = {
   archive: {
-    color: 'fill-label-amaranth-04 text-label-amaranth-04',
     icon: baselineFolderZip,
-    iconBackground: 'bg-label-amaranth-10',
     variant: 'amaranth',
   },
   attachment: {
-    color: 'fill-tone-neutral-04 text-tone-neutral-04',
     icon: baselineAttachFile,
-    iconBackground: 'bg-tone-neutral-10',
     variant: 'dark',
   },
   audio: {
-    color: 'fill-label-violet-04 text-label-violet-04',
     icon: baselineAudiotrack,
-    iconBackground: 'bg-label-violet-10',
     variant: 'violet',
   },
   code: {
-    color: 'fill-label-yellow-04 text-label-yellow-04',
     icon: baselineTerminal,
-    iconBackground: 'bg-label-yellow-10',
     variant: 'yellow',
   },
   data: {
-    color: 'fill-label-yellow-04 text-label-yellow-04',
     icon: mdiHardDisk,
-    iconBackground: 'bg-label-yellow-10',
     variant: 'yellow',
   },
   document: {
-    color: 'fill-label-orange-04 text-label-orange-04',
     icon: baselineInsertDriveFile,
-    iconBackground: 'bg-label-orange-10',
     variant: 'orange',
   },
   email: {
-    color: 'fill-label-blue-04 text-label-blue-04',
     icon: baselineEmail,
-    iconBackground: 'bg-label-blue-10',
     variant: 'blue',
   },
   executable: {
-    color: 'fill-label-amaranth-04 text-label-amaranth-04',
     icon: baselineWysiwyg,
-    iconBackground: 'bg-label-amaranth-10',
     variant: 'amaranth',
   },
   image: {
-    color: 'fill-label-green-04 text-label-green-04',
     icon: baselinePanorama,
-    iconBackground: 'bg-label-green-10',
     variant: 'green',
   },
   imageRaster: {
-    color: 'fill-label-green-04 text-label-green-04',
     icon: baselinePanorama,
-    iconBackground: 'bg-label-green-10',
     variant: 'green',
   },
   markup: {
-    color: 'fill-label-yellow-04 text-label-yellow-04',
     icon: baselineWeb,
-    iconBackground: 'bg-label-yellow-10',
     variant: 'yellow',
   },
   slide: {
-    color: 'fill-label-orchid-04 text-label-orchid-04',
     icon: baselineTV,
-    iconBackground: 'bg-label-orchid-10',
     variant: 'orchid',
   },
   spreadsheet: {
-    color: 'fill-label-lime-04 text-label-lime-04',
     icon: baselineBorderAll,
-    iconBackground: 'bg-label-lime-10',
     variant: 'lime',
   },
   text: {
-    color: 'fill-label-blue-04 text-label-blue-04',
     icon: baselineDescription,
-    iconBackground: 'bg-label-blue-10',
     variant: 'blue',
   },
   vectorImage: {
-    color: 'fill-label-aqua-04 text-label-aqua-04',
     icon: mdiVectorCurve,
-    iconBackground: 'bg-label-aqua-10',
     variant: 'aqua',
   },
   vector: {
-    color: 'fill-label-aqua-04 text-label-aqua-04',
     icon: mdiVectorCurve,
-    iconBackground: 'bg-label-aqua-10',
     variant: 'aqua',
   },
   video: {
-    color: 'fill-label-violet-04 text-label-violet-04',
     icon: baselineVideocam,
-    iconBackground: 'bg-label-violet-10',
     variant: 'violet',
   },
 }
 
 export {
   fileFormatsVariant,
+  FileFormat,
   FileFormatVariant,
   FileFormatVariants,
 }
