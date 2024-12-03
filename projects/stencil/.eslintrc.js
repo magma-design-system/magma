2461159078aac6c64be4370d1fe382d509b38f1d
Nicola Tamburini <nicola.tamburini@maggioli.it>
2024-12-03 19:02:14 +0100

    chore(magma): update jest to 29.7.0

--- projects/stencil/.eslintrc.js
+++ projects/stencil/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended', 'plugin:@stencil-community/eslint-plugin'],
   ignorePatterns: ['**/*.config.js', '**/*.stories.js', './.build', './dist', './src/componnts.d.ts', './template'],
   overrides: [{
     files: ['./**/*.tsx', './**/*.ts', './*.config.ts'],
