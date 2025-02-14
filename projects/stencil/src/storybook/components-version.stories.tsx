40e83fb7628c569abbcf44a79cd9faddd487711e
Vittorio Vittori <vittorio.vittori@maggioli.it>
2025-02-14 18:28:13 +0100

    test(stencil): add better test presentation

--- projects/stencil/src/storybook/components-version.stories.tsx
+++ projects/stencil/src/storybook/components-version.stories.tsx
@@ -6,15 +6,30 @@ export default {
 }
 
 const Template = () =>
-  <div>
-    Here will be displayed the published components available here in Storybook alongside their running version
-    <div class='py-2 grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))]'>
-      {
-        Object.keys(versions).map((component, i) => <div key={i}>
-          <mds-text typography='action'>{component}</mds-text>: v{versions[component]}
-        </div>)
-      }
-    </div>
+  <div class="grid gap-600">
+    <mds-text typography='detail'>Here will be displayed the published components available here in Storybook alongside their running version</mds-text>
+    <mds-table interactive>
+      <mds-table-header>
+        <mds-table-header-cell sortable label="Component name"></mds-table-header-cell>
+        <mds-table-header-cell sortable label="Version"></mds-table-header-cell>
+      </mds-table-header>
+      <mds-table-body>
+        {
+          Object.keys(versions).map((component, i) => <mds-table-row key={i}>
+            <mds-table-cell value={component}><mds-text typography="detail" truncate="word"><b>{component}</b></mds-text></mds-table-cell>
+            <mds-table-cell value={versions[component]}><mds-text typography='hack'>v{versions[component]}</mds-text></mds-table-cell>
+            <mds-button
+              icon="mdi/npm"
+              slot="action"
+              title="Go to NPM module"
+              tone="strong"
+              variant="error"
+              onClick={() => { window.open(`https://www.npmjs.com/package/@maggioli-design-system/${component}`, '_blank') }}
+            ></mds-button>
+          </mds-table-row>)
+        }
+      </mds-table-body>
+    </mds-table>
   </div>
 
 export const Versions = Template.bind({})
--- projects/stencil/src/storybook/hydrated.stories.tsx
+++ projects/stencil/src/storybook/hydrated.stories.tsx
@@ -20,7 +20,7 @@ const Template = () => {
       <mds-input-switch id="toggle">Render component</mds-input-switch>
     </div>
     { isRendered && <div class="grid gap-400">
-      <div class="flex bg-tone-neutral-09 p-600 rounded-lg items-center gap-400">
+      <div class="flex bg-tone-neutral-09 p-600 rounded-lg items-center gap-400 flex-wrap">
         <mds-button variant="light">This is a rendered component</mds-button>
         <mds-text>Use <mds-text typography='snippet'>hydrated.css</mds-text> from styles to avoid <b>FOUC, Flash of Unstyled Content</b></mds-text>
       </div>
