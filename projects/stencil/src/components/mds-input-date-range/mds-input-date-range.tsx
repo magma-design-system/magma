3d0b42600e8f03ecee6269a3547c616a410de1f2
andrea.savo <andrea.savo@maggioli.it>
2025-03-27 11:24:12 +0100

    fix(mds-input-date-range): emit event with startDate and endDate

--- projects/stencil/src/components/mds-input-date-range/mds-input-date-range.tsx
+++ projects/stencil/src/components/mds-input-date-range/mds-input-date-range.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Host, h, Prop, Watch, Listen, State } from '@stencil/core'
+import { Component, Element, Host, h, Prop, Watch, Listen, State, Event, EventEmitter } from '@stencil/core'
 import miBaselineCalendarToday from '@icon/mi/baseline/calendar-today.svg'
 import { FocusEvent } from 'react'
 
@@ -16,6 +16,15 @@ export class MdsInputDateRange {
   @Prop({ reflect: true, mutable: true }) startDate: string = ''
   @Prop({ reflect: true, mutable: true }) endDate: string = ''
 
+  @Event() dateRangeSelected: EventEmitter<{ startDate: string, endDate: string }>
+
+  @State() isStartFocused: boolean = false
+  @State() isEndFocused: boolean = false
+
+  @State() dropdownRef?: HTMLMdsDropdownElement
+
+  private previousStartDate: string = ''
+  private previousEndDate: string = ''
 
   @Watch('startDate')
   onStartDateChange (newValue: string): void {
@@ -54,19 +63,51 @@ export class MdsInputDateRange {
     this.focusInput(this.host.querySelector('mds-input-date[slot="end"]') as HTMLMdsInputDateElement)
   }
 
+  @Listen('focusin', { target: 'body' })
+  handleFocusInEvent (ev: FocusEvent): void {
+    const target = ev.target as HTMLElement
+    if (target.closest('mds-input-date[slot="start"]')) {
+      this.isStartFocused = true
+    }
+    if (target.closest('mds-input-date[slot="end"]')) {
+      this.isEndFocused = true
+    }
+  }
+
   @Listen('focusout', { target: 'body' })
   handleFocusOutEvent (ev: FocusEvent): void {
     const target = ev.target as HTMLElement
 
     if (target && target.closest('mds-input-date[slot="start"]')) {
+      this.isStartFocused = false
       const startInput = target as HTMLMdsInputDateElement
       this.startDate = startInput.value
     }
 
     if (target && target.closest('mds-input-date[slot="end"]')) {
+      this.isEndFocused = false
       const endInput = target as HTMLMdsInputDateElement
       this.endDate = endInput.value
     }
+
+    setTimeout(() => {
+      if (!this.isStartFocused && !this.isEndFocused) {
+        if (this.startDate && this.endDate && this.endDate < this.startDate) {
+          this.endDate = this.startDate
+        }
+
+        if (this.startDate !== this.previousStartDate || this.endDate !== this.previousEndDate) {
+          this.previousStartDate = this.startDate
+          this.previousEndDate = this.endDate
+
+          this.dateRangeSelected.emit({
+            startDate: this.startDate,
+            endDate: this.endDate,
+          })
+        }
+      }
+
+    }, 0)
   }
 
   render () {
@@ -98,7 +139,7 @@ export class MdsInputDateRange {
           }}>
         </mds-button>
 
-        <mds-dropdown target="#calendar-dropdown">
+        <mds-dropdown ref={el => this.dropdownRef = el as HTMLMdsDropdownElement} target="#calendar-dropdown">
           <mds-calendar
             key={this.calendarKey}
             rangePicker={true}
@@ -106,6 +147,17 @@ export class MdsInputDateRange {
               this.startDate = ev.detail.startDate
               if (ev.detail.endDate !== undefined) {
                 this.endDate = ev.detail.endDate
+              }
+
+              if (this.startDate && this.endDate) {
+                this.dateRangeSelected.emit({
+                  startDate: this.startDate,
+                  endDate: this.endDate,
+                })
+
+                if (this.dropdownRef) {
+                  this.dropdownRef.visible = false
+                }
               }
             }}
             startDate={this.startDate}
