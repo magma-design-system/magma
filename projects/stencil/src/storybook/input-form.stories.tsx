e6cd787d7ff85d06e233e79f60f6c9f735c4720b
Vittorio Vittori <vittorio.vittori@maggioli.it>
2025-04-18 09:39:39 +0200

    test(mds-input): add async validation example

--- projects/stencil/src/storybook/input-form.stories.tsx
+++ projects/stencil/src/storybook/input-form.stories.tsx
@@ -17,6 +17,17 @@ mds-input-range cannot be required
 const Template = () => {
   const [haveBeenJapan, setInJapan] = useState(false)
   useEffect(() => {
+    const elEmail = document.querySelector('#email') as HTMLMdsInputElement
+    const elInputEmail = document.querySelector('#email mds-input') as HTMLMdsInputElement
+    elEmail.addEventListener('mdsInputBlur', () => {
+      elInputEmail.setAttribute('await', 'true')
+      elEmail.setAttribute('variant', 'primary')
+      setTimeout(() => {
+        elEmail.setAttribute('variant', 'error')
+        elEmail.setAttribute('message', 'Email already present in database')
+        elInputEmail.setAttribute('await', 'false')
+      }, 2000)
+    })
     const elSwitch = document.querySelector('#japan-switch') as HTMLMdsInputSwitchElement
     elSwitch.addEventListener('mdsInputSwitchChange', (e: CustomEvent) => {
       setInJapan(e.detail.checked)
@@ -32,9 +43,20 @@ const Template = () => {
       <mds-input-field label="Full name">
         <mds-input type="text" placeholder="Es: Mario Rossi" maxlength={50} name="fullName" required></mds-input>
       </mds-input-field>
+      <mds-input-field label="email" id="email">
+        <mds-input type="email" placeholder="Es: used@email.com" name="mail" required></mds-input>
+      </mds-input-field>
       <mds-input-field label="Fiscal code">
         <mds-input type="cf" placeholder="Es: MROBRS86R14D788J" name="cf" required></mds-input>
       </mds-input-field>
+      <div class="grid gap-x-600 gap-y-300 grid-cols-fit-md">
+        <mds-input-field label="temporary code">
+          <mds-input type="text" placeholder="Es: MROBRS86R14D788J" name="secretPhrase" readonly value="BFG9000"></mds-input>
+        </mds-input-field>
+        <mds-input-field label="Confirm madness">
+          <mds-input type="text" placeholder="Es: MROBRS86R14D788J" name="madness" disabled value="I confirm I'm MAD"></mds-input>
+        </mds-input-field>
+      </div>
       <div class="grid gap-x-600 gap-y-300 grid-cols-fit-md">
         <mds-input-field label="Birth date">
           <mds-input-date></mds-input-date>
@@ -49,7 +71,7 @@ const Template = () => {
         </mds-input-field>
       </div>
       <mds-input-field label="Bigraphy">
-        <mds-input type="textarea" placeholder="Es: I would like to save the world, but first I'll try to save myself" name="bigraphy" maxlength={250}></mds-input>
+        <mds-input type="textarea" placeholder="Es: I would like to save the world, but first I'll try to save myself" name="bigraphy" maxlength={350}></mds-input>
       </mds-input-field>
       <mds-input-switch id="japan-switch" size="sm">I've been in Japan</mds-input-switch>
       { haveBeenJapan && <mds-input-field label="period in japan">
@@ -60,6 +82,10 @@ const Template = () => {
       </mds-input-field> }
       <mds-input-range min={0} max={100}>Coolness</mds-input-range>
       <mds-input-switch type="checkbox" explicit size="sm" name="isSellPrivacy" icon="mi/baseline/privacy-tip">Sell your privacy to our best buyer without gain nothing</mds-input-switch>
+      <div class="grid gap-x-600 gap-y-300 grid-cols-fit-md">
+        <div></div>
+        <mds-button type="submit" size="lg">Submit</mds-button>
+      </div>
     </div>
   </form>
 }
