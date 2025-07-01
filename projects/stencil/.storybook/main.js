093f7873bd2f55437b09d08680b7c8cdb93cc028
Nicola Tamburini <nicola.tamburini@maggioli.it>
2025-07-01 16:40:49 +0200

    refact(stencil): fix storybook eslint error

--- projects/stencil/.storybook/main.js
+++ projects/stencil/.storybook/main.js
@@ -1,15 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 // https://www.mokkapps.de/blog/run-build-and-deploy-stencil-and-storybook-from-one-repository
-const path = require('path');
+const path = require('path')
 const alias = {
   '@dictionary': path.resolve(__dirname, '../src/dictionary/'),
   '@fixture': path.resolve(__dirname, '../src/fixtures/'),
   '@icon': path.resolve(__dirname, '../assets/svg/'),
   '@placeholder': 'https://via.placeholder.com',
   '@test': path.resolve(__dirname, '../src/test/'),
-  '@type': path.resolve(__dirname, '../src/types/')
-};
-const TsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const stories = ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
+  '@type': path.resolve(__dirname, '../src/types/'),
+}
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const stories = [
+  '../src/**/*.stories.mdx',
+  '../src/**/*.stories.@(js|jsx|ts|tsx)',
+]
 const staticDirs = ['../assets', '../dist']
 const addons = [
   '@storybook/addon-essentials',
@@ -18,27 +22,27 @@ const addons = [
     name: '@storybook/addon-styling',
     options: {
       postCss: {
-        implementation: require('postcss')
-      }
+        implementation: require('postcss'),
+      },
     },
   },
 ]
-const  webpackFinal = async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
+const webpackFinal = async config => {
+  // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
 
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      ...alias
-    };
-    config.module.rules.push(
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    ...alias,
+  }
+  config.module.rules.push(
+    {
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+    {
       test: /\.stories.tsx$/,
       exclude: /node_modules/,
       use: [
@@ -47,31 +51,46 @@ const  webpackFinal = async (config, { configType }) => {
           options: {
             sourceType: 'unambiguous',
             babelrc: false,
-            presets: [['@babel/preset-react',{"runtime": "automatic"}], '@babel/preset-typescript']
-          }
-        }
-      ]
-    }, {
+            presets: [
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      ],
+    },
+    {
       test: /\.css$/,
-      use: [{
-        loader: 'postcss-loader',
-        options: {
-          postcssOptions: {
-            plugins: [require('postcss-iconsauce')('./.storybook/iconsauce.config.js'), require('tailwindcss')('./.storybook/tailwind.config.js'), require('autoprefixer')]
-          }
-        }
-      }],
-      include: path.resolve(__dirname, '../')
-    });
-    config.resolve.fallback = {
-      path: false
-    };
-    config.resolve.plugins = config.resolve.plugins || [];
-    config.resolve.plugins.push(new TsConfigPathsPlugin({
-      configFile: path.resolve(__dirname, "../tsconfig.json")
-    }));
-    return config;
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                require('postcss-iconsauce')(
+                  './.storybook/iconsauce.config.js',
+                ),
+                require('tailwindcss')('./.storybook/tailwind.config.js'),
+                require('autoprefixer'),
+              ],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    },
+  )
+  config.resolve.fallback = {
+    path: false,
   }
+  config.resolve.plugins = config.resolve.plugins || []
+  config.resolve.plugins.push(
+    new TsConfigPathsPlugin({
+      configFile: path.resolve(__dirname, '../tsconfig.json'),
+    }),
+  )
+  return config
+}
 const framework = { name: '@storybook/react-webpack5', options: {} }
 const docs = { autodocs: true }
 
@@ -83,10 +102,10 @@ const config = {
   framework,
   options: {
     storySort: (a, b) => {
-      return a.title.localeCompare(b.title, 'it-IT', { numeric: true });
+      return a.title.localeCompare(b.title, 'it-IT', { numeric: true })
     },
   },
-  docs
+  docs,
 }
 
-export default config;
+export default config
--- projects/stencil/.storybook/manager.js
+++ projects/stencil/.storybook/manager.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AddonPanel, Form } from '@storybook/components'
-import { useGlobals, addons, types } from '@storybook/manager-api'
+import { addons, types } from '@storybook/manager-api'
 import themeMaggioli from './theme'
-import { useState } from 'react'
 import clsx from 'clsx'
 
 const AccessibilityPanel = () => {
 
-  const capitalize = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  const capitalize = string => {
+    return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
   const setAccessibility = (preference, value, list) => {
@@ -33,7 +32,7 @@ const AccessibilityPanel = () => {
     }
   }
 
-  const setLanguage = (lang) => {
+  const setLanguage = lang => {
     if (window) {
       window.localStorage.setItem('mdsPrefLanguage', lang)
     }
@@ -49,7 +48,7 @@ const AccessibilityPanel = () => {
     }
   }
 
-  const checkAccessibilityUse = (reset) => {
+  const checkAccessibilityUse = reset => {
     if (reset) {
       setAccessibility('theme', 'unset')
       setAccessibility('contrast', 'unset')
@@ -73,7 +72,7 @@ const AccessibilityPanel = () => {
 
   const [enabled, setEnabledPrefs] = useState(usePrefs)
 
-  const togglePreferences = (isEnabled) => {
+  const togglePreferences = isEnabled => {
     setEnabledPrefs(isEnabled === 'enable')
     checkAccessibilityUse(enabled)
     window.localStorage.setItem('mdsPrefStorybookPrefs', enabled)
@@ -85,7 +84,7 @@ const AccessibilityPanel = () => {
         <Form.Select
           name="pref-disable"
           defaultValue="disable"
-          onChange={(event) => { togglePreferences(event.target.value) }}
+          onChange={event => { togglePreferences(event.target.value) }}
         >
           <option value="enable">Enabled</option>
           <option value="disable">Disabled</option>
@@ -97,7 +96,7 @@ const AccessibilityPanel = () => {
           className={clsx(!enabled && 'opacity-30 pointer-events-none')}
           name="pref-theme"
           defaultValue={ window.localStorage.getItem('mdsPrefTheme') ?? 'light' }
-          onChange={(event) => { setAccessibility('theme', event.target.value, ['light', 'system', 'dark']) }}
+          onChange={event => { setAccessibility('theme', event.target.value, ['light', 'system', 'dark']) }}
         >
           <option value="light">Light</option>
           <option value="system">System</option>
@@ -111,7 +110,7 @@ const AccessibilityPanel = () => {
           className={clsx(!enabled && 'opacity-30 pointer-events-none')}
           name="pref-contrast"
           defaultValue={ window.localStorage.getItem('mdsPrefContrast') ?? 'no-preference' }
-          onChange={(event) => { setAccessibility('contrast', event.target.value, ['more', 'system', 'no-preference']) }}
+          onChange={event => { setAccessibility('contrast', event.target.value, ['more', 'system', 'no-preference']) }}
         >
           <option value="more">More</option>
           <option value="system">System</option>
@@ -125,7 +124,7 @@ const AccessibilityPanel = () => {
           className={clsx(!enabled && 'opacity-30 pointer-events-none')}
           name="pref-animation"
           defaultValue={ window.localStorage.getItem('mdsPrefAnimation') ?? 'no-preference' }
-          onChange={(event) => { setAccessibility('animation', event.target.value, ['reduce', 'system', 'no-preference']) }}
+          onChange={event => { setAccessibility('animation', event.target.value, ['reduce', 'system', 'no-preference']) }}
         >
           <option value="reduce">Reduce</option>
           <option value="system">System</option>
@@ -139,7 +138,7 @@ const AccessibilityPanel = () => {
           className={clsx(!enabled && 'opacity-30 pointer-events-none')}
           name="pref-consumption"
           defaultValue={ window.localStorage.getItem('mdsPrefConsumption') ?? 'high' }
-          onChange={(event) => { setAccessibility('consumption', event.target.value, ['low', 'medium', 'high']) }}
+          onChange={event => { setAccessibility('consumption', event.target.value, ['low', 'medium', 'high']) }}
         >
           <option value="low">Low</option>
           <option value="medium">Medium</option>
@@ -153,7 +152,7 @@ const AccessibilityPanel = () => {
           className={clsx(!enabled && 'opacity-30 pointer-events-none')}
           name="pref-language"
           defaultValue={ window.localStorage.getItem('mdsPrefLanguage') ?? 'en' }
-          onChange={(event) => { setLanguage(event.target.value) }}
+          onChange={event => { setLanguage(event.target.value) }}
         >
           <option value="it">Italiano</option>
           <option value="en">English</option>
@@ -168,14 +167,14 @@ const AccessibilityPanel = () => {
 addons.register('maggioli/panel', () => {
   addons.add('maggioli-addon/accessibility', {
     title: 'Magma accesibility',
-    //👇 Sets the type of UI element in Storybook
+    // 👇 Sets the type of UI element in Storybook
     type: types.PANEL,
     render: ({ active }) => {
       return (
-      <AddonPanel active={active}>
-        <AccessibilityPanel/>
-      </AddonPanel>
-    )},
+        <AddonPanel active={active}>
+          <AccessibilityPanel/>
+        </AddonPanel>
+      )},
   })
 })
 
