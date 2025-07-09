1786085ffdfac857623d96d310ed51c95084c2a0
Vittorio Vittori <vittorio.vittori@maggioli.it>
2025-07-09 10:53:39 +0200

    test(stencil): change color

--- projects/stencil/.storybook/theme.js
+++ projects/stencil/.storybook/theme.js
@@ -1,12 +1,12 @@
 import { create } from 'storybook/theming'
 import brandImage from './magma-logo.svg'
 export default create({
+  appBorderRadius: 6,
   base: 'light',
-  colorPrimary: '#00379e',
-  colorSecondary: '#00379e',
-  fontBase: 'Roboto, Karla, system, "Open Sans", sans-serif',
-  fontCode: 'monospace',
+  brandImage,
   brandTitle: 'Magma Design System',
   brandUrl: 'https://magma.maggiolicloud.it',
-  brandImage,
+  colorSecondary: '#0A50D4',
+  fontBase: 'Roboto, Karla, system, "Open Sans", sans-serif',
+  fontCode: 'monospace',
 })
