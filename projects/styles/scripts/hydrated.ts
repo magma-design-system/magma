72507946f06217977281676f62f56f92e1222154
Vittorio Vittori <vittorio.vittori@maggioli.it>
2026-03-17 10:00:23 +0100

    fix(styles): enhance component directory filtering to include only valid Stencil component folders

--- projects/styles/scripts/hydrated.ts
+++ projects/styles/scripts/hydrated.ts
@@ -7,8 +7,12 @@ import Handlebars from 'handlebars'
 
 const COMPONENTS_DIR = '../stencil/src/components'
 const main = async () => {
-  const componentsFolderContents = await readdir(COMPONENTS_DIR)
-  const components = componentsFolderContents.filter(element => element !== '.gitlab-ci.yml')
+  const componentsFolderContents = await readdir(COMPONENTS_DIR, { withFileTypes: true })
+  const components = componentsFolderContents
+    .filter(entry => entry.isDirectory())
+    .map(entry => entry.name)
+    // only include Stencil component tags/folders like "mds-foo-bar"
+    .filter(name => /^mds-[a-z0-9-]+$/.test(name))
   const hydratedTemplate = await readFile(join(TEMPLATES_DIR, 'hydrated.css.hbs')).catch(error => { throw new Error(chalk.red(error)) })
   const template = Handlebars.compile(hydratedTemplate.toString())
   const compiledHydratedComponents = template({ components })
