b58cb0a185842955603da43986fd135e466f156a
Nicola Tamburini <nicola.tamburini@maggioli.it>
2026-06-03 16:25:53 +0200

    chore(magma): remove deprecated projects

--- projects/deprecated/lit/scripts/meta.ts
+++
@@ -1,9 +0,0 @@
-import { resolve, join } from 'path'
-const PROJECT_DIR = resolve(__dirname, '../')
-const TEMPLATES_DIR = join(PROJECT_DIR, 'template')
-const COMPONENTS_DIR = join(PROJECT_DIR, 'components')
-
-export {
-  COMPONENTS_DIR,
-  TEMPLATES_DIR,
-}
